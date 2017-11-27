0d00b002adbdf924dbd29817e97665566a7d08cb
Ido Shamun <ido@elegantmonkeys.com>
2017-11-27 16:49:46 +0200

    refactor: change default link for redirection of download

--- src/routes/download.js
+++ src/routes/download.js
@@ -11,12 +11,10 @@ router.get(
 
     const browser = ctx.userAgent.browser.toLowerCase();
 
-    if (browser === 'chrome') {
-      ctx.redirect('https://chrome.google.com/webstore/detail/daily-discover-web-techno/jlmpjdjjbgclbocgajdjefcidcncaied');
-    } else if (browser === 'firefox') {
+    if (browser === 'firefox') {
       ctx.redirect('https://addons.mozilla.org/en-US/firefox/addon/daily/');
     } else {
-      ctx.redirect('https://www.dailynow.co');
+      ctx.redirect('https://chrome.google.com/webstore/detail/daily-discover-web-techno/jlmpjdjjbgclbocgajdjefcidcncaied');
     }
   },
 );
