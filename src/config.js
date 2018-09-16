be002885ad6f9e67657c993653e97387b7b420b7
Ido Shamun <ido@elegantmonkeys.com>
2018-09-16 12:56:35 +0300

    chore: set default pool size

--- src/config.js
+++ src/config.js
@@ -7,6 +7,8 @@ const getMysqlConfig = () => {
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     charset: 'utf8mb4',
+    pool: { min: 2, max: 20 },
+    acquireConnectionTimeout: 10000,
   };
 
   if (process.env.MYSQL_INSTANCE && process.env.NODE_ENV === 'production') {
