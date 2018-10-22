8b0377fa920d616c2c1a1b65a196768c6c754713
Ido Shamun <ido@elegantmonkeys.com>
2018-10-22 18:17:35 +0300

    chore: increased max connections to db

--- src/config.js
+++ src/config.js
@@ -7,8 +7,7 @@ const getMysqlConfig = () => {
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     charset: 'utf8mb4',
-    pool: { min: 2, max: 100 },
-    acquireConnectionTimeout: 30000,
+    pool: { min: 2, max: 150 },
   };
 
   if (process.env.MYSQL_INSTANCE && process.env.NODE_ENV === 'production') {
