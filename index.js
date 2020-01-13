a63ab8a1d3aa62745794902204a34a3e89dbff80
Ido Shamun <ido@elegantmonkeys.com>
2020-01-14 00:44:50 +0200

    refactor: separate pubsub processing to another standalone process

--- index.js
+++ index.js
@@ -3,19 +3,9 @@ import logger from './src/logger';
 import app from './src/index';
 import config from './src/config';
 import { migrate } from './src/db';
-import newPostsWorker from './src/workers/newPosts';
-import newViewEventsWorker from './src/workers/newViewEvents';
-
-const registerWorks = async () => {
-  if (process.env.GCLOUD_PROJECT || process.env.GOOGLE_APPLICATION_CREDENTIALS) {
-    await newPostsWorker();
-    await newViewEventsWorker();
-  }
-};
 
 logger.info('migrating database');
 migrate()
-  .then(registerWorks)
   .then(() => {
     const server = app.listen(config.port);
 
