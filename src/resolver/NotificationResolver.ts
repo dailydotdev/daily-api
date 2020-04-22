30c63cddfc38d3f6120c3d48592cc0d891a03696
Ido Shamun <ido@elegantmonkeys.com>
2020-04-23 01:14:06 +0300

    refactor: fix lint issues

--- src/resolver/NotificationResolver.ts
+++ src/resolver/NotificationResolver.ts
@@ -4,7 +4,9 @@ import { Context } from '../Context';
 
 @Resolver()
 export class NotificationResolver {
-  @Query(() => [ Notification ], { description: 'Get up to 5 latest notifications' })
+  @Query(() => [Notification], {
+    description: 'Get up to 5 latest notifications',
+  })
   latestNotifications(@Ctx() ctx: Context): Promise<Notification[]> {
     return ctx
       .getRepository(Notification)
