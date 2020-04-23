9c72c280860f7700133d98be35297f0e6352d731
Ido Shamun <ido@daily.dev>
2020-04-23 13:02:09 +0300

    feat: trace graphql resolvers (#52)
    A global middleware is attached to the apollo server to trace every resolver call.

--- __tests__/helpers.ts
+++ __tests__/helpers.ts
@@ -1,10 +1,22 @@
-import { mock } from 'jest-mock-extended';
+import { mock, MockProxy } from 'jest-mock-extended';
 import { FastifyRequest } from 'fastify';
 import { Connection } from 'typeorm';
+import {
+  RootSpan,
+  Span,
+} from '@google-cloud/trace-agent/build/src/plugin-types';
 import { Context } from '../src/Context';
 
 export class MockContext extends Context {
+  mockSpan: MockProxy<RootSpan> & RootSpan;
+
   constructor(con: Connection) {
     super(mock<FastifyRequest>(), con);
+    this.mockSpan = mock<RootSpan>();
+    this.mockSpan.createChildSpan.mockImplementation(() => mock<Span>());
+  }
+
+  get span(): RootSpan {
+    return this.mockSpan;
   }
 }
--- src/apollo.ts
+++ src/apollo.ts
@@ -1,17 +1,13 @@
 import { ApolloServer, Config } from 'apollo-server-fastify';
 import { buildSchema } from 'type-graphql';
 import { NotificationResolver } from './resolver';
-
-// import { Context } from './Context';
-
-// const authChecker: AuthChecker<Context> = ({ context }) => {
-//   return !!context.userId;
-// };
+import { ResolverTracing } from './middleware';
 
 export default async function (config: Config): Promise<ApolloServer> {
   const schema = await buildSchema({
     resolvers: [NotificationResolver],
     emitSchemaFile: !process.env.NODE_ENV,
+    globalMiddlewares: [ResolverTracing],
   });
   return new ApolloServer({
     schema,
