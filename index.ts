bb9773da461ba23c6efcac1b16b1e5831021997f
Ido Shamun <ido@elegantmonkeys.com>
2020-05-01 15:32:10 +0300

    refactor: listen on all available addresses

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import appFunc from './src';
 
 appFunc()
-  .then((app) => app.listen(parseInt(process.env.PORT) || 3000))
+  .then((app) => app.listen(parseInt(process.env.PORT) || 3000, '0.0.0.0'))
   .catch((err) => {
     console.error(err);
     process.exit(1);
