77b37f4e9be8618da9162105ec122d1b044af2e0
Ido Shamun <ido@daily.dev>
2020-10-13 15:49:19 +0300

    feat: user stats query (#181)
    Get the user statistics for the profile page

---
+++ __tests__/users.ts
@@ -0,0 +1,169 @@
+import { FastifyInstance } from 'fastify';
+import { Connection, getConnection } from 'typeorm';
+import { ApolloServer } from 'apollo-server-fastify';
+import {
+  ApolloServerTestClient,
+  createTestClient,
+} from 'apollo-server-testing';
+import createApolloServer from '../src/apollo';
+import { Context } from '../src/Context';
+import { MockContext, saveFixtures } from './helpers';
+import appFunc from '../src';
+import { Comment, Post, Source, User } from '../src/entity';
+import { sourcesFixture } from './fixture/source';
+
+let app: FastifyInstance;
+let con: Connection;
+let server: ApolloServer;
+let client: ApolloServerTestClient;
+let loggedUser: string = null;
+
+beforeAll(async () => {
+  con = await getConnection();
+  server = await createApolloServer({
+    context: (): Context => new MockContext(con, loggedUser),
+    playground: false,
+  });
+  client = createTestClient(server);
+  app = await appFunc();
+  return app.ready();
+});
+
+const now = new Date();
+
+beforeEach(async () => {
+  loggedUser = null;
+
+  await con.getRepository(User).save([
+    {
+      id: '1',
+      name: 'Ido',
+      image: 'https://daily.dev/ido.jpg',
+    },
+    { id: '2', name: 'Tsahi', image: 'https://daily.dev/tsahi.jpg' },
+  ]);
+  await saveFixtures(con, Source, sourcesFixture);
+  await saveFixtures(con, Post, [
+    {
+      id: 'p1',
+      shortId: 'sp1',
+      title: 'P1',
+      url: 'http://p1.com',
+      sourceId: 'a',
+      createdAt: now,
+      authorId: '1',
+      views: 20,
+      upvotes: 5,
+    },
+    {
+      id: 'p2',
+      shortId: 'sp2',
+      title: 'P2',
+      url: 'http://p2.com',
+      sourceId: 'b',
+      createdAt: new Date(now.getTime() - 1000),
+      views: 5,
+      upvotes: 1,
+    },
+    {
+      id: 'p3',
+      shortId: 'sp3',
+      title: 'P3',
+      url: 'http://p3.com',
+      sourceId: 'c',
+      createdAt: new Date(now.getTime() - 2000),
+      authorId: '1',
+      views: 80,
+      upvotes: 10,
+    },
+    {
+      id: 'p4',
+      shortId: 'sp4',
+      title: 'P4',
+      url: 'http://p4.com',
+      sourceId: 'a',
+      createdAt: new Date(now.getTime() - 3000),
+      authorId: '1',
+      upvotes: 5,
+    },
+    {
+      id: 'p5',
+      shortId: 'sp5',
+      title: 'P5',
+      url: 'http://p5.com',
+      sourceId: 'b',
+      createdAt: new Date(now.getTime() - 4000),
+    },
+    {
+      id: 'p6',
+      shortId: 'sp6',
+      title: 'P6',
+      url: 'http://p6.com',
+      sourceId: 'p',
+      createdAt: new Date(now.getTime() - 5000),
+      views: 40,
+    },
+  ]);
+  await con.getRepository(Comment).save([
+    {
+      id: 'c1',
+      postId: 'p1',
+      userId: '1',
+      content: 'parent comment',
+      createdAt: new Date(2020, 1, 6, 0, 0),
+      upvotes: 5,
+    },
+    {
+      id: 'c2',
+      parentId: 'c1',
+      postId: 'p1',
+      userId: '1',
+      content: 'child comment',
+      createdAt: new Date(2020, 1, 7, 0, 0),
+      upvotes: 10,
+    },
+    {
+      id: 'c3',
+      postId: 'p1',
+      userId: '2',
+      content: 'parent comment #2',
+      createdAt: new Date(2020, 1, 8, 0, 0),
+      upvotes: 2,
+    },
+  ]);
+});
+
+afterAll(() => app.close());
+
+describe('query userStats', () => {
+  const QUERY = `query UserStats($id: ID!){
+    userStats(id: $id) {
+      numPosts
+      numComments
+      numPostViews
+      numPostUpvotes
+      numCommentUpvotes
+    }
+  }`;
+
+  it('should return null when the user is not the stats owner', async () => {
+    loggedUser = '1';
+    const res = await client.query({ query: QUERY, variables: { id: '2' } });
+    expect(res.errors).toBeFalsy();
+    expect(res.data.userStats).toEqual(null);
+  });
+
+  it('should return the user stats', async () => {
+    loggedUser = '1';
+    const res = await client.query({ query: QUERY, variables: { id: '1' } });
+    expect(res.errors).toBeFalsy();
+    expect(res.data).toMatchSnapshot();
+  });
+
+  it('should return partial user stats when no posts or no comments', async () => {
+    loggedUser = '2';
+    const res = await client.query({ query: QUERY, variables: { id: '2' } });
+    expect(res.errors).toBeFalsy();
+    expect(res.data).toMatchSnapshot();
+  });
+});
