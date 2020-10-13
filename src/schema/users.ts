c438b58210f297fa30b9c138a6d3fd5410e1f9e1
Ido Shamun <idoesh1@gmail.com>
2020-10-13 15:43:34 +0300

    feat: user stats query
    Get the user statistics for the profile page

--- src/schema/users.ts
+++ src/schema/users.ts
@@ -1,5 +1,7 @@
 import { gql, IResolvers } from 'apollo-server-fastify';
 import { Context } from '../Context';
+import { traceResolverObject } from './trace';
+import { Comment, Post } from '../entity';
 
 export interface GQLUser {
   id: string;
@@ -7,6 +9,15 @@ export interface GQLUser {
   image: string;
   username?: string;
 }
+
+type PostStats = {
+  numPosts: number;
+  numPostViews: number;
+  numPostUpvotes: number;
+};
+type CommentStats = { numComments: number; numCommentUpvotes: number };
+
+export type GQLUserStats = PostStats & CommentStats;
 
 export const typeDefs = gql`
   """
@@ -34,10 +45,60 @@ export const typeDefs = gql`
     """
     permalink: String!
   }
+
+  type UserStats {
+    numPosts: Int!
+    numComments: Int!
+    numPostViews: Int!
+    numPostUpvotes: Int!
+    numCommentUpvotes: Int!
+  }
+
+  extend type Query {
+    """
+    Get the statistics of the user
+    """
+    userStats(id: ID!): UserStats
+  }
 `;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const resolvers: IResolvers<any, Context> = {
+  Query: traceResolverObject({
+    userStats: async (
+      source,
+      { id }: { id: string },
+      ctx: Context,
+    ): Promise<GQLUserStats | null> => {
+      if (ctx.userId != id) {
+        return null;
+      }
+      const [postStats, commentStats] = await Promise.all([
+        ctx.con
+          .createQueryBuilder()
+          .select('count(*)', 'numPosts')
+          .addSelect('sum(post.views)', 'numPostViews')
+          .addSelect('sum(post.upvotes)', 'numPostUpvotes')
+          .from(Post, 'post')
+          .where({ authorId: id })
+          .getRawOne<PostStats>(),
+        ctx.con
+          .createQueryBuilder()
+          .select('count(*)', 'numComments')
+          .addSelect('sum(comment.upvotes)', 'numCommentUpvotes')
+          .from(Comment, 'comment')
+          .where({ userId: id })
+          .getRawOne<CommentStats>(),
+      ]);
+      return {
+        numPosts: postStats?.numPosts ?? 0,
+        numComments: commentStats?.numComments ?? 0,
+        numPostViews: postStats?.numPostViews ?? 0,
+        numPostUpvotes: postStats?.numPostUpvotes ?? 0,
+        numCommentUpvotes: commentStats?.numCommentUpvotes ?? 0,
+      };
+    },
+  }),
   User: {
     permalink: (user: GQLUser): string =>
       `${process.env.COMMENTS_PREFIX}/${user.username ?? user.id}`,
