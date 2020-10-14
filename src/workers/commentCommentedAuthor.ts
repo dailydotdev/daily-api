8fe33cc72d60c0bf33ea1128618c37e76dba213a
Ido Shamun <idoesh1@gmail.com>
2020-10-14 11:25:09 +0300

    feat: send email to the post author on every comment

--- src/workers/commentCommentedAuthor.ts
+++ src/workers/commentCommentedAuthor.ts
@@ -5,7 +5,7 @@ import {
   sendEmail,
   truncateComment,
 } from '../common';
-import { fetchUser, getDiscussionLink } from '../common';
+import { fetchUser, getDiscussionLink, pickImageUrl } from '../common';
 
 interface Data {
   userId: string;
@@ -15,37 +15,31 @@ interface Data {
 
 const worker: Worker = {
   topic: 'comment-commented',
-  subscription: envBasedName('comment-commented-mail'),
+  subscription: envBasedName('comment-commented-author-mail'),
   handler: async (message, con, logger): Promise<void> => {
     const data: Data = messageToJson(message);
     try {
       const comment = await con
         .getRepository(Comment)
-        .findOne(data.childCommentId, { relations: ['post', 'parent'] });
-      const parent = await comment.parent;
-      const [author, commenter] = await Promise.all([
-        fetchUser(parent.userId),
-        fetchUser(data.userId),
-      ]);
+        .findOne(data.childCommentId, { relations: ['post'] });
       const post = await comment.post;
-      if (
-        parent &&
-        parent.userId !== data.userId &&
-        author.id !== post.authorId
-      ) {
+      if (post.authorId) {
+        const [author, commenter] = await Promise.all([
+          fetchUser(post.authorId),
+          fetchUser(data.userId),
+        ]);
         const link = getDiscussionLink(post.id);
         await sendEmail({
           ...baseNotificationEmailData,
           to: author.email,
-          templateId: 'd-90c229bde4af427c8708a7615bfd85b4',
+          templateId: 'd-aba78d1947b14307892713ad6c2cafc5',
           dynamicTemplateData: {
             /* eslint-disable @typescript-eslint/camelcase */
             profile_image: commenter.image,
             full_name: commenter.name,
-            main_comment: truncateComment(parent),
-            new_comment: truncateComment(comment),
-            main_comment_link: link,
             post_title: post.title,
+            post_image: post.image || pickImageUrl(post),
+            new_comment: truncateComment(comment),
             discussion_link: link,
             user_reputation: commenter.reputation,
             /* eslint-enable @typescript-eslint/camelcase */
@@ -56,7 +50,7 @@ const worker: Worker = {
             data,
             messageId: message.id,
           },
-          'comment email sent',
+          'comment author email sent',
         );
       }
       message.ack();
@@ -67,7 +61,7 @@ const worker: Worker = {
           messageId: message.id,
           err,
         },
-        'failed to send comment email',
+        'failed to send comment author email',
       );
       if (err.name === 'QueryFailedError') {
         message.ack();
