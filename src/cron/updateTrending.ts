4ab9200fefc948a6fe14ba8b263dcea8d31acdbb
Ido Shamun <ido@daily.dev>
2020-11-24 12:41:51 +0200

    refactor: limit trending articles (#298)
    Limit trending articles to be able to reduce the trending threshold

--- src/cron/updateTrending.ts
+++ src/cron/updateTrending.ts
@@ -11,12 +11,20 @@ where "trending" is not null
       await entityManager.query(`
 update "post" set "trending" = res."total", "lastTrending" = now()
 from (
-    select "postId", count(*) total, count(*) filter(where "timestamp" >= timezone('utc', now()) - interval '30 minutes') as "partial"
-    from "view"
-    where "timestamp" >= timezone('utc', now()) - interval '1 hour'
-    group by "postId"
+    select *
+    from (
+        select "postId", count(*) total, count(*) filter(where "timestamp" >= timezone('utc', now()) - interval '30 minutes') as "partial"
+        from "view"
+        inner join "post" on "view"."postId" = "post"."id"
+        where "view"."timestamp" >= timezone('utc', now()) - interval '1 hour'
+            and post."createdAt" <= timezone('utc', now()) - interval '30 minutes'
+        group by "postId"
+    ) res
+    where res."partial" != res."total" and res."total" >= 30 and (res."partial" * 1.0 / res."total") >= 0.6
+    order by (res."partial" * 1.0 / res."total") desc
+    limit 3
 ) as res
-where "post".id = res."postId" and post."createdAt" <= timezone('utc', now()) - interval '30 minutes' and res."partial" != res."total" and res."total" >= 30 and (res."partial" * 1.0 / res."total") >= 0.65
+where "post".id = res."postId"
 `);
     }),
 };
