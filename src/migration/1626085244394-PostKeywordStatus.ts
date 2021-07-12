446af834aa60ec9e2adc7b6a29396b63d79b15d0
Ido Shamun <idoesh1@gmail.com>
2021-07-12 15:35:39 +0300

    feat: denormalize keyword status to post_keyword table
    Joining the keyword table to get the keyword status is often used and might decrease performance.
    This change denormalizes the status to the post keyword as well to prevent the join

---
+++ src/migration/1626085244394-PostKeywordStatus.ts
@@ -0,0 +1,60 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class PostKeywordStatus1626085244394 implements MigrationInterface {
+  name = 'PostKeywordStatus1626085244394';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "public"."post_keyword" ADD "status" text`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_post_keyword_postId_status" ON "public"."post_keyword" ("postId", "status") `,
+    );
+    await queryRunner.query(
+      'UPDATE "public"."post_keyword" SET "status" = k."status" FROM keyword k WHERE k."value" = "post_keyword"."keyword"',
+    );
+    await queryRunner.query(
+      `CREATE OR REPLACE FUNCTION keyword_trigger() RETURNS trigger AS $$
+begin
+  if ((TG_OP = 'UPDATE' and new."status" <> old."status") or TG_OP = 'INSERT') then
+    update "public"."post_keyword" SET "status" = new.status WHERE "keyword" = new."value";
+  elsif (TG_OP = 'DELETE') then
+    update "public"."post_keyword" SET "status" = null WHERE "keyword" = old."value";
+  end if;
+  return new;
+end
+$$ LANGUAGE plpgsql;`,
+    );
+    await queryRunner.query(
+      `CREATE TRIGGER keyword_trigger AFTER INSERT OR UPDATE OR DELETE ON "public"."keyword" FOR EACH ROW EXECUTE PROCEDURE keyword_trigger()`,
+    );
+    await queryRunner.query(
+      `CREATE OR REPLACE FUNCTION post_keyword_trigger() RETURNS trigger AS $$
+begin
+  select status into new.status from keyword where new.keyword = keyword.value;
+  return new;
+end
+$$ LANGUAGE plpgsql;`,
+    );
+    await queryRunner.query(
+      `CREATE TRIGGER post_keyword_trigger BEFORE INSERT ON "public"."post_keyword" FOR EACH ROW EXECUTE PROCEDURE post_keyword_trigger()`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP TRIGGER post_keyword_trigger ON "public"."post_keyword"`,
+    );
+    await queryRunner.query(`DROP FUNCTION post_keyword_trigger`);
+    await queryRunner.query(
+      `DROP TRIGGER keyword_trigger ON "public"."keyword"`,
+    );
+    await queryRunner.query(`DROP FUNCTION keyword_trigger`);
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_post_keyword_postId_status"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "public"."post_keyword" DROP COLUMN "status"`,
+    );
+  }
+}
