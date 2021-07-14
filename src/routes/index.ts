9e8dc89982a757a695f2c97542c81dc5f7ba12c9
Ido Shamun <idoesh1@gmail.com>
2021-07-14 15:46:14 +0300

    refactor: change devcard route to devcards

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,10 @@ import { FastifyInstance } from 'fastify';
 
 import rss from './rss';
 import redirector from './redirector';
-import devcard from './devcard';
+import devcards from './devcards';
 
 export default async function (fastify: FastifyInstance): Promise<void> {
   fastify.register(rss, { prefix: '/rss' });
   fastify.register(redirector, { prefix: '/r' });
-  fastify.register(devcard, { prefix: '/devcard' });
+  fastify.register(devcards, { prefix: '/devcards' });
 }
