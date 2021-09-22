e30ff88cda58f7dacc0f783758528a22d270c09c
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-09-22 19:05:34 +0800

    refactor: Upvote Entities - Create a Family of Classes

--- src/entity/CommentUpvote.ts
+++ src/entity/CommentUpvote.ts
@@ -1,29 +1,16 @@
-import { Column, Entity, Index, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Entity, Index, ManyToOne, PrimaryColumn } from 'typeorm';
+import BaseUpvote from './BaseUpvote';
 import { Comment } from './Comment';
-import { User } from './User';
 
 @Entity()
-export class CommentUpvote {
+export class CommentUpvote extends BaseUpvote {
   @PrimaryColumn({ length: 14 })
   @Index()
   commentId: string;
-
-  @PrimaryColumn({ length: 36 })
-  @Index()
-  userId: string;
-
-  @Column({ default: () => 'now()' })
-  createdAt: Date;
 
   @ManyToOne(() => Comment, {
     lazy: true,
     onDelete: 'CASCADE',
   })
   comment: Promise<Comment>;
-
-  @ManyToOne(() => User, {
-    lazy: true,
-    onDelete: 'CASCADE',
-  })
-  user: Promise<User>;
 }
