16c6fc5a697e1f8e7c9939f8b82e0fdda3240fa5
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-10-18 11:48:01 +0800

    feat: alert schema

---
+++ src/schema/alerts.ts
@@ -0,0 +1,83 @@
+import { gql, IResolvers } from 'apollo-server-fastify';
+import { traceResolvers } from './trace';
+import { Context } from '../Context';
+import { Alert } from '../entity/Alert';
+
+interface GQLAlerts {
+  userId: string;
+  filter: boolean;
+}
+
+interface GQLUpdateAlertsInput extends Partial<GQLAlerts> {
+  filter?: boolean;
+}
+
+export const typeDefs = gql`
+  """
+  Alerts to display to the relevant user
+  """
+  type Alerts {
+    """
+    User ID
+    """
+    userId: ID!
+
+    """
+    Status to display for filter red dot
+    """
+    filter: Boolean!
+  }
+
+  input UpdateAlertsInput {
+    """
+    Status to display for filter red dot
+    """
+    filter: Boolean
+  }
+
+  extend type Mutation {
+    """
+    Update the alerts for user
+    """
+    updateUserAlerts(data: UpdateAlertsInput!): Alerts! @auth
+  }
+
+  extend type Query {
+    """
+    Get the alerts for user
+    """
+    userAlerts: Alerts! @auth
+  }
+`;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const resolvers: IResolvers<any, Context> = traceResolvers({
+  Mutation: {
+    updateUserAlerts: async (
+      _,
+      { data }: { data: GQLUpdateAlertsInput },
+      ctx,
+    ): Promise<GQLAlerts> => {
+      const repo = ctx.getRepository(Alert);
+      const alerts = await repo.findOne(ctx.userId);
+
+      if (!alerts) {
+        return repo.save(repo.merge(repo.create(data), { userId: ctx.userId }));
+      }
+
+      return repo.save(repo.merge(alerts, data));
+    },
+  },
+  Query: {
+    userAlerts: async (_, __, ctx): Promise<GQLAlerts> => {
+      const repo = ctx.getRepository(Alert);
+      const alerts = await repo.findOne(ctx.userId);
+
+      if (!alerts) {
+        await repo.insert({ userId: ctx.userId });
+        return repo.findOne(ctx.userId);
+      }
+      return alerts;
+    },
+  },
+});
