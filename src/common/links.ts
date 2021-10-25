9115ca497ade56a330c1ae48e240e9dd28f69fb7
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-10-25 19:16:43 +0800

    feat: allow receiving of url

--- src/common/links.ts
+++ src/common/links.ts
@@ -1,2 +1,14 @@
 export const getDiscussionLink = (postId: string): string =>
   `${process.env.COMMENTS_PREFIX}/posts/${postId}`;
+
+export function isValidHttpUrl(link: string): boolean {
+  let url: URL;
+
+  try {
+    url = new URL(link);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === 'http:' || url.protocol === 'https:';
+}
