e5129f16bcba12b08424ec45f6587344b450e2af
Lee Hansel Solevilla <sshanzel@yahoo.com>
2021-11-11 07:40:34 +0800

    refactor: view timestamp 0 precision

--- src/migration/1636431439048-ViewHiddenColumn.ts
+++ src/migration/1636431439048-ViewHiddenColumn.ts
@@ -5,7 +5,7 @@ export class ViewHiddenColumn1636431439048 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "public"."view" ADD "hidden" boolean NOT NULL DEFAULT false`);
-        await queryRunner.query(`ALTER TABLE "public"."view" ALTER COLUMN "timestamp" TYPE TIMESTAMP(3)`);
+        await queryRunner.query(`ALTER TABLE "public"."view" ALTER COLUMN "timestamp" TYPE TIMESTAMP(0)`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
