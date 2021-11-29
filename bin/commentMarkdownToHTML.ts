0e9500ebe3fc977b009ee8ddad9e78e96979f8e4
Chris Bongers <chrisbongers@gmail.com>
2021-11-29 15:27:48 +0400

    feat: markdown comments (#806)
    Added a markdown parser for the comment section.
    Also added a helper to convert old comments.
    
    DD-210

---
+++ bin/commentMarkdownToHTML.ts
@@ -0,0 +1,37 @@
+import '../src/config';
+import { createOrGetConnection } from '../src/db';
+import { Comment } from '../src/entity';
+import { markdown } from '../src/common/markdown';
+
+interface Row {
+  id: string;
+  content: string;
+}
+
+(async (): Promise<void> => {
+  const con = await createOrGetConnection();
+
+  const resStream = await con
+    .createQueryBuilder()
+    .select('id, content')
+    .from(Comment, 'c')
+    .where('c."contentHtml" is NULL')
+    .stream();
+
+  let index = 0;
+  resStream.on('data', async ({ id, content }: Row) => {
+    console.log(`updating ${index}`);
+    index++;
+
+    await con
+      .createQueryBuilder()
+      .update(Comment)
+      .set({ contentHtml: markdown.render(content) })
+      .where({ id })
+      .execute();
+  });
+  await new Promise((resolve, reject) => {
+    resStream.on('error', reject);
+    resStream.on('end', resolve);
+  });
+})();
---
+++ src/common/markdown.ts
@@ -0,0 +1,16 @@
+import MarkdownIt from 'markdown-it';
+import hljs from 'highlight.js';
+
+export const markdown: MarkdownIt = MarkdownIt({
+  html: true,
+  linkify: true,
+  typographer: true,
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(str, { language: lang }).value;
+      } catch (__) {}
+    }
+    return str;
+  },
+});
