eaec2ca0c52704cb61f990ad87e31986b0a6b8da
Chris Bongers <chrisbongers@gmail.com>
2021-11-29 18:16:15 +0400

    Making sure markdown links have target blank + rel

--- src/common/markdown.ts
+++ src/common/markdown.ts
@@ -14,3 +14,27 @@ export const markdown: MarkdownIt = MarkdownIt({
     return str;
   },
 });
+
+const defaultRender =
+  markdown.renderer.rules.link_open ||
+  function (tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options);
+  };
+
+markdown.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  const targetIndex = tokens[idx].attrIndex('target');
+  if (targetIndex < 0) {
+    tokens[idx].attrPush(['target', '_blank']);
+  } else {
+    tokens[idx].attrs[targetIndex][1] = '_blank';
+  }
+
+  const relIndex = tokens[idx].attrIndex('rel');
+  if (relIndex < 0) {
+    tokens[idx].attrPush(['rel', 'noopener nofollow']);
+  } else {
+    tokens[idx].attrs[relIndex][1] = 'noopener nofollow';
+  }
+
+  return defaultRender(tokens, idx, options, env, self);
+};
