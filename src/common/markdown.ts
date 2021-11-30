a5e1448ab111b68a08985e029eb3971ea964b031
Chris Bongers <chrisbongers@gmail.com>
2021-11-30 10:17:34 +0400

    fix: markdown links target and rel (#809)
    Making sure markdown links have target blank + rel

--- src/common/markdown.ts
+++ src/common/markdown.ts
@@ -14,3 +14,25 @@ export const markdown: MarkdownIt = MarkdownIt({
     return str;
   },
 });
+
+const defaultRender =
+  markdown.renderer.rules.link_open ||
+  function (tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options);
+  };
+
+const setTokenAttribute = (tokens, attribute, attributeValue) => {
+  const attributeIndex = tokens.attrIndex('attribute');
+  if (attributeIndex < 0) {
+    tokens.attrPush([attribute, attributeValue]);
+  } else {
+    tokens.attrs[attributeIndex][1] = attributeValue;
+  }
+  return tokens;
+};
+
+markdown.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  tokens[idx] = setTokenAttribute(tokens[idx], 'target', '_blank');
+  tokens[idx] = setTokenAttribute(tokens[idx], 'rel', 'noopener nofollow');
+  return defaultRender(tokens, idx, options, env, self);
+};
