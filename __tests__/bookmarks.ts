004ab54108c0acee6fdbe61a80c797366f59a619
Ido Shamun <idoesh1@gmail.com>
2021-12-02 12:22:24 +0200

    test: close open handles
    Signed-off-by: Ido Shamun <idoesh1@gmail.com>

--- __tests__/bookmarks.ts
+++ __tests__/bookmarks.ts
@@ -548,7 +548,7 @@ describe('compatibility routes', () => {
   describe('POST /posts/bookmarks', () => {
     it('should return bad request when no body is provided', () => {
       loggedUser = '1';
-      request(app.server).post('/v1/posts/bookmarks').expect(400);
+      return request(app.server).post('/v1/posts/bookmarks').expect(400);
     });
 
     it('should add new bookmarks', async () => {
