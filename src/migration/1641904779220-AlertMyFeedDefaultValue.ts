09b78c854e469a773ac25b2c58042b6dcb8cd953
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-01-12 14:29:14 +0800

    fix: migration

--- src/migration/1641904779220-AlertMyFeedDefaultValue.ts
+++ src/migration/1641904779220-AlertMyFeedDefaultValue.ts
@@ -4,14 +4,12 @@ export class AlertMyFeedDefaultValue1641904779220
   implements MigrationInterface
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "public"."alerts" ALTER COLUMN "myFeed" SET DEFAULT NULL`,
-    );
-  }
+        await queryRunner.query(`ALTER TABLE "alerts" ALTER COLUMN "myFeed" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "alerts" ALTER COLUMN "myFeed" DROP DEFAULT`);
+    }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "public"."alerts" ALTER COLUMN "myFeed" SET DEFAULT 'created'`,
-    );
-  }
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "alerts" ALTER COLUMN "myFeed" SET DEFAULT 'created'`);
+        await queryRunner.query(`ALTER TABLE "alerts" ALTER COLUMN "myFeed" SET NOT NULL`);
+    }
 }
