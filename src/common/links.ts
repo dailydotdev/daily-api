13071885889c08d75eb5ae4020f1d57bd4a18f7f
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-01-20 15:39:50 +0800

    refactor: is valid regex url

--- src/common/links.ts
+++ src/common/links.ts
@@ -1,6 +1,14 @@
 export const getDiscussionLink = (postId: string): string =>
   `${process.env.COMMENTS_PREFIX}/posts/${postId}`;
 
+const isRegexValidURL = (url: string) => {
+  const res = url.match(
+    /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g,
+  );
+
+  return res !== null;
+};
+
 export function isValidHttpUrl(link: string): boolean {
   let url: URL;
 
@@ -10,5 +18,8 @@ export function isValidHttpUrl(link: string): boolean {
     return false;
   }
 
-  return url.protocol === 'http:' || url.protocol === 'https:';
+  return (
+    (url.protocol === 'http:' || url.protocol === 'https:') &&
+    isRegexValidURL(url.href)
+  );
 }
