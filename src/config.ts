3170d89640281eae2de063773b7f31a7a1c60cda
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-02-18 17:57:09 +0800

    feat: webapp config url

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,8 @@ import dotenv from 'dotenv';
 
 const env = process.env.NODE_ENV || 'development';
 
+export const WEBAPP_URL =
+  env === 'development' ? 'http://localhost:5002' : 'https://app.daily.dev';
+
 dotenv.config({ path: `.env.${env}` });
 dotenv.config({ path: '.env' });
