245bcfcd7b1580594d4cb62be7bb9c7c08e9585e
Chris Bongers <chrisbongers@gmail.com>
2022-02-23 17:35:10 +0200

    fix: test and added migration  for the view

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -4,9 +4,11 @@ import { createOrGetConnection } from '../src/db';
 import { redisClient, redisPubSub } from '../src/redis';
 
 let con: Connection;
+const excludeEntities = ['ActiveView'];
 
 const cleanDatabase = async (): Promise<void> => {
   for (const entity of con.entityMetadatas) {
+    if (excludeEntities.includes(entity.name)) continue;
     const repository = con.getRepository(entity.name);
     await repository.query(`DELETE FROM "${entity.tableName}";`);
 
