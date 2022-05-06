b165912ca62f434c62b98d5038cbacf70c18a789
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-05-06 12:37:57 +0800

    refactor: url validation

--- src/common/links.ts
+++ src/common/links.ts
@@ -2,13 +2,11 @@ export const getDiscussionLink = (postId: string): string =>
   `${process.env.COMMENTS_PREFIX}/posts/${postId}`;
 
 export function isValidHttpUrl(link: string): boolean {
-  let url: URL;
-
   try {
-    url = new URL(link);
+    const url = new URL(link);
+
+    return url.protocol === 'http:' || url.protocol === 'https:';
   } catch (_) {
     return false;
   }
-
-  return url.protocol === 'http:' || url.protocol === 'https:';
 }
