516e19643c14675f325dbc3547ff8b62e456df1d
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-05-20 15:15:45 +0800

    test: accepted submission

--- __tests__/private.ts
+++ __tests__/private.ts
@@ -73,6 +73,33 @@ describe('POST /p/newPost', () => {
     });
   });
 
+  it('should save a new post with the relevant scout id and update submission', async () => {
+    const uuid = randomUUID();
+    await createDefaultUser();
+    await createDefaultSubmission(uuid);
+    const { body } = await request(app.server)
+      .post('/p/newPost')
+      .set('Content-type', 'application/json')
+      .set('authorization', `Service ${process.env.ACCESS_SECRET}`)
+      .send({
+        id: 'p1',
+        title: 'Title',
+        url: 'https://post.com',
+        publicationId: 'a',
+        submissionId: uuid,
+      })
+      .expect(200);
+    const posts = await con.getRepository(Post).find();
+    expect(posts.length).toEqual(1);
+    expect(body).toEqual({ status: 'ok', postId: posts[0].id });
+    expect(posts[0].scoutId).toEqual('1');
+    const submissions = await con.getRepository(Submission).find();
+    const [submission] = submissions;
+    expect(submissions.length).toEqual(1);
+    expect(submission.id).toEqual(uuid);
+    expect(submission.status).toEqual(SubmissionStatus.Accepted);
+  });
+
   it('should handle empty body', async () => {
     const { body } = await request(app.server)
       .post('/p/newPost')
