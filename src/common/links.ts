bd4d66d2f6cff19267f6c55fe1437a3e8fbdb0ed
Chris Bongers <chrisbongers@gmail.com>
2022-05-25 16:02:07 +0200

    feat: standardize urls (#946)
    Up until now we standardises URLs on the app directory.
    As this is an ever-growing list of items we decided the API would be a better place.
    
    For now the only site allowing querystrings is youtube.
    
    WT-58

--- src/common/links.ts
+++ src/common/links.ts
@@ -1,5 +1,26 @@
+const excludeFromStandardization = ['youtube.com'];
+
+const isExcluded = (url: string) =>
+  excludeFromStandardization.some((e) => url.includes(e));
+
+const subtractDomain = (url: string): string => {
+  const matches = url.match(
+    /^(?:https?:\/\/)?(?:[^@/\n]+@)?(?:www\.)?([^:/?\n]+)/i,
+  );
+  return matches && matches[1];
+};
+
 export const getDiscussionLink = (postId: string): string =>
   `${process.env.COMMENTS_PREFIX}/posts/${postId}`;
+
+export const standardizeURL = (url: string): string => {
+  const domain = subtractDomain(url);
+  if (!isExcluded(domain)) {
+    return url.split('?')[0];
+  }
+
+  return url;
+};
 
 export function isValidHttpUrl(link: string): boolean {
   let url: URL;
