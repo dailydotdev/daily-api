c31b44fd7ecc8044cdadaeba7c29b24716c0a4f6
Viktor Poluksht <viktor@daily.dev>
2022-05-29 11:49:30 +0100

    deps: switched to in-house forked ts-ioredis-pool
    Signed-off-by: Viktor Poluksht <viktor@daily.dev>

--- src/redis.ts
+++ src/redis.ts
@@ -1,18 +1,14 @@
 import { RedisPubSub } from 'graphql-redis-subscriptions';
-import Redis, { RedisOptions } from 'ioredis';
-import { IORedisPool, IORedisPoolOptions } from 'ts-ioredis-pool';
+import { IORedisPool, IORedisPoolOptions } from '@dailydotdev/ts-ioredis-pool';
 
-export const redisOptions: RedisOptions = {
+export const redisOptions = {
   host: process.env.REDIS_HOST,
   port: parseInt(process.env.REDIS_PORT, 10),
   password: process.env.REDIS_PASS,
 };
 
-// export const redisClient: Redis.Redis = new Redis(redisOptions);
-
 export const redisPubSub = new RedisPubSub({
-  publisher: new Redis(redisOptions),
-  subscriber: new Redis(redisOptions),
+  connection: redisOptions,
 });
 
 export function deleteKeysByPattern(pattern: string): Promise<void> {
