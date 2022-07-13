6894564c6f5700526c277ef5caefe47f07067ede
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-07-13 18:24:57 +0800

    fix: analytics report email

--- src/workers/sendAnalyticsReportMail.ts
+++ src/workers/sendAnalyticsReportMail.ts
@@ -2,7 +2,7 @@ import { templateId } from './../common/mailing';
 import { differenceInHours } from 'date-fns';
 import { messageToJson, Worker } from './worker';
 import { getAuthorPostStats, Post } from '../entity';
-import { fetchUser, pickImageUrl } from '../common';
+import { fetchUser, pickImageUrl, User } from '../common';
 import {
   baseNotificationEmailData,
   sendEmail,
@@ -19,40 +19,58 @@ const worker: Worker = {
     const data: Data = messageToJson(message);
     try {
       const post = await con.getRepository(Post).findOne(data.postId);
-      if (!post.sentAnalyticsReport && post.authorId) {
-        const user = await fetchUser(post.authorId);
-        const source = await post.source;
-        const stats = await getAuthorPostStats(con, user.id);
-        await sendEmail({
-          ...baseNotificationEmailData,
-          to: user.email,
-          templateId: templateId.analyticsReport,
-          dynamicTemplateData: {
-            first_name: user.name.split(' ')[0],
-            source_image: source.image,
-            post_image: post.image || pickImageUrl(post),
-            post_title: truncatePostToTweet(post),
-            live_hours: differenceInHours(new Date(), post.createdAt),
-            post_views: post.views?.toLocaleString() ?? 0,
-            post_views_total: stats.numPostViews?.toLocaleString() ?? 0,
-            post_upvotes: post.upvotes?.toLocaleString() ?? 0,
-            post_upvotes_total: stats.numPostUpvotes?.toLocaleString() ?? 0,
-            post_comments: post.comments?.toLocaleString() ?? 0,
-            user_reputation: user.reputation?.toLocaleString() ?? 0,
-            profile_image: user.image,
-            full_name: user.name,
-            user_handle: user.username,
-            profile_link: user.permalink,
-          },
-        });
-        logger.info(
-          {
-            data,
-            messageId: message.messageId,
-          },
-          'analytics report sent',
-        );
+      if (!post.authorId && !post.scoutId) {
+        return;
       }
+
+      const requests: Promise<User>[] = [];
+      if (post.authorId) {
+        requests.push(fetchUser(post.authorId));
+      }
+
+      if (post.scoutId) {
+        requests.push(fetchUser(post.scoutId));
+      }
+
+      const users = await Promise.all(requests);
+      const source = await post.source;
+      const stats = await Promise.all(
+        users.map((user) => getAuthorPostStats(con, user.id)),
+      );
+      await Promise.all(
+        users.map((user, i) =>
+          sendEmail({
+            ...baseNotificationEmailData,
+            to: user.email,
+            templateId: templateId.analyticsReport,
+            dynamicTemplateData: {
+              first_name: user.name.split(' ')[0],
+              source_image: source.image,
+              post_image: post.image || pickImageUrl(post),
+              post_title: truncatePostToTweet(post),
+              live_hours: differenceInHours(new Date(), post.createdAt),
+              post_views: post.views?.toLocaleString() ?? 0,
+              post_views_total: stats[i].numPostViews?.toLocaleString() ?? 0,
+              post_upvotes: post.upvotes?.toLocaleString() ?? 0,
+              post_upvotes_total:
+                stats[i].numPostUpvotes?.toLocaleString() ?? 0,
+              post_comments: post.comments?.toLocaleString() ?? 0,
+              user_reputation: user.reputation?.toLocaleString() ?? 0,
+              profile_image: user.image,
+              full_name: user.name,
+              user_handle: user.username,
+              profile_link: user.permalink,
+            },
+          }),
+        ),
+      );
+      logger.info(
+        {
+          data,
+          messageId: message.messageId,
+        },
+        'analytics report sent',
+      );
     } catch (err) {
       logger.error(
         {
