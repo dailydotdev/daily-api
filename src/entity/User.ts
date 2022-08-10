1ed34f4a68c4f033f03f48ef79c181180ef6327c
Chris Bongers <chrisbongers@gmail.com>
2022-08-10 14:33:55 +0200

    feat: optional github handle for the user profile

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -87,7 +87,7 @@ export class User {
 
 export type AddUserData = Pick<
   User,
-  'id' | 'name' | 'image' | 'username' | 'email' | 'createdAt'
+  'id' | 'name' | 'image' | 'username' | 'email' | 'createdAt' | 'github'
 >;
 type AddNewUserResult =
   | { status: 'ok'; userId: string }
@@ -146,6 +146,7 @@ export const addNewUser = async (
         profileConfirmed: true,
         infoConfirmed: true,
         createdAt: data.createdAt,
+        ...(data?.github && { github: data.github }),
       });
 
       logger.info(`Created profile for user with ID: ${data.id}`);
