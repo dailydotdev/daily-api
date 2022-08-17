b4b5888bdca78a6d310a5f4594ba36f0cc3ce415
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-08-17 14:52:11 +0800

    fix: migration

--- src/migration/1660640181400-UniqueSocialHandles.ts
+++ src/migration/1660640181400-UniqueSocialHandles.ts
@@ -5,6 +5,12 @@ export class UniqueSocialHandles1660640181400 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
+      `DROP INDEX "public"."IDX_b67337b7f8aa8406e936c2ff75"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_d7820fad49c99eb918d92519e2"`,
+    );
+    await queryRunner.query(
       `CREATE UNIQUE INDEX "users_username_unique" ON "user" ("username") `,
     );
     await queryRunner.query(
@@ -23,5 +29,11 @@ export class UniqueSocialHandles1660640181400 implements MigrationInterface {
     await queryRunner.query(`DROP INDEX "public"."users_github_unique"`);
     await queryRunner.query(`DROP INDEX "public"."users_twitter_unique"`);
     await queryRunner.query(`DROP INDEX "public"."users_username_unique"`);
+    await queryRunner.query(
+      `CREATE INDEX "IDX_d7820fad49c99eb918d92519e2" ON "user" ("twitter") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_b67337b7f8aa8406e936c2ff75" ON "user" ("username") `,
+    );
   }
 }
