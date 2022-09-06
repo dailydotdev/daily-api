a228b225e6e5d43d09032dcedb8c9ea497b839f1
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-09-06 18:03:29 +0800

    refactor: include name in condition for info confirmed
    Co-authored-by: Ido Shamun <ido@daily.dev>

--- src/schema/users.ts
+++ src/schema/users.ts
@@ -766,7 +766,7 @@ export const resolvers: IResolvers<any, Context> = {
 
       try {
         const updatedUser = { ...user, ...data, image: avatar };
-        if (!user.infoConfirmed && updatedUser.email && updatedUser.username) {
+        if (!user.infoConfirmed && updatedUser.email && updatedUser.username && updatedUser.name) {
           updatedUser.infoConfirmed = true;
         }
         const result = await ctx.con.getRepository(User).save(updatedUser);
