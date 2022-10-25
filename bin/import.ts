6862db4d4c29708733ceeb2a61a2a8050f05943b
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-10-25 21:31:19 +0800

    refactor: removal of importing keyword and only as part of migration (#1057)
    There was a recent introduction in one of the migration files where it would also automatically populate the `Keywords` table. With that, running import command results in an error and requires manual intervention. To make it easier for starting a clean slate, we decided to remove importing the `Keywords` from the command itself and let the migration do the work.

--- bin/import.ts
+++ bin/import.ts
@@ -5,7 +5,7 @@ import { createOrGetConnection } from '../src/db';
 const importEntity = async (con: Connection, name: string): Promise<void> => {
   console.log(`importing ${name}`);
   const entities = JSON.parse(readFileSync(`./seeds/${name}.json`).toString());
-  const repository = await con.getRepository(name);
+  const repository = con.getRepository(name);
   // Batch insert with dirty hack
   for (let i = 0; i < entities.length; i += 1000) {
     await repository.insert(entities.slice(i, i + 1000));
@@ -17,7 +17,6 @@ const start = async (): Promise<void> => {
   await importEntity(con, 'AdvancedSettings');
   await importEntity(con, 'Source');
   await importEntity(con, 'Post');
-  await importEntity(con, 'Keyword');
   await importEntity(con, 'Category');
   await importEntity(con, 'PostKeyword');
 };
