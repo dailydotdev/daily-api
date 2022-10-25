111d6d1d64340bfbd086d73040a4ecf5a77c5130
Lee Hansel Solevilla <sshanzel@yahoo.com>
2022-10-25 17:53:09 +0800

    revert: usage if enum and for each

--- bin/import.ts
+++ bin/import.ts
@@ -2,33 +2,23 @@ import { readFileSync } from 'fs';
 import { Connection } from 'typeorm';
 import { createOrGetConnection } from '../src/db';
 
-enum Entity {
-  AdvancedSettings = 'AdvancedSettings',
-  Source = 'Source',
-  Post = 'Post',
-  Keyword = 'Keyword',
-  Category = 'Category',
-  PostKeyword = 'PostKeyword',
-}
-
-const importEntity = async (con: Connection, name: Entity): Promise<void> => {
+const importEntity = async (con: Connection, name: string): Promise<void> => {
   console.log(`importing ${name}`);
   const entities = JSON.parse(readFileSync(`./seeds/${name}.json`).toString());
   const repository = con.getRepository(name);
   // Batch insert with dirty hack
   for (let i = 0; i < entities.length; i += 1000) {
-    if (name === Entity.Keyword) {
-      await repository.save(entities.slice(i, i + 1000));
-    } else {
-      await repository.insert(entities.slice(i, i + 1000));
-    }
+    await repository.insert(entities.slice(i, i + 1000));
   }
 };
 
 const start = async (): Promise<void> => {
   const con = await createOrGetConnection();
-  const entities = Object.values(Entity);
-  entities.forEach(async (entity) => await importEntity(con, entity));
+  await importEntity(con, 'AdvancedSettings');
+  await importEntity(con, 'Source');
+  await importEntity(con, 'Post');
+  await importEntity(con, 'Category');
+  await importEntity(con, 'PostKeyword');
 };
 
 start()
