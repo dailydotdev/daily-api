8cf87423efd9985ca16bf2f7c382ad8e27da0041
Ido Shamun <idoesh1@gmail.com>
2022-12-12 09:32:30 +0200

    refactor: remove milestones for 2 and 4

--- src/workers/notifications/utils.ts
+++ src/workers/notifications/utils.ts
@@ -38,5 +38,5 @@ export async function articleNewCommentHandler(
 }
 
 export const UPVOTE_MILESTONES = [
-  1, 2, 3, 4, 5, 10, 20, 50, 100, 200, 500, 1000, 1250, 1500, 5000, 7500, 10000,
+  1, 3, 5, 10, 20, 50, 100, 200, 500, 1000, 1250, 1500, 5000, 7500, 10000,
 ];
