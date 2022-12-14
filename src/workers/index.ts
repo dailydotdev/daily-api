213727e8b36f2d116ec25e1b70a70d0b621c69b9
Ido Shamun <idoesh1@gmail.com>
2022-12-14 20:54:27 +0200

    fix: add missing worker to the workers array

--- src/workers/index.ts
+++ src/workers/index.ts
@@ -13,6 +13,7 @@ import postCommentedRedis from './postCommentedRedis';
 import postUpvotedRedis from './postUpvotedRedis';
 import postBannedRep from './postBannedRep';
 import usernameChanged from './usernameChanged';
+import sourceRequestApprovedRep from './sourceRequestApprovedRep';
 import updateComments from './updateComments';
 import cdc from './cdc';
 import updateMailingList from './updateMailingList';
@@ -40,6 +41,7 @@ export const workers: Worker[] = [
   postCommentedRedis,
   postUpvotedRedis,
   postBannedRep,
+  sourceRequestApprovedRep,
   usernameChanged,
   updateComments,
   unreadNotificationCount,
