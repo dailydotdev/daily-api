b33e10437eec6fe775fa3ddeb9559ce8429aa7aa
Ido Shamun <ido@daily.dev>
2023-03-02 16:23:56 +0200

    fix: use the `set-cookie` header to set properly kratos cookie (#1199)
    Instead of patching the expiration of the kratos cookie, we use the
    returned `set-cookie` header. This will make sure there are no
    discrepancies.

--- src/routes/boot.ts
+++ src/routes/boot.ts
@@ -25,7 +25,7 @@ import { getRedisObject, setRedisObject } from '../redis';
 import { REDIS_CHANGELOG_KEY } from '../config';
 import { getSourceLink } from '../common';
 import { AccessToken, signJwt } from '../auth';
-import { cookies, setCookie } from '../cookies';
+import { cookies, setCookie, setRawCookie } from '../cookies';
 import { parse } from 'graphql/language/parser';
 import { execute } from 'graphql/execution/execute';
 import { schema } from '../graphql';
@@ -290,9 +290,9 @@ export const getBootData = async (
 ): Promise<AnonymousBoot | LoggedInBoot> => {
   const whoami = await dispatchWhoami(req);
   if (whoami.valid) {
-    setCookie(req, res, 'kratos', req.cookies[cookies.kratos.key], {
-      expires: whoami.expires,
-    });
+    if (whoami.cookie) {
+      setRawCookie(res, whoami.cookie);
+    }
     if (req.userId !== whoami.userId) {
       req.userId = whoami.userId;
       req.trackingId = req.userId;
