f910f5572616765f4c91f052265553a9b1550884
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-03-10 15:40:27 +0800

    refactor: unnecessary class properties

--- src/entity/posts/ActivePost.ts
+++ src/entity/posts/ActivePost.ts
@@ -1,6 +1,5 @@
 import { ViewColumn, DataSource, ViewEntity } from 'typeorm';
-import { Post, PostOrigin, PostType } from './Post';
-import { Toc } from './ArticlePost';
+import { Post } from './Post';
 
 @ViewEntity({
   expression: (connection: DataSource) =>
@@ -13,118 +12,8 @@ import { Toc } from './ArticlePost';
 })
 export class ActivePost {
   @ViewColumn()
-  id: string;
-
-  @ViewColumn()
-  type: PostType;
-
-  @ViewColumn()
-  title: string;
-
-  @ViewColumn()
-  shortId: string;
-
-  @ViewColumn()
-  createdAt: Date;
-
-  @ViewColumn()
-  metadataChangedAt: Date;
-
-  @ViewColumn()
   sourceId: string;
 
   @ViewColumn()
-  tweeted: boolean;
-
-  @ViewColumn()
-  views: number;
-
-  @ViewColumn()
-  score: number;
-
-  @ViewColumn()
   tagsStr: string;
-
-  @ViewColumn()
-  upvotes: number;
-
-  @ViewColumn()
-  comments: number;
-
-  @ViewColumn()
-  scoutId: string | null;
-
-  @ViewColumn()
-  authorId: string | null;
-
-  @ViewColumn()
-  viewsThreshold: number;
-
-  @ViewColumn()
-  trending?: number;
-
-  @ViewColumn()
-  lastTrending?: Date;
-
-  @ViewColumn()
-  discussionScore?: number;
-
-  @ViewColumn()
-  banned: boolean;
-
-  @ViewColumn()
-  deleted: boolean;
-
-  @ViewColumn()
-  tsv: unknown;
-
-  @ViewColumn()
-  private: boolean;
-
-  @ViewColumn()
-  origin: PostOrigin;
-
-  @ViewColumn()
-  visible: boolean;
-
-  // ArticlePost columns
-  @ViewColumn()
-  publishedAt?: Date;
-
-  @ViewColumn()
-  url: string;
-
-  @ViewColumn()
-  canonicalUrl?: string;
-
-  @ViewColumn()
-  image?: string;
-
-  @ViewColumn()
-  ratio?: number;
-
-  @ViewColumn()
-  placeholder?: string;
-
-  @ViewColumn()
-  siteTwitter?: string;
-
-  @ViewColumn()
-  creatorTwitter?: string;
-
-  @ViewColumn()
-  readTime?: number;
-
-  @ViewColumn()
-  description?: string;
-
-  @ViewColumn()
-  toc?: Toc;
-
-  @ViewColumn()
-  summary?: string;
-
-  // SharedPost columns
-  @ViewColumn()
-  sharedPostId: string;
 }
