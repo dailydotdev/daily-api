bfd7577465ed03d136d7ec60ce71f37538902cf5
capJavert <ante@kickass.website>
2023-03-30 17:13:05 +0200

    feat: add allowMemberPosting column

---
+++ src/migration/1680188261116-AllowMemberPosting.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AllowMemberPosting1680188261116 implements MigrationInterface {
+    name = 'AllowMemberPosting1680188261116'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(`ALTER TABLE "source" ADD "allowMemberPosting" boolean DEFAULT true`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(`ALTER TABLE "source" DROP COLUMN "allowMemberPosting"`);
+    }
+
+}
