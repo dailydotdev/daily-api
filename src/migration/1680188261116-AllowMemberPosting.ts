a7a72f36b05a32a9399843f0361c7fda17b09dc6
capJavert <ante@kickass.website>
2023-03-31 17:29:41 +0200

    fix: not null for column

--- src/migration/1680188261116-AllowMemberPosting.ts
+++ src/migration/1680188261116-AllowMemberPosting.ts
@@ -4,7 +4,7 @@ export class AllowMemberPosting1680188261116 implements MigrationInterface {
     name = 'AllowMemberPosting1680188261116'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.query(`ALTER TABLE "source" ADD "allowMemberPosting" boolean DEFAULT true`);
+      await queryRunner.query(`ALTER TABLE "source" ADD "allowMemberPosting" boolean DEFAULT true NOT NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
