c9699b239e150632d815a490d120594264f209ce
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-04-18 23:38:55 +0800

    fix: post updated worker setting visible property

--- src/workers/postUpdated.ts
+++ src/workers/postUpdated.ts
@@ -81,7 +81,8 @@ const worker: Worker = {
             : data?.extra?.creator_twitter;
 
         const authorId = await findAuthor(entityManager, creatorTwitter);
-        const becomesVisible = !databasePost?.visible && !!data?.title?.length;
+        const title = data?.title || databasePost.title;
+        const becomesVisible = !!title.length;
 
         const { allowedKeywords, mergedKeywords } = await mergeKeywords(
           entityManager,
@@ -110,7 +111,7 @@ const worker: Worker = {
           canonicalUrl: data?.extra?.canonical_url || data?.url,
           image: data?.image,
           sourceId: data?.source_id,
-          title: data?.title && he.decode(data?.title),
+          title: title && he.decode(title),
           readTime: parseReadTime(data?.extra?.read_time),
           publishedAt: data?.published_at && new Date(data?.published_at),
           metadataChangedAt: updatedDate,
