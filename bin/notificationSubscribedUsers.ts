2cb459c668e5fc1601cbcb34c3806bf624372cdc
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-05-04 17:45:28 +0800

    feat: read from csv

--- bin/notificationSubscribedUsers.ts
+++ bin/notificationSubscribedUsers.ts
@@ -1,30 +1,44 @@
 import { UserAction, UserActionType } from '../src/entity';
 import createOrGetConnection from '../src/db';
-
-const subscribedUsers = [];
+import * as fs from 'fs';
+import { parse } from 'csv-parse';
 
 (async () => {
   const con = await createOrGetConnection();
   const existing = await con
     .getRepository(UserAction)
     .findBy({ type: UserActionType.EnableNotification });
+  const subscribed = {};
 
-  await con
-    .createQueryBuilder()
-    .insert()
-    .into(UserAction)
-    .values(
-      subscribedUsers
-        .filter(
-          (subscribedId) =>
-            !existing.some(
-              ({ userId: existingId }) => existingId === subscribedId,
-            ),
-        )
-        .map((userId) => ({
+  console.log('reading csv');
+  const stream = fs
+    .createReadStream('bin/onesignal_subscribed_notifications.csv')
+    .pipe(parse({ delimiter: ',', from_line: 2 }))
+    .on('data', function ([userId]) {
+      if (
+        !userId?.trim() ||
+        existing.some(({ userId: existingId }) => existingId === userId)
+      ) {
+        return;
+      }
+
+      console.log('pushing: ', userId);
+      subscribed[userId] = true;
+    });
+
+  stream.on('end', async () => {
+    await con
+      .createQueryBuilder()
+      .insert()
+      .into(UserAction)
+      .values(
+        Object.keys(subscribed).map((userId) => ({
           userId,
           type: UserActionType.EnableNotification,
         })),
-    )
-    .execute();
+      )
+      .execute();
+
+    console.log('saved to db');
+  });
 })();
