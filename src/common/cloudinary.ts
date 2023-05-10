706ea63891578f64dbfa4c35f9ab0bba0bb83c48
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-05-10 11:51:33 +0800

    refactor: upload post image with preset

--- src/common/cloudinary.ts
+++ src/common/cloudinary.ts
@@ -54,7 +54,34 @@ const avatarPreset = 'avatar';
 export const uploadAvatar = (userId: string, stream: Readable) =>
   uploadFile(`${avatarPreset}_${userId}`, avatarPreset, stream);
 
-const postPreset = 'post';
+const postPreset = 'post_image';
+
+export const uploadPostFile = (
+  name: string,
+  stream: Readable,
+): Promise<string> =>
+  new Promise((resolve, reject) => {
+    const outStream = cloudinary.v2.uploader.upload_stream(
+      {
+        public_id: name,
+        upload_preset: postPreset,
+      },
+      (err, callResult) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(
+          cloudinary.v2.url(callResult.public_id, {
+            secure: true,
+            fetch_format: 'auto',
+            quality: 'auto',
+          }),
+        );
+      },
+    );
+    stream.pipe(outStream);
+  });
 
 export const uploadPostImage = (postId: string, stream: Readable) =>
-  uploadFile(`${postPreset}_${postId}`, postPreset, stream);
+  uploadPostFile(`${postPreset}_${postId}`, stream);
