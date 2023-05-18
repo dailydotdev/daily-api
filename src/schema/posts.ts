15dfaf2a4c3c6dd72afb46b06a5e7d8dd7a3fd09
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-05-18 14:03:04 +0800

    feat: upload image (#1280)
    Upload image for freeform content.

--- src/schema/posts.ts
+++ src/schema/posts.ts
@@ -21,7 +21,8 @@ import {
   notifyView,
   pickImageUrl,
   standardizeURL,
-  uploadPostImage,
+  uploadPostFile,
+  UploadPreset,
 } from '../common';
 import {
   ArticlePost,
@@ -57,6 +58,7 @@ import { Roles } from '../roles';
 import { markdown, saveMentions } from '../common/markdown';
 import { FileUpload } from 'graphql-upload/GraphQLUpload';
 import { insertOrIgnoreAction } from './actions';
+import { generateShortId } from '../ids';
 
 export interface GQLPost {
   id: string;
@@ -559,6 +561,16 @@ export const typeDefs = /* GraphQL */ `
     ): Post! @auth
 
     """
+    Upload an asset from writing a post
+    """
+    uploadContentImage(
+      """
+      Asset to upload to our cloudinary server
+      """
+      image: Upload!
+    ): String! @auth @rateLimit(limit: 5, duration: 60)
+
+    """
     Pin or unpin a post
     """
     updatePinPost(
@@ -900,6 +912,29 @@ export const resolvers: IResolvers<any, Context> = {
       }
       return { _: true };
     },
+    uploadContentImage: async (
+      _,
+      { image }: { image: Promise<FileUpload> },
+    ): Promise<string> => {
+      if (!image) {
+        throw new ValidationError('File is missing!');
+      }
+
+      if (!process.env.CLOUDINARY_URL) {
+        throw new Error('Unable to upload asset to cloudinary!');
+      }
+
+      const upload = await image;
+      const extension = upload.filename?.split('.').pop().toLowerCase();
+      const preset =
+        extension === 'gif'
+          ? UploadPreset.FreeformGif
+          : UploadPreset.FreeformImage;
+      const id = generateShortId();
+      const filename = `post_content_${id}`;
+
+      return uploadPostFile(filename, upload.createReadStream(), preset);
+    },
     deletePost: async (
       _,
       { id }: { id: string },
@@ -1004,9 +1039,10 @@ export const resolvers: IResolvers<any, Context> = {
 
           if (image && process.env.CLOUDINARY_URL) {
             const upload = await image;
-            updated.image = await uploadPostImage(
+            updated.image = await uploadPostFile(
               id,
               upload.createReadStream(),
+              UploadPreset.PostBannerImage,
             );
           }
         }
