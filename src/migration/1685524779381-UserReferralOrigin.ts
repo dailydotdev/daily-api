2932aa49de456303ecf2c075c099379bea8e85fb
capJavert <ante@kickass.website>
2023-06-01 21:39:34 +0200

    feat: set default value for referral origin in migration

--- src/migration/1685524779381-UserReferralOrigin.ts
+++ src/migration/1685524779381-UserReferralOrigin.ts
@@ -6,6 +6,7 @@ export class UserReferralOrigin1685524779381 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "user" ADD "referralOrigin" text`);
         await queryRunner.query(`CREATE INDEX "IDX_user_referral_origin" ON "user" ("referralOrigin") `);
+        await queryRunner.query(`UPDATE "user" SET "referralOrigin" = 'squad' WHERE "referralId" IS NOT NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
