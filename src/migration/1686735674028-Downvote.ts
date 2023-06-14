34697624f3a42d8c2f42c61c830595fc427b009a
capJavert <ante@kickass.website>
2023-06-14 12:50:33 +0200

    feat: set replica identify setting

--- src/migration/1686735674028-Downvote.ts
+++ src/migration/1686735674028-Downvote.ts
@@ -5,6 +5,7 @@ export class Downvote1686735674028 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "downvote" ("postId" text NOT NULL, "userId" character varying(36) NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_bcc60336dec1986306e0306b003" PRIMARY KEY ("postId", "userId"))`);
+        await queryRunner.query(`ALTER TABLE "downvote" REPLICA IDENTITY FULL`);
         await queryRunner.query(`CREATE INDEX "IDX_267ec54f7b32d8236ecef46070" ON "downvote" ("postId") `);
         await queryRunner.query(`CREATE INDEX "IDX_93db2b31185f4555f43b0b63bb" ON "downvote" ("userId") `);
         await queryRunner.query(`ALTER TABLE "downvote" ADD CONSTRAINT "FK_267ec54f7b32d8236ecef46070c" FOREIGN KEY ("postId") REFERENCES "post"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
