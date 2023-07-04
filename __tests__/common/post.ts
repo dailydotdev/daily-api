d3a9c0035095dbd0683b8642460fd08f01e9a0a7
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-07-04 19:09:35 +0800

    fix: lint

--- __tests__/common/post.ts
+++ __tests__/common/post.ts
@@ -1,40 +1,18 @@
-import { FastifyInstance } from 'fastify';
 import { DataSource } from 'typeorm';
-import {
-  GraphQLTestClient,
-  GraphQLTestingState,
-  initializeGraphQLTesting,
-  MockContext,
-  saveFixtures,
-} from '../helpers';
-import { Roles } from '../../src/roles';
+import { saveFixtures } from '../helpers';
 import createOrGetConnection from '../../src/db';
 import { ArticlePost, PostTag, Source, User } from '../../src/entity';
 import { sourcesFixture } from '../fixture/source';
 import { postsFixture, postTagsFixture } from '../fixture/post';
 import { getPostsTinybirdExport } from '../../src/common';
 
-let app: FastifyInstance;
 let con: DataSource;
-let state: GraphQLTestingState;
-let client: GraphQLTestClient;
-let loggedUser: string = null;
-let premiumUser = false;
-let roles: Roles[] = [];
 
 beforeAll(async () => {
   con = await createOrGetConnection();
-  state = await initializeGraphQLTesting(
-    () => new MockContext(con, loggedUser, premiumUser, roles),
-  );
-  client = state.client;
-  app = state.app;
 });
 
 beforeEach(async () => {
-  loggedUser = null;
-  premiumUser = false;
-  roles = [];
   jest.clearAllMocks();
 
   await saveFixtures(con, Source, sourcesFixture);
