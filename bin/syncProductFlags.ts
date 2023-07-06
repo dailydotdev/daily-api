388bcdbeb47022f00e1e732539b5cf77a80935ba
capJavert <ante@kickass.website>
2023-07-07 00:32:14 +0200

    feat: move migration values to script
    feat: adjust migration to correctly set default values
    
    feat: add trigger to make sure all default values are set
    
    when setting some flags during insert

---
+++ bin/syncProductFlags.ts
@@ -0,0 +1,28 @@
+import '../src/config';
+import createOrGetConnection from '../src/db';
+
+(async (): Promise<void> => {
+  const createdAfterArgument = process.argv[2];
+
+  if (!createdAfterArgument) {
+    throw new Error('createdAfterDate argument is required');
+  }
+
+  const createdAfterDate = new Date(createdAfterArgument);
+
+  if (Number.isNaN(createdAfterDate.getTime())) {
+    throw new Error(
+      'createdAfterDate argument is invalid, format should be ISO 6801',
+    );
+  }
+
+  const con = await createOrGetConnection();
+
+  await con.transaction(async (manager) => {
+    await manager.query(
+      `UPDATE post SET flags = flags || jsonb_build_object('sentAnalyticsReport', "sentAnalyticsReport", 'banned', "banned", 'deleted', "deleted", 'private', "private", 'visible', "visible", 'showOnFeed', "showOnFeed") where "createdAt" > '${createdAfterDate.toISOString()}'`,
+    );
+  });
+
+  process.exit();
+})();
