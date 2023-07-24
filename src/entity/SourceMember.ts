1126c5144e2ca5f1880df193deb8b34fb72e6064
capJavert <ante@kickass.website>
2023-07-24 15:05:02 +0200

    feat: add new column

--- src/entity/SourceMember.ts
+++ src/entity/SourceMember.ts
@@ -7,6 +7,10 @@ import { SourceMemberRoles } from '../roles';
 
 const randomBytesAsync = promisify(randomBytes);
 
+export type SourceMemberFlags = Partial<{
+  showPostsOnFeed: boolean;
+}>;
+
 @Entity()
 export class SourceMember {
   @PrimaryColumn({ type: 'text' })
@@ -38,6 +42,10 @@ export class SourceMember {
   @Column({ type: 'text' })
   @Index('IDX_source_member_referralToken', { unique: true })
   referralToken: string;
+
+  @Column({ type: 'jsonb', default: {} })
+  @Index('IDX_source_member_flags_showPostsOnFeed', { synchronize: false })
+  flags: SourceMemberFlags;
 }
 
 const TOKEN_BYTES = 32;
---
+++ src/migration/1690196456312-SourceMemberFlags.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class SourceMemberFlags1690196456312 implements MigrationInterface {
+    name = 'SourceMemberFlags1690196456312'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "source_member" ADD "flags" jsonb NOT NULL DEFAULT '{}'`);
+        await queryRunner.query(`CREATE INDEX "IDX_source_member_flags_showPostsOnFeed" ON source_member USING HASH (((flags->'showPostsOnFeed')::boolean))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX "IDX_source_member_flags_showPostsOnFeed"`);
+        await queryRunner.query(`ALTER TABLE "source_member" DROP COLUMN "flags"`);
+    }
+
+}
