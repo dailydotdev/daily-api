9d9e7bf6e5ff820701a1a42070cf570e5b32217e
Chris Bongers <chrisbongers@gmail.com>
2023-07-25 11:13:24 +0200

    fix: add flags as well

--- src/cron/exportToTinybird.ts
+++ src/cron/exportToTinybird.ts
@@ -18,7 +18,8 @@ export const getPostsTinybirdExport = (con: DataSource, latest: Date) =>
               ("banned" or "deleted" or not "showOnFeed")::int AS "banned", "type" AS "post_type",
               "private"::int      AS "post_private",
               "contentCuration"   AS "content_curation",
-              (SELECT "s"."type" FROM "source" AS "s" WHERE "s"."id" = "sourceId") AS "source_type"
+              (SELECT "s"."type" FROM "source" AS "s" WHERE "s"."id" = "sourceId") AS "source_type",
+              "flags"
        FROM "post"
        WHERE "metadataChangedAt" > $1
          and "sourceId" != '${UNKNOWN_SOURCE}'
