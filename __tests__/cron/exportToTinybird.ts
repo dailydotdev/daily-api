f7e2ca2380699b0d4b9b46a498d532a11240fd61
Mykyta Nikitenko <mykyta@daily.dev>
2023-07-26 15:02:00 +0200

    feat: splitted cron job into testable components

---
+++ __tests__/cron/exportToTinybird.ts
@@ -0,0 +1,118 @@
+import { DataSource } from 'typeorm';
+import { saveFixtures } from '../helpers';
+import createOrGetConnection from '../../src/db';
+import { ArticlePost, PostTag, Source, User } from '../../src/entity';
+import { sourcesFixture } from '../fixture/source';
+import { postsFixture, postTagsFixture } from '../fixture/post';
+import {
+  PostsRepository,
+  TinybirdError,
+  TinybirdDatasourceMode,
+  PostsMetadataRepository,
+  ITinybirdClient,
+  TinybirdPost,
+} from '../../src/cron/exportToTinybird';
+import * as fs from 'fs';
+import * as path from 'path';
+
+let con: DataSource;
+
+beforeAll(async () => {
+  con = await createOrGetConnection();
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+
+  await saveFixtures(con, Source, sourcesFixture);
+  await saveFixtures(con, ArticlePost, postsFixture);
+  await saveFixtures(con, PostTag, postTagsFixture);
+  await con
+    .getRepository(User)
+    .save({ id: '1', name: 'Ido', image: 'https://daily.dev/ido.jpg' });
+  await con.getRepository(User).save({
+    id: '2',
+    name: 'Lee',
+    image: 'https://daily.dev/lee.jpg',
+  });
+});
+
+describe('PostsRepository', () => {
+  it('should return posts to export to tinybird with specific properties', async () => {
+    const now = new Date();
+    const latest = new Date(now.getTime() - 10000);
+    const repo = new PostsRepository(con);
+
+    const posts = await repo.getForTinybirdExport(latest);
+    posts.forEach((post) => {
+      post.created_at = '';
+      post.metadata_changed_at = '';
+    });
+    expect(posts).toMatchSnapshot();
+  });
+});
+
+describe('TinybirdClient', () => {
+  return;
+});
+
+describe('PostsMetadataRepository', () => {
+  it('append', async () => {
+    const expectedCsv = fs
+      .readFileSync(
+        path.resolve(__dirname, './testdata/expected_tinybird_export.csv'),
+      )
+      .toString();
+
+    const expectedDataSource = 'posts_metadata';
+    const tinybirdMock = {
+      postToDatasource: (
+        datasource: string,
+        mode: TinybirdDatasourceMode,
+        csv: string,
+      ): Promise<null | TinybirdError> => {
+        expect(datasource).toEqual(expectedDataSource);
+        expect(mode).toEqual(TinybirdDatasourceMode.APPEND);
+        expect(csv + '\n').toEqual(expectedCsv);
+
+        return null;
+      },
+    } as ITinybirdClient;
+
+    const postsMetadataRepository = new PostsMetadataRepository(
+      tinybirdMock,
+      expectedDataSource,
+    );
+
+    const posts: TinybirdPost[] = [
+      {
+        id: 'id1',
+        author_id: 'author_id1',
+        created_at: 'created_at',
+        metadata_changed_at: 'metadata_changed_at',
+        creator_twitter: 'creator_twitter',
+        source_id: 'source_id',
+        tags_str: 'tags_str',
+        post_type: 'post_type',
+        post_private: 1,
+        content_curation: ['content_curation'],
+        source_type: 'source_type',
+      },
+      {
+        id: 'id2',
+        author_id: 'author_id2',
+        created_at: 'created_at2',
+        metadata_changed_at: 'metadata_changed_at2',
+        creator_twitter: 'creator_twitter2',
+        source_id: 'source_id2',
+        tags_str: 'tags_str2',
+        post_type: 'post_type2',
+        post_private: 1,
+        content_curation: ['content_curation1', 'content_curation2'],
+        source_type: 'source_type2',
+      },
+    ];
+    const result = await postsMetadataRepository.append(posts);
+    expect(result).toBeNull();
+  });
+});
--- src/cron/exportToTinybird.ts
+++ src/cron/exportToTinybird.ts
@@ -1,14 +1,39 @@
 import { Cron } from './cron';
-import fetch from 'node-fetch';
+import fetch, { RequestInit } from 'node-fetch';
 import jsonexport from 'jsonexport';
 import FormData from 'form-data';
 import { DataSource } from 'typeorm';
 import { PostType, UNKNOWN_SOURCE } from '../entity';
 import { promisify } from 'util';
+import { FastifyBaseLogger } from 'fastify';
 
-export const getPostsTinybirdExport = (con: DataSource, latest: Date) =>
-  con.query(
-    `SELECT "id",
+export interface TinybirdPost {
+  id: string;
+  author_id: string | null;
+  created_at: string;
+  metadata_changed_at: string;
+  creator_twitter: string | null;
+  source_id: string;
+  tags_str: string | null;
+  post_type: string;
+  post_private: number;
+  content_curation: string[];
+  source_type: string;
+}
+export interface IPostsRepository {
+  getForTinybirdExport(latest: Date): Promise<TinybirdPost[]>;
+}
+
+export class PostsRepository implements IPostsRepository {
+  private readonly con: DataSource;
+
+  constructor(con: DataSource) {
+    this.con = con;
+  }
+
+  public async getForTinybirdExport(latest: Date): Promise<TinybirdPost[]> {
+    return await this.con.query(
+      `SELECT "id",
               "authorId"          AS "author_id",
               "createdAt"         AS "created_at",
               "metadataChangedAt" AS "metadata_changed_at",
@@ -17,7 +42,7 @@ export const getPostsTinybirdExport = (con: DataSource, latest: Date) =>
               "tagsStr"           AS "tags_str",
               ("banned" or "deleted" or not "showOnFeed")::int AS "banned", "type" AS "post_type",
               "private"::int      AS "post_private",
-              "contentCuration"   AS "content_curation",
+         "contentCuration"   AS "content_curation",
               (SELECT "s"."type" FROM "source" AS "s" WHERE "s"."id" = "sourceId") AS "source_type"
        FROM "post"
        WHERE "metadataChangedAt" > $1
@@ -25,63 +50,300 @@ export const getPostsTinybirdExport = (con: DataSource, latest: Date) =>
          and "visible" = true
          and "type" != '${PostType.Welcome}'
       `,
-    [latest],
-  );
+      [latest],
+    );
+  }
+}
 
 const jsonexportPromise = promisify(jsonexport);
 
-// TODO: need to add tests
+export type fetchfn = (
+  url: RequestInfo,
+  init?: RequestInit,
+) => Promise<Response>;
+
+export enum TinybirdDatasourceMode {
+  APPEND = 'append',
+}
+
+export interface ITinybirdClient {
+  query<T>(query: string): Promise<TinybirdQueryResult<T>>;
+  postToDatasource(
+    datasource: string,
+    mode: TinybirdDatasourceMode,
+    csv: string,
+  ): Promise<null | TinybirdError>;
+}
+
+export class TinybirdError {
+  text: string;
+  status: number;
+}
+
+export class TinybirdQueryResult<T> {
+  error: TinybirdError | null;
+  data: T[] | null;
+  rows: number | null;
+}
+
+export class TinybirdClient implements ITinybirdClient {
+  private readonly accessToken: string;
+  private readonly host: string;
+  private readonly fetch: fetchfn;
+  constructor(accessToken: string, host: string, fetch: fetchfn) {
+    this.accessToken = accessToken;
+    this.host = host;
+    this.fetch = fetch;
+  }
+  public async query<T>(query: string): Promise<TinybirdQueryResult<T>> {
+    const url = `${this.host}/v0/sql`;
+
+    const response = await this.fetch(url, {
+      headers: this.headers(),
+      body: query,
+      method: 'POST',
+    } as RequestInit);
+
+    if (!response.ok) {
+      const text = await response.text();
+      return {
+        error: {
+          text: text,
+          status: response.status,
+        },
+        data: null,
+        rows: 0,
+      };
+    }
+
+    const body = await response.json();
+    return {
+      error: null,
+      data: body.data,
+      rows: body.rows,
+    };
+  }
+
+  public async postToDatasource(
+    datasource: string,
+    mode: TinybirdDatasourceMode,
+    csv: string,
+  ): Promise<null | TinybirdError> {
+    const params = TinybirdClient.queryParams({
+      name: datasource,
+      mode: mode,
+    });
+
+    const url = `${this.host}/v0/datasources?${params}`;
+    const body = new FormData();
+    body.append('csv', csv);
+
+    const response = await this.fetch(url, {
+      method: 'POST',
+      headers: this.headers(),
+      body: body,
+    } as RequestInit);
+
+    if (!response.ok) {
+      const text = await response.text();
+      return {
+        text: text,
+        status: response.status,
+      } as TinybirdError;
+    }
+
+    return null;
+  }
+
+  private headers(): NonNullable<unknown> {
+    return {
+      Authorization: `Bearer ${this.accessToken}`,
+    };
+  }
+
+  private static queryParams(params: NonNullable<unknown>): string {
+    return Object.keys(params)
+      .map(
+        (key) =>
+          encodeURIComponent(key) + '=' + encodeURIComponent(params[key]),
+      )
+      .join('&');
+  }
+}
+
+interface LatestResult {
+  error: TinybirdError | null;
+  latest: Date | null;
+}
+
+export interface IPostsMetadataRepository {
+  latest(): Promise<LatestResult>;
+  append(posts: TinybirdPost[]): Promise<TinybirdError | null>;
+}
+
+export class PostsMetadataRepository implements IPostsMetadataRepository {
+  private readonly tinybirdClient: ITinybirdClient;
+  private readonly datasource: string;
+  private readonly latestQuery: string;
+  constructor(tinybirdClient: ITinybirdClient, datasource: string) {
+    this.tinybirdClient = tinybirdClient;
+    this.datasource = datasource;
+
+    this.latestQuery = `
+      SELECT
+          max(metadata_changed_at) as latest
+      FROM ${datasource}
+      FORMAT JSON
+    `;
+  }
+
+  public async latest(): Promise<LatestResult> {
+    interface latest {
+      latest: string;
+    }
+
+    const result = await this.tinybirdClient.query<latest>(this.latestQuery);
+    if (result.error) {
+      return {
+        error: result.error,
+        latest: null,
+      };
+    }
+
+    if (result.rows === 0) {
+      return {
+        error: null,
+        latest: null,
+      };
+    }
+
+    return {
+      error: null,
+      latest: new Date(result.data[0].latest),
+    };
+  }
+
+  public async append(posts: TinybirdPost[]): Promise<TinybirdError | null> {
+    const csv: string = await jsonexportPromise(posts, {
+      includeHeaders: false,
+      typeHandlers: {
+        Date: (date: Date) => date.toISOString(),
+        Array: (arr: string[]) => {
+          return '[' + arr.map((x) => `'${x}'`).join(', ') + ']';
+        },
+      },
+    });
+
+    return await this.tinybirdClient.postToDatasource(
+      this.datasource,
+      TinybirdDatasourceMode.APPEND,
+      csv,
+    );
+  }
+}
+
+interface ExportResult {
+  count: number | null;
+  error: string | null;
+}
+
+export class TinybirdExportService {
+  private readonly logger: FastifyBaseLogger;
+  private readonly postsRepository: IPostsRepository;
+  private readonly postsMetadataRepository: IPostsMetadataRepository;
+
+  constructor(
+    logger: FastifyBaseLogger,
+    postsRepository: IPostsRepository,
+    postsMetadataRepository: IPostsMetadataRepository,
+  ) {
+    this.logger = logger;
+    this.postsRepository = postsRepository;
+    this.postsMetadataRepository = postsMetadataRepository;
+  }
+
+  public async export(): Promise<ExportResult> {
+    const latestResult = await this.postsMetadataRepository.latest();
+    if (latestResult.error) {
+      return {
+        error: latestResult.error.text,
+      } as ExportResult;
+    }
+
+    if (!latestResult.latest) {
+      return {
+        error: 'latest value is null',
+      } as ExportResult;
+    }
+
+    const postsToExport = await this.postsRepository.getForTinybirdExport(
+      latestResult.latest,
+    );
+
+    if (!postsToExport) {
+      return {
+        count: 0,
+      } as ExportResult;
+    }
+
+    const appendResultError = await this.postsMetadataRepository.append(
+      postsToExport,
+    );
+
+    if (appendResultError) {
+      return {
+        error: appendResultError.text,
+      } as ExportResult;
+    }
+
+    return {
+      count: postsToExport.length,
+    } as ExportResult;
+  }
+
+  public async exportAndLog(): Promise<void> {
+    const result = await this.export();
+    if (result.error) {
+      this.logger.error(
+        { tinybirdResponse: result.error },
+        `failed to replicate posts to tinybird`,
+      );
+      return;
+    }
+
+    if (result.count === 0) {
+      this.logger.info('no posts to replicate');
+      return;
+    }
+
+    this.logger.info(
+      `${result.count} posts replicated successfully to tinybird`,
+    );
+
+    return;
+  }
+}
+
 const cron: Cron = {
   name: 'export-to-tinybird',
   handler: async (con, logger) => {
-    const headers = { Authorization: `Bearer ${process.env.TINYBIRD_TOKEN}` };
-    const latestResponse = await (
-      await fetch(
-        `${process.env.TINYBIRD_HOST}/v0/sql?q=SELECT max(metadata_changed_at) as latest FROM posts_metadata FORMAT JSON`,
-        { headers },
-      )
-    ).json();
-    const latest = new Date(latestResponse.data[0].latest);
-    logger.info(`fetching post changes since ${latest.toISOString()}`);
-    const posts = await getPostsTinybirdExport(con, latest);
-    if (posts.length) {
-      const csv = await jsonexportPromise(posts, {
-        includeHeaders: false,
-        typeHandlers: {
-          Date: (date: Date) => date.toISOString(),
-          Array: (arr: string[]) => {
-            return '[' + arr.map((x) => `'${x}'`).join(', ') + ']';
-          },
-        },
-      });
+    const postsRepository = new PostsRepository(con);
+    const tinybirdClient = new TinybirdClient(
+      process.env.TINYBIRD_TOKEN,
+      process.env.TINYBIRD_HOST,
+      fetch as unknown as fetchfn,
+    );
+    const postsMetadataRepository = new PostsMetadataRepository(
+      tinybirdClient,
+      'posts_metadata',
+    );
+    const exportService = new TinybirdExportService(
+      logger,
+      postsRepository,
+      postsMetadataRepository,
+    );
 
-      const form = new FormData();
-      form.append('csv', csv, 'posts.csv');
-
-      const res = await fetch(
-        `${process.env.TINYBIRD_HOST}/v0/datasources?name=posts_metadata&mode=append`,
-        {
-          method: 'POST',
-          body: form,
-          headers,
-        },
-      );
-      const text = await res.text();
-      if (res.status >= 200 && res.status < 300) {
-        logger.info(
-          { tinybirdResponse: text },
-          `${posts.length} posts replicated successfully to tinybird`,
-        );
-      } else {
-        logger.error(
-          { tinybirdResponse: text },
-          `failed to replicate posts to tinybird`,
-        );
-      }
-    } else {
-      logger.info('no posts to replicate');
-    }
+    await exportService.exportAndLog();
   },
 };
-
 export default cron;
