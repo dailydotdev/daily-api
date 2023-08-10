f5a55e1d35321fe00e02cf3430348416df9e5a87
capJavert <ante@kickass.website>
2023-08-10 14:45:32 +0200

    feat: add user_post table

---
+++ src/entity/UserPost.ts
@@ -0,0 +1,60 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Post } from './posts';
+import { User } from './User';
+
+export type UserPostFlags = Partial<{
+  feedbackDimiss: boolean;
+}>;
+
+export type UserPostFlagsPublic = Pick<UserPostFlags, 'feedbackDimiss'>;
+
+export enum UserPostVote {
+  Up = 1,
+  None = 0,
+  Down = -1,
+}
+
+@Entity()
+@Index(['postId', 'userId'], { unique: true })
+export class UserPost {
+  @PrimaryColumn({ type: 'text' })
+  postId: string;
+
+  @PrimaryColumn({ type: 'text' })
+  userId: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column({ type: 'smallint' })
+  vote: UserPostVote;
+
+  @Column({ default: false })
+  hidden: boolean;
+
+  @Column({ type: 'jsonb', default: {} })
+  flags: UserPostFlags;
+
+  @ManyToOne(() => Post, {
+    lazy: true,
+    onDelete: 'CASCADE',
+  })
+  post: Promise<Post>;
+
+  @ManyToOne(() => User, {
+    lazy: true,
+    onDelete: 'CASCADE',
+  })
+  user: Promise<User>;
+}
---
+++ src/migration/1691668733329-UserPost.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class UserPost1691668733329 implements MigrationInterface {
+    name = 'UserPost1691668733329'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "user_post" ("postId" text NOT NULL, "userId" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "vote" smallint NOT NULL, "hidden" boolean NOT NULL DEFAULT false, "flags" jsonb NOT NULL DEFAULT '{}', CONSTRAINT "PK_45cdc90ca0fd4cf0f8e8026e395" PRIMARY KEY ("postId", "userId"))`);
+        await queryRunner.query(`ALTER TABLE "user_post" REPLICA IDENTITY FULL`);
+        await queryRunner.query(`CREATE UNIQUE INDEX "IDX_45cdc90ca0fd4cf0f8e8026e39" ON "user_post" ("postId", "userId") `);
+        await queryRunner.query(`ALTER TABLE "user_post" ADD CONSTRAINT "FK_3eb8e2db42e1474c4e900b96688" FOREIGN KEY ("postId") REFERENCES "post"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "user_post" ADD CONSTRAINT "FK_61c64496bf096b321869175021a" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user_post" DROP CONSTRAINT "FK_61c64496bf096b321869175021a"`);
+        await queryRunner.query(`ALTER TABLE "user_post" DROP CONSTRAINT "FK_3eb8e2db42e1474c4e900b96688"`);
+        await queryRunner.query(`DROP TABLE "user_post"`);
+    }
+
+}
