fec4ae2a7957eee8a9b6a40faa430e4f1e072c69
capJavert <ante@kickass.website>
2023-08-14 10:18:20 +0200

    feat: optimize trigger to avoid multiple update queries

--- src/migration/1691668733329-UserPost.ts
+++ src/migration/1691668733329-UserPost.ts
@@ -17,16 +17,19 @@ export class UserPost1691668733329 implements MigrationInterface {
             AS
           $$
           BEGIN
-            UPDATE user_post SET "votedAt" = now();
+            NEW."votedAt" = now();
             RETURN NEW;
           END;
           $$
         `)
-        await queryRunner.query('CREATE TRIGGER voted_insert_trigger AFTER INSERT ON "user_post" FOR EACH ROW WHEN (NEW.vote != 0) EXECUTE PROCEDURE voted_at_time()')
-        await queryRunner.query('CREATE TRIGGER voted_update_trigger AFTER UPDATE ON "user_post" FOR EACH ROW WHEN (OLD.vote IS DISTINCT FROM NEW.vote) EXECUTE PROCEDURE voted_at_time()')
+        await queryRunner.query('CREATE TRIGGER voted_insert_trigger BEFORE INSERT ON "user_post" FOR EACH ROW WHEN (NEW.vote != 0) EXECUTE PROCEDURE voted_at_time()')
+        await queryRunner.query('CREATE TRIGGER voted_update_trigger BEFORE UPDATE ON "user_post" FOR EACH ROW WHEN (OLD.vote IS DISTINCT FROM NEW.vote) EXECUTE PROCEDURE voted_at_time()')
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('DROP TRIGGER IF EXISTS voted_update_trigger ON "user_post"')
+        await queryRunner.query('DROP TRIGGER IF EXISTS voted_insert_trigger ON "user_post"')
+        await queryRunner.query('DROP FUNCTION IF EXISTS voted_at_time')
         await queryRunner.query(`ALTER TABLE "user_post" DROP CONSTRAINT "FK_61c64496bf096b321869175021a"`);
         await queryRunner.query(`ALTER TABLE "user_post" DROP CONSTRAINT "FK_3eb8e2db42e1474c4e900b96688"`);
         await queryRunner.query(`DROP TABLE "user_post"`);
