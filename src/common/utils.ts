19a3836c3b026e4f17524de6a0c79ed93932d6a5
Chris Bongers <chrisbongers@gmail.com>
2023-08-15 08:25:05 +0200

    fix: normalize tagStr values (#1393)
    Added normalize function for tag values.
    Also added specific test case to ensure some edge-cases

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,6 +1,8 @@
 import { startOfISOWeek, endOfISOWeek } from 'date-fns';
 import { zonedTimeToUtc } from 'date-fns-tz';
 
+const REMOVE_SPECIAL_CHARACTERS_REGEX = /[^a-zA-Z0-9-_#.]/g;
+
 interface GetTimezonedIsoWeekProps {
   date: Date;
   timezone: string;
@@ -26,3 +28,29 @@ export const updateFlagsStatement = <
 ): (() => string) => {
   return () => `flags || '${JSON.stringify(update)}'`;
 };
+
+/**
+ * Remove special characters from a string
+ * Only allows alphanumeric, -, _, # and . characters
+ *
+ * @param text
+ */
+export const removeSpecialCharacters = (text: string): string => {
+  return text.replace(REMOVE_SPECIAL_CHARACTERS_REGEX, '');
+};
+
+/**
+ * Remove duplicate values from an array
+ *
+ * @param array
+ */
+export const uniqueifyArray = <T>(array: T[]): T[] => {
+  return [...new Set(array)];
+};
+
+/**
+ * Remove empty values from an array
+ *
+ * @param array
+ */
+export const removeEmptyValues = <T>(array: T[]): T[] => array.filter(Boolean);
