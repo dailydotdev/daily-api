a5cd5084fa32bde71aad5d6905318e10fdcfce74
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-08-17 16:55:40 +0800

    Revert "feat: search history integration with magni"
    This reverts commit 9308efff0fdff505118d46ba339e986d146d2015.

--- src/integrations/magni.ts
+++ src/integrations/magni.ts
@@ -1,18 +1,12 @@
 import fetch from 'node-fetch';
-import { fetchOptions } from '../http';
 import { ValidationError } from 'apollo-server-errors';
+import { fetchOptions } from '../http';
 
 export const magniOrigin = process.env.MAGNI_ORIGIN;
 
 export interface SearchResultFeedback {
   chunkId: string;
   value: number;
-}
-
-export interface SearchSessionHistory {
-  id: string;
-  prompt: string;
-  createdAt: string;
 }
 
 export const postFeedback = async (
@@ -30,35 +24,6 @@ export const postFeedback = async (
   });
 
   if (!res.ok) throw new ValidationError(await res.text());
-};
-
-interface SessionResponse {
-  sessions: SearchSessionHistory[];
-}
-
-export const getSessions = async (
-  userId: string,
-  limit = 30,
-  lastId?: string,
-): Promise<SearchSessionHistory[]> => {
-  const params = new URLSearchParams({ limit: limit.toString() });
-
-  if (lastId) params.append('lastId', lastId);
-
-  const res = await fetch(`${magniOrigin}/feedback`, {
-    ...fetchOptions,
-    method: 'get',
-    headers: {
-      'X-User-Id': userId,
-      'Content-Type': 'application/json',
-    },
-  });
-
-  if (!res.ok) throw new ValidationError(await res.text());
-
-  const json: SessionResponse = await res.json();
-
-  return json.sessions;
 };
 
 interface SearchChunkError {
