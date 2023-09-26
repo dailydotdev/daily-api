fa6eb940dcaf6d50a288514d515f4046b288bd9d
capJavert <ante@kickass.website>
2023-09-26 15:42:17 +0200

    feat: add db entity

---
+++ src/entity/UserPersonalizedDigest.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, OneToOne, PrimaryColumn } from 'typeorm';
+import { User } from './';
+import { DayOfWeek } from '../types';
+
+@Entity()
+export class UserPersonalizedDigest {
+  @PrimaryColumn({ type: 'text' })
+  userId: string;
+
+  @Column({ type: 'smallint', default: 9 })
+  preferredHour: number;
+
+  @Column({ type: 'smallint', default: DayOfWeek.Monday })
+  preferredDay = DayOfWeek.Monday;
+
+  @Column({ type: 'text' })
+  preferredTimezone: string;
+
+  @OneToOne(() => User, {
+    lazy: true,
+    onDelete: 'CASCADE',
+  })
+  user: Promise<User>;
+}
