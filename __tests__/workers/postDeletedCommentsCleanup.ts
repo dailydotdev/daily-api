ff31444bb197f893310eee555424b364b7dd5c2f
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2023-10-04 10:35:47 +0200

    test: add test for verifying notifications are deleted

--- __tests__/workers/postDeletedCommentsCleanup.ts
+++ __tests__/workers/postDeletedCommentsCleanup.ts
@@ -1,10 +1,14 @@
 import { expectSuccessfulBackground, saveFixtures } from '../helpers';
 import worker from '../../src/workers/postDeletedCommentsCleanup';
+import notificationWorker from '../../src/workers/notifications/commentDeleted';
 import {
   ArticlePost,
   Comment,
   CommentMention,
   CommentUpvote,
+  Notification,
+  NotificationAttachment,
+  NotificationAvatar,
   Post,
   PostOrigin,
   PostType,
@@ -17,6 +21,7 @@ import { sourcesFixture } from '../fixture/source';
 import { DataSource } from 'typeorm';
 import createOrGetConnection from '../../src/db';
 import { usersFixture } from '../fixture/user';
+import { NotificationType } from '../../src/notifications/common';
 
 let con: DataSource;
 
@@ -101,6 +106,33 @@ beforeEach(async () => {
     commentByUserId: '1',
     mentionedUserId: '2',
   });
+  await con.getRepository(Notification).save({
+    id: '60e66c38-84a3-46cc-a97e-23080a31d410',
+    userId: '1',
+    type: NotificationType.SquadNewComment,
+    title: 'title',
+    referenceId: 'c3',
+    referenceType: 'comment',
+    icon: 'bell',
+    targetUrl: 'http://target.com',
+  });
+  await con.getRepository(NotificationAttachment).save({
+    notificationId: '60e66c38-84a3-46cc-a97e-23080a31d410',
+    order: 0,
+    type: 'post',
+    image: 'http://image.com',
+    title: 'title',
+    referenceId: 'referenceId',
+  });
+  await con.getRepository(NotificationAvatar).save({
+    notificationId: '60e66c38-84a3-46cc-a97e-23080a31d410',
+    order: 0,
+    type: 'source',
+    image: 'http://image.com',
+    targetUrl: 'http://target.com',
+    name: 'test',
+    referenceId: 'referenceId',
+  });
 });
 
 it('should not delete comments if post not found', async () => {
@@ -169,4 +201,28 @@ it('should not delete comment reports if post is deleted', async () => {
 
   expect(comments.length).toEqual(0);
   expect(reports.length).toEqual(1);
+});
+
+it('should delete notifications for the deleted comments', async () => {
+  await con.getRepository(SharePost).update({ id: 'sp1' }, { deleted: true });
+  await expectSuccessfulBackground(worker, {
+    post: {
+      id: 'sp1',
+    },
+  });
+  await expectSuccessfulBackground(notificationWorker, {
+    comment: {
+      id: 'c3',
+    },
+  });
+
+  const comments = await con.getRepository(Comment).find();
+  const upvotes = await con.getRepository(CommentUpvote).find();
+  const mentions = await con.getRepository(CommentMention).find();
+  const notifications = await con.getRepository(Notification).find();
+
+  expect(comments.length).toEqual(1);
+  expect(upvotes.length).toEqual(1);
+  expect(mentions.length).toEqual(1);
+  expect(notifications.length).toEqual(0);
 });
