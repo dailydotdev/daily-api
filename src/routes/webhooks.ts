16cf9ea5ea2a2355ccfd83388b9b8582b5fa8efc
Ido Shamun <idoesh1@gmail.com>
2023-10-05 18:36:55 +0300

    feat: send sendgrid events to analytics

---
+++ src/routes/webhooks.ts
@@ -0,0 +1,101 @@
+import { FastifyInstance, FastifyRequest } from 'fastify';
+import { EventWebhook, EventWebhookHeader } from '@sendgrid/eventwebhook';
+import createOrGetConnection from '../db';
+import { User } from '../entity';
+import { In } from 'typeorm';
+import { sendAnalyticsEvent } from '../integrations/analytics';
+
+type SendgridEvent = {
+  email: string;
+  timestamp: number;
+  'smtp-id': string;
+  event: string;
+  category: string;
+  sg_event_id: string;
+  sg_message_id: string;
+  url?: string;
+  useragent?: string;
+  asm_group_id?: number;
+  type?: string;
+  url_offset?: {
+    index: number;
+  };
+  sg_machine_open?: boolean;
+} & Record<string, unknown>;
+
+const verifyRequest = (publicKey: string, req: FastifyRequest): boolean => {
+  const eventWebhook = new EventWebhook();
+  const ecPublicKey = eventWebhook.convertPublicKeyToECDSA(publicKey);
+
+  const signature = req.headers[
+    EventWebhookHeader.SIGNATURE().toLowerCase()
+  ] as string;
+  const timestamp = req.headers[
+    EventWebhookHeader.TIMESTAMP().toLowerCase()
+  ] as string;
+
+  return eventWebhook.verifySignature(
+    ecPublicKey,
+    req.rawBody,
+    signature,
+    timestamp,
+  );
+};
+
+export default async function (fastify: FastifyInstance): Promise<void> {
+  fastify.post<{ Body: SendgridEvent[] }>('/sendgrid/analytics', {
+    config: {
+      rawBody: true,
+    },
+    handler: async (req, res) => {
+      const body = req.body as SendgridEvent[];
+
+      // Verify the signature provided by SendGrid: https://www.twilio.com/docs/serverless/functions-assets/quickstart/validate-webhook-requests-from-sendgrid
+      const valid = verifyRequest(
+        process.env.SENDGRID_WEBHOOK_ANALYTICS_KEY,
+        req,
+      );
+      if (!valid) {
+        return res.status(204).send();
+      }
+
+      // Look for the users of the emails in the events
+      const emails = Array.from(new Set(body.map((event) => event.email)));
+      const con = await createOrGetConnection();
+      const userIds = await con.getRepository(User).find({
+        select: ['id', 'email'],
+        where: { email: In(emails) },
+      });
+      const userIdsMap = userIds.reduce((acc, user) => {
+        acc[user.email] = user.id;
+        return acc;
+      }, {});
+
+      // Transform events and push to analytics
+      const events = body
+        .map((event) => ({
+          event_id: event.sg_event_id,
+          user_id: userIdsMap[event.email],
+          event_timestamp: new Date(event.timestamp),
+          event_name: `email ${event.event}`,
+          user_agent: event.useragent,
+          app_platform: 'sendgrid',
+          feed_item_target_url: event.url,
+          extra: JSON.stringify({
+            url_offset: event.url_offset?.index,
+            category: event.category,
+            sg_machine_open: event.sg_machine_open,
+            asm_group_id: event.asm_group_id,
+          }),
+        }))
+        .filter((event) => !!event.user_id);
+      await sendAnalyticsEvent(events);
+
+      req.log.info(
+        { count: events.length },
+        'pushed sendgrid events to analytics',
+      );
+      return res.status(204).send();
+    },
+  });
+}
