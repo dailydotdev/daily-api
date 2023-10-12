dd039414db3dd3b9a5e07a9049ed6fcf8cab9a96
Chris Bongers <chrisbongers@gmail.com>
2023-10-12 13:38:01 +0200

    fix: added local ad render function

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -13,6 +13,7 @@ import boot from './boot';
 import users from './users';
 import redirects from './redirects';
 import webhooks from './webhooks';
+import localAds from './localAds';
 
 export default async function (fastify: FastifyInstance): Promise<void> {
   fastify.register(rss, { prefix: '/rss' });
@@ -31,6 +32,10 @@ export default async function (fastify: FastifyInstance): Promise<void> {
   fastify.register(users, { prefix: '/v1/users' });
   fastify.register(webhooks, { prefix: '/webhooks' });
   fastify.register(redirects);
+
+  if (process.env.NODE_ENV === 'development') {
+    fastify.register(localAds);
+  }
 
   fastify.get('/id', (req, res) => {
     return res.status(200).send(req.userId);
