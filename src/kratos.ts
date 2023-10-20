6cac9a90146c9dae4f3ae8855a52a8345dc5d773
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2023-10-20 11:53:10 +0200

    fix: remove span for `dispatchWhoami`, `logout` and `clearAuthentication`

--- src/kratos.ts
+++ src/kratos.ts
@@ -60,23 +60,22 @@ export const clearAuthentication = async (
   req: FastifyRequest,
   res: FastifyReply,
   reason: string,
-): Promise<void> =>
-  runInSpan('clearAuthentication', async () => {
-    req.log.info(
-      {
-        reason,
-        userId: req.userId,
-        cookie: req.cookies[cookies.kratos.key],
-      },
-      'clearing authentication',
-    );
-    req.trackingId = await generateTrackingId();
-    req.userId = undefined;
-    setTrackingId(req, res, req.trackingId);
-    setCookie(req, res, 'auth', undefined);
-    setCookie(req, res, 'kratosContinuity', undefined);
-    setCookie(req, res, 'kratos', undefined);
-  });
+): Promise<void> => {
+  req.log.info(
+    {
+      reason,
+      userId: req.userId,
+      cookie: req.cookies[cookies.kratos.key],
+    },
+    'clearing authentication',
+  );
+  req.trackingId = await generateTrackingId();
+  req.userId = undefined;
+  setTrackingId(req, res, req.trackingId);
+  setCookie(req, res, 'auth', undefined);
+  setCookie(req, res, 'kratosContinuity', undefined);
+  setCookie(req, res, 'kratos', undefined);
+};
 
 type WhoamiResponse =
   | { valid: true; userId: string; expires: Date; cookie?: string }
@@ -84,54 +83,52 @@ type WhoamiResponse =
 
 export const dispatchWhoami = async (
   req: FastifyRequest,
-): Promise<WhoamiResponse> =>
-  runInSpan('dispatchWhoami', async () => {
-    if (heimdallOrigin === 'disabled' || !req.cookies[cookies.kratos.key]) {
-      return { valid: false };
-    }
-    try {
-      const { res: whoami, headers } = await fetchKratos(
-        req,
-        `${heimdallOrigin}/api/whoami`,
-      );
-      if (whoami?.identity?.traits?.userId) {
-        return {
-          valid: true,
-          userId: whoami.identity.traits.userId,
-          expires: new Date(whoami.expires_at),
-          cookie: headers.get('set-cookie'),
-        };
-      }
-      req.log.info({ whoami }, 'invalid whoami response');
-    } catch (e) {
-      if (e.statusCode !== 401) {
-        throw e;
-      }
-    }
-
+): Promise<WhoamiResponse> => {
+  if (heimdallOrigin === 'disabled' || !req.cookies[cookies.kratos.key]) {
     return { valid: false };
-  });
+  }
+  try {
+    const { res: whoami, headers } = await fetchKratos(
+      req,
+      `${heimdallOrigin}/api/whoami`,
+    );
+    if (whoami?.identity?.traits?.userId) {
+      return {
+        valid: true,
+        userId: whoami.identity.traits.userId,
+        expires: new Date(whoami.expires_at),
+        cookie: headers.get('set-cookie'),
+      };
+    }
+    req.log.info({ whoami }, 'invalid whoami response');
+  } catch (e) {
+    if (e.statusCode !== 401) {
+      throw e;
+    }
+  }
+
+  return { valid: false };
+};
 
 export const logout = async (
   req: FastifyRequest,
   res: FastifyReply,
-): Promise<FastifyReply> =>
-  runInSpan('logout', async () => {
-    try {
-      const { res: logoutFlow } = await fetchKratos(
-        req,
-        `${kratosOrigin}/self-service/logout/browser`,
-      );
-      if (logoutFlow?.logout_url) {
-        const logoutParts = logoutFlow.logout_url.split('/self-service/');
-        const logoutUrl = `${kratosOrigin}/self-service/${logoutParts[1]}`;
-        await fetchKratos(req, logoutUrl, { redirect: 'manual' });
-      }
-    } catch (e) {
-      if (e.statusCode !== 303 && e.statusCode !== 401) {
-        throw e;
-      }
+): Promise<FastifyReply> => {
+  try {
+    const { res: logoutFlow } = await fetchKratos(
+      req,
+      `${kratosOrigin}/self-service/logout/browser`,
+    );
+    if (logoutFlow?.logout_url) {
+      const logoutParts = logoutFlow.logout_url.split('/self-service/');
+      const logoutUrl = `${kratosOrigin}/self-service/${logoutParts[1]}`;
+      await fetchKratos(req, logoutUrl, { redirect: 'manual' });
     }
-    await clearAuthentication(req, res, 'manual logout');
-    return res.status(204).send();
-  });
+  } catch (e) {
+    if (e.statusCode !== 303 && e.statusCode !== 401) {
+      throw e;
+    }
+  }
+  await clearAuthentication(req, res, 'manual logout');
+  return res.status(204).send();
+};
