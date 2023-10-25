b15c31e2cc18808995db8823c2352521de9bac1b
capJavert <ante@kickass.website>
2023-10-25 18:55:05 +0200

    feat: remove single column index

--- src/entity/Keyword.ts
+++ src/entity/Keyword.ts
@@ -43,6 +43,5 @@ export class Keyword {
   occurrences: number;
 
   @Column({ type: 'jsonb', default: {} })
-  @Index('IDX_keyword_flags_onboarding', { synchronize: false })
   flags: KeywordFlags = {};
 }
--- src/migration/1698230451545-OnboardingTagsIndex.ts
+++ src/migration/1698230451545-OnboardingTagsIndex.ts
@@ -4,11 +4,13 @@ export class OnboardingTagsIndex1698230451545 implements MigrationInterface {
     name = 'OnboardingTagsIndex1698230451545'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(`DROP INDEX "IDX_keyword_flags_onboarding"`);
       await queryRunner.query(`CREATE INDEX "IDX_keyword_flags_onboarding_value" ON keyword (((flags->'onboarding')::boolean), "value")`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
       await queryRunner.query(`DROP INDEX "IDX_keyword_flags_onboarding_value"`);
+      await queryRunner.query(`CREATE INDEX "IDX_keyword_flags_onboarding" ON post USING HASH (((flags->'onboarding')::boolean))`);
     }
 
 }
