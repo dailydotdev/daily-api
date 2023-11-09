19e5be9d37b57f01140d263d78e5b9b9106f8f5c
Lee Hansel Solevilla <sshanzel@yahoo.com>
2023-11-09 14:37:20 +0800

    feat: insert alerts trigger

---
+++ src/migration/1699508350425-UserAlertsTrigger.ts
@@ -0,0 +1,27 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class UserAlertsTrigger1699508350425 implements MigrationInterface {
+    name = 'UserAlertsTrigger1699508350425'
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+        CREATE OR REPLACE FUNCTION insert_user_alerts()
+          RETURNS TRIGGER
+          LANGUAGE PLPGSQL
+          AS
+        $$
+        BEGIN
+          INSERT INTO public.alerts ("userId") VALUES (NEW."id");
+          RETURN NEW;
+        END;
+        $$
+      `)
+    queryRunner.query('CREATE TRIGGER user_create_trigger AFTER INSERT ON public."user" FOR EACH ROW EXECUTE PROCEDURE insert_user_alerts()')
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query('DROP TRIGGER IF EXISTS user_create_trigger ON public.user')
+    queryRunner.query('DROP FUNCTION IF EXISTS insert_user_alerts')
+  }
+
+}
