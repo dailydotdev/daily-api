d96eb074b8e93c695a5b6a11dab6a46d4fc5f1af
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2023-11-26 12:52:01 +0100

    fix: keep default `group` column value

--- src/migration/1700735236452-YouTubePost.ts
+++ src/migration/1700735236452-YouTubePost.ts
@@ -6,7 +6,6 @@ export class YouTubePost1700735236452 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "post" ADD "video_id" text`);
         await queryRunner.query(`ALTER TABLE "advanced_settings" ADD "group" text NOT NULL DEFAULT 'advanced'`);
-        await queryRunner.query(`ALTER TABLE "advanced_settings" ALTER COLUMN "group" DROP DEFAULT`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
