e7d66368e1ae4fd6a87a8581cfd46b2b34a94289
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2023-11-27 14:13:36 +0100

    fix: column name in migration

--- src/migration/1700735236452-YouTubePost.ts
+++ src/migration/1700735236452-YouTubePost.ts
@@ -4,12 +4,12 @@ export class YouTubePost1700735236452 implements MigrationInterface {
     name = 'YouTubePost1700735236452'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "post" ADD "video_id" text`);
+        await queryRunner.query(`ALTER TABLE "post" ADD "videoId" text`);
         await queryRunner.query(`ALTER TABLE "advanced_settings" ADD "group" text NOT NULL DEFAULT 'advanced'`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "post" DROP COLUMN "video_id"`);
+        await queryRunner.query(`ALTER TABLE "post" DROP COLUMN "videoId"`);
         await queryRunner.query(`ALTER TABLE "advanced_settings" DROP COLUMN "group"`);
     }
 }
