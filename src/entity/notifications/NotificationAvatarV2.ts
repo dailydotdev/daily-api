06c25673c8650b52e152ee4cde09354177472c88
Ido Shamun <ido@daily.dev>
2023-12-08 17:22:24 +0200

    feat: notification v2 schema (#1571)
    Apply schema changes for notification v2 as described here:
    https://dailydotdev.atlassian.net/wiki/spaces/HAN/pages/701267969/WT-1936+Optimize+notifications#Database-Schema
    
    WT-2042 #done

---
+++ src/entity/notifications/NotificationAvatarV2.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
+import { NotificationAvatarType } from './NotificationAvatar';
+
+@Entity()
+@Index('IDX_notification_avatar_v2_type_reference_id', ['type', 'referenceId'])
+export class NotificationAvatarV2 {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'text' })
+  type: NotificationAvatarType;
+
+  @Column({ type: 'text' })
+  name: string;
+
+  @Column({ type: 'text' })
+  image: string;
+
+  @Column({ type: 'text' })
+  targetUrl: string;
+
+  @Column({ type: 'text' })
+  referenceId: string;
+}
