467d094fc9b306b46792f92fb24346ae40ed56c3
Ido Shamun <idoesh1@gmail.com>
2023-12-12 14:25:48 +0200

    feat: notification v2 push worker
    In an effort to reduce the complexity of the PRs, I'm not plugging this one to production messages, but I'd like it to be available in the codebase so I won't have to create one gigantic PR.
    
    This is the implementation of v2 for the push notifications.
    
    WT-2045

--- src/common/streaming.ts
+++ src/common/streaming.ts
@@ -16,3 +16,28 @@ export async function processStream<T>(
   });
   await queue.drained();
 }
+
+export async function processStreamInBatches<T>(
+  stream: Readable,
+  func: (x: T[]) => Promise<void>,
+  concurrency: number,
+  batchSize: number,
+): Promise<void> {
+  let batch: T[] = [];
+  const queue = fastq.promise(func, concurrency);
+  stream.on('data', (row: T) => {
+    batch.push(row);
+    if (batch.length >= batchSize) {
+      queue.push(batch);
+      batch = [];
+    }
+  });
+  await new Promise((resolve, reject) => {
+    stream.on('error', reject);
+    stream.on('end', resolve);
+  });
+  if (batch.length > 0) {
+    await queue.push(batch);
+  }
+  await queue.drained();
+}
