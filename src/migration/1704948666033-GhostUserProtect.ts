3ca44126640297161531fa87bddb759d05eeb678
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-01-11 08:42:11 +0100

    fix: create rules for protecting the ghost user from accidental deletion or modification (#1638)

---
+++ src/migration/1704948666033-GhostUserProtect.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class GhostUserProtect1704948666033 implements MigrationInterface {
+    name = 'GhostUserProtect1704948666033'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(`UPDATE "user" SET "infoConfirmed" = true WHERE "id" = '404';`);
+      await queryRunner.query(`CREATE RULE prototect_ghostuser_deletion AS ON DELETE TO "user" WHERE old.id IN ('404') DO INSTEAD nothing;`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(`UPDATE "user" SET "infoConfirmed" = false WHERE "id" = '404';`);
+      await queryRunner.query(`DROP RULE prototect_ghostuser_deletion on "user";`);
+    }
+}
