02cc4e30250050470b2f1a30b10036b6e0efd407
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-01-11 08:27:39 +0100

    fix: remove update rule

--- src/migration/1704948666033-GhostUserProtect.ts
+++ src/migration/1704948666033-GhostUserProtect.ts
@@ -6,12 +6,10 @@ export class GhostUserProtect1704948666033 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
       await queryRunner.query(`UPDATE "user" SET "infoConfirmed" = true WHERE "id" = '404';`);
       await queryRunner.query(`CREATE RULE prototect_ghostuser_deletion AS ON DELETE TO "user" WHERE old.id IN ('404') DO INSTEAD nothing;`);
-      await queryRunner.query(`CREATE RULE prototect_ghostuser_update AS ON UPDATE TO "user" WHERE old.id IN ('404') DO INSTEAD nothing;`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
       await queryRunner.query(`UPDATE "user" SET "infoConfirmed" = false WHERE "id" = '404';`);
       await queryRunner.query(`DROP RULE prototect_ghostuser_deletion on "user";`);
-      await queryRunner.query(`DROP RULE prototect_ghostuser_update on "user";`);
     }
 }
