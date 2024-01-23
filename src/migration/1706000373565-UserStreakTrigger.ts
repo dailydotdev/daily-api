f15f52da9ada6f06d902af24cf94b49ce1a2696e
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-01-23 17:00:26 +0800

    feat: user streak trigger on user creation

---
+++ src/migration/1706000373565-UserStreakTrigger.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class UserStreakTrigger1706000373565 implements MigrationInterface {
+  name = 'UserStreakTrigger1706000373565';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // Create the trigger function
+    await queryRunner.query(`
+      CREATE OR REPLACE FUNCTION create_user_streak()
+      RETURNS TRIGGER AS $$
+      BEGIN
+          INSERT INTO user_streak ("userId")
+          VALUES (NEW.id);
+          RETURN NEW;
+      END;
+      $$ LANGUAGE plpgsql;
+    `);
+
+    // Create the trigger
+    await queryRunner.query(`
+      CREATE TRIGGER trigger_user_after_insert
+      AFTER INSERT ON public.user
+      FOR EACH ROW
+      EXECUTE FUNCTION create_user_streak();
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // Drop the trigger
+    await queryRunner.query(
+      `DROP TRIGGER IF EXISTS trigger_user_after_insert ON public.user;`,
+    );
+
+    // Drop the trigger function
+    await queryRunner.query(`DROP FUNCTION IF EXISTS create_user_streak;`);
+  }
+}
