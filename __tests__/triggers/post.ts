2c212ced6cbd1c411f0443614acf864ea17acedd
Ido Shamun <idoesh1@gmail.com>
2024-01-27 16:29:32 +0200

    feat: share post tags trigger
    Introduce new triggers to keep SharePost tagsStr up-to-date with the tags of the shared post itself.

---
+++ __tests__/triggers/post.ts
@@ -0,0 +1,40 @@
+import { DataSource } from 'typeorm';
+import createOrGetConnection from '../../src/db';
+import { saveFixtures } from '../helpers';
+import { ArticlePost, Source, SharePost, Post } from '../../src/entity';
+import { sourcesFixture } from '../fixture/source';
+import { postsFixture } from '../fixture/post';
+
+let con: DataSource;
+
+beforeAll(async () => {
+  con = await createOrGetConnection();
+});
+
+beforeEach(async () => {
+  jest.resetAllMocks();
+});
+
+beforeEach(async () => {
+  jest.resetAllMocks();
+  await saveFixtures(con, Source, sourcesFixture);
+  await saveFixtures(con, ArticlePost, postsFixture);
+});
+
+it('should set tags str of shared post on insert and update', async () => {
+  await con.getRepository(SharePost).insert({
+    id: 'sp',
+    shortId: 'sp',
+    title: 'T',
+    sharedPostId: 'p1',
+    sourceId: postsFixture[0].sourceId,
+  });
+  const obj = await con.getRepository(Post).findOneBy({ id: 'sp' });
+  expect(obj.tagsStr).toEqual(postsFixture[0].tagsStr);
+  await con.getRepository(ArticlePost).update({ id: 'p1' }, { tagsStr: 'a,b' });
+  // Make sure only sp gets affected
+  const obj2 = await con
+    .getRepository(Post)
+    .find({ where: { tagsStr: 'a,b' }, order: { id: 'ASC' }, select: ['id'] });
+  expect(obj2.map((x) => x.id)).toEqual(['p1', 'sp']);
+});
