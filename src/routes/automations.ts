6e9aacbc92cade0f923d62d5fe14af359c7f07ab
Ido Shamun <idoesh1@gmail.com>
2024-02-02 16:59:29 +0200

    refactor: add metric

--- src/routes/automations.ts
+++ src/routes/automations.ts
@@ -3,11 +3,16 @@ import { Automation, automations } from '../integrations/automation';
 import { HttpError } from '../integrations/retry';
 
 export default async function (fastify: FastifyInstance): Promise<void> {
+  const counter = fastify.meter?.createCounter('automations', {
+    description: 'How many automations were triggered',
+  });
+
   fastify.post<{ Body: Record<string, unknown>; Params: { name: Automation } }>(
     '/:name',
     async (req, res) => {
       const { body, userId } = req;
-      const auto = automations[req.params.name];
+      const name = req.params.name;
+      const auto = automations[name];
       if (!auto) {
         return res.status(404).send();
       }
@@ -15,6 +20,7 @@ export default async function (fastify: FastifyInstance): Promise<void> {
         return res.status(401).send();
       }
       try {
+        counter?.add(1, { name });
         const autoRes = await auto.run({ ...body, userId });
         return res.status(200).send(autoRes);
       } catch (err) {
