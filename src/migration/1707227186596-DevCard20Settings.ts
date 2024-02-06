5093e88d2cb03acfd6c92b6ab6ab793fef7f2035
Milan Freml <milan@daily.dev>
2024-02-06 14:57:47 +0100

    feat: store devcard 2.0 fields in DB

---
+++ src/migration/1707227186596-DevCard20Settings.ts
@@ -0,0 +1,29 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class DevCard20Settings1707227186596 implements MigrationInterface {
+  name = 'DevCard20Settings1707227186596';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TYPE "public"."dev_card_theme_enum" AS ENUM('default', 'iron', 'bronze', 'silver', 'gold', 'platinum', 'diamond', 'legendary')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "dev_card" ADD "theme" "public"."dev_card_theme_enum" NOT NULL DEFAULT 'default'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "dev_card" ADD "isProfileCover" boolean NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "dev_card" ADD "showBorder" boolean NOT NULL DEFAULT true`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "dev_card" DROP COLUMN "showBorder"`);
+    await queryRunner.query(
+      `ALTER TABLE "dev_card" DROP COLUMN "isProfileCover"`,
+    );
+    await queryRunner.query(`ALTER TABLE "dev_card" DROP COLUMN "theme"`);
+    await queryRunner.query(`DROP TYPE "public"."dev_card_theme_enum"`);
+  }
+}
