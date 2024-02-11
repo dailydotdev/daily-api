f929f55701a97fbe7e6eaa098c29a22a171a65cf
Ido Shamun <ido@daily.dev>
2024-02-11 21:33:01 +0200

    feat: posts sitemap (#1681)

---
+++ __tests__/sitemaps.ts
@@ -0,0 +1,86 @@
+import { FastifyInstance } from 'fastify';
+import appFunc from '../src';
+import request from 'supertest';
+import nock from 'nock';
+import { saveFixtures } from './helpers';
+import { DataSource, DeepPartial } from 'typeorm';
+import createOrGetConnection from '../src/db';
+import { Post, PostType, Source } from '../src/entity';
+import { sourcesFixture } from './fixture/source';
+
+let app: FastifyInstance;
+let con: DataSource;
+
+const now = new Date();
+const postsFixture: DeepPartial<Post>[] = [
+  {
+    id: 'p1',
+    shortId: 'p1',
+    title: 'P1',
+    sourceId: 'a',
+    createdAt: now,
+    type: PostType.Article,
+  },
+  {
+    id: 'p2',
+    shortId: 'p2',
+    title: 'P2',
+    sourceId: 'b',
+    banned: true,
+    createdAt: new Date(now.getTime() - 1000),
+    type: PostType.Article,
+  },
+  {
+    id: 'p3',
+    shortId: 'p3',
+    title: 'P3',
+    sourceId: 'c',
+    private: true,
+    createdAt: new Date(now.getTime() - 2000),
+    type: PostType.Collection,
+  },
+  {
+    id: 'p4',
+    shortId: 'p4',
+    title: 'P4',
+    sourceId: 'a',
+    createdAt: new Date(now.getTime() - 3000),
+    type: PostType.Article,
+  },
+  {
+    id: 'p5',
+    shortId: 'p5',
+    title: 'P5',
+    sourceId: 'b',
+    createdAt: new Date(now.getTime() - 4000),
+    type: PostType.Collection,
+  },
+];
+
+beforeAll(async () => {
+  con = await createOrGetConnection();
+  app = await appFunc();
+  return app.ready();
+});
+
+beforeEach(async () => {
+  nock.cleanAll();
+  await saveFixtures(con, Source, sourcesFixture);
+  await con.getRepository(Post).insert(postsFixture);
+});
+
+afterAll(() => app.close());
+
+describe('GET /sitemaps/posts.txt', () => {
+  it('should return posts ordered by time', async () => {
+    const res = await request(app.server)
+      .get('/sitemaps/posts.txt')
+      .expect(200);
+    expect(res.header['content-type']).toEqual('text/plain');
+    expect(res.header['cache-control']).toBeTruthy();
+    expect(res.text).toEqual(`http://localhost:5002/posts/p1
+http://localhost:5002/posts/p4
+http://localhost:5002/posts/p5
+`);
+  });
+});
