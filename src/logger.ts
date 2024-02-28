4374f952cdd25677f628668d268ade9cc7f0c20d
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-02-28 17:25:43 +0100

    fix: map pino log levels to Google Cloud Logging severity levels

---
+++ src/logger.ts
@@ -0,0 +1,25 @@
+import pino, { LoggerOptions } from 'pino';
+
+// https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#logseverity
+const PinoLevelToSeverityLookup = {
+  trace: 'DEBUG',
+  debug: 'DEBUG',
+  info: 'INFO',
+  warn: 'WARNING',
+  error: 'ERROR',
+  fatal: 'CRITICAL',
+};
+
+export const loggerConfig: LoggerOptions = {
+  level: process.env.LOG_LEVEL || 'info',
+  messageKey: 'message',
+  formatters: {
+    level(label, number) {
+      return {
+        severity:
+          PinoLevelToSeverityLookup[label] || PinoLevelToSeverityLookup['info'],
+        level: number,
+      };
+    },
+  },
+};
