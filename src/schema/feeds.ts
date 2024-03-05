11cf4c94793b70b56548cca1a52ef72208278ad8
Chris Bongers <chrisbongers@gmail.com>
2024-03-05 15:46:25 +0200

    fix: add force refresh arg (#1736)
    We push a `refresh` key from frontend, needs to propagate to feed
    itself.

--- src/schema/feeds.ts
+++ src/schema/feeds.ts
@@ -53,6 +53,7 @@ import {
   versionToFeedGenerator,
 } from '../integrations/feed';
 import { AuthenticationError } from 'apollo-server-errors';
+import { opentelemetry } from '../telemetry/opentelemetry';
 
 interface GQLTagsCategory {
   id: string;
@@ -211,6 +212,11 @@ export const typeDefs = /* GraphQL */ `
       Return only unread posts
       """
       unreadOnly: Boolean = false
+
+      """
+      Force refresh the feed
+      """
+      refresh: Boolean = false
 
       """
       Version of the feed algorithm
@@ -619,6 +625,7 @@ interface AnonymousFeedArgs extends FeedArgs {
 interface ConfiguredFeedArgs extends FeedArgs {
   unreadOnly: boolean;
   version: number;
+  refresh?: boolean;
 }
 
 interface SourceFeedArgs extends FeedArgs {
@@ -881,6 +888,7 @@ const feedResolverCursor: IFieldResolver<
         offset: 0,
         cursor: page.cursor,
         allowed_post_types: args.supportedTypes,
+        ...(args?.refresh && { refresh: true }),
       }),
     warnOnPartialFirstPage: true,
   },
@@ -954,6 +962,12 @@ export const resolvers: IResolvers<any, Context> = traceResolvers({
     },
     feed: (source, args: ConfiguredFeedArgs, ctx: Context, info) => {
       if (args.version >= 2 && args.ranking === Ranking.POPULARITY) {
+        if (args?.refresh) {
+          const meter = opentelemetry.metrics.getMeter('api-bg');
+          const counter = meter.createCounter('force_refresh');
+          counter.add(1);
+        }
+
         return feedResolverCursor(
           source,
           {
