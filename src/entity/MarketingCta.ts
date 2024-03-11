e6c28e717fc33dd2c5124a87452e8d8c989ed659
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-03-11 19:51:12 +0100

    feat: add entities for marketing CTAs

---
+++ src/entity/MarketingCta.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+export type MarketingCtaVariant = 'card' | 'popover';
+
+export type MarketingCtaFlags = Partial<{
+  tagText: string;
+  tagColor: string;
+  ctaUrl: string;
+  ctaText: string;
+  description: string;
+  image: string;
+  title: string;
+}>;
+
+@Entity()
+export class MarketingCta {
+  @PrimaryColumn({ type: 'text' })
+  campaignId?: string;
+
+  @Column({ default: () => 'now()' })
+  createdAt: Date;
+
+  @Column({ type: 'text' })
+  variant?: MarketingCtaVariant;
+
+  @Column({ type: 'jsonb', default: {} })
+  flags: MarketingCtaFlags;
+}
---
+++ src/entity/user/UserMarketingCta.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, ManyToOne, PrimaryColumn } from 'typeorm';
+import { User } from './User';
+import { MarketingCta } from '../MarketingCTA';
+
+@Entity()
+export class UserMarketingCta {
+  @PrimaryColumn({ type: 'text' })
+  marketingCtaId: string;
+
+  @PrimaryColumn({ length: 36 })
+  userId: string;
+
+  @Column({ default: () => 'now()' })
+  createdAt: Date;
+
+  @Column({ nullable: true })
+  readAt: Date;
+
+  @ManyToOne(() => MarketingCta, {
+    lazy: true,
+    onDelete: 'CASCADE',
+  })
+  marketingCta: MarketingCta;
+
+  @ManyToOne(() => User, {
+    lazy: true,
+    onDelete: 'CASCADE',
+  })
+  user: Promise<User>;
+}
