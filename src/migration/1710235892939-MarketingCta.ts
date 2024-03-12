292200aa0d4960fdb97790137e35dd701df5cc32
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-03-12 12:21:19 +0100

    fix: add replica identities

--- src/migration/1710235892939-MarketingCta.ts
+++ src/migration/1710235892939-MarketingCta.ts
@@ -8,9 +8,13 @@ export class MarketingCta1710235892939 implements MigrationInterface {
         await queryRunner.query(`CREATE TABLE "user_marketing_cta" ("marketingCtaId" text NOT NULL, "userId" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "readAt" TIMESTAMP, "marketingCtaCampaignId" text, CONSTRAINT "PK_e4b378e49cd24e16b8cfa3a523d" PRIMARY KEY ("marketingCtaId", "userId"))`);
         await queryRunner.query(`ALTER TABLE "user_marketing_cta" ADD CONSTRAINT "FK_e7a0cce9b2eb428baded9231949" FOREIGN KEY ("marketingCtaCampaignId") REFERENCES "marketing_cta"("campaignId") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "user_marketing_cta" ADD CONSTRAINT "FK_52b027499322daf67dacb8fe368" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "user_marketing_cta" REPLICA IDENTITY FULL`);
+        await queryRunner.query(`ALTER TABLE "marketing_cta" REPLICA IDENTITY FULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "marketing_cta" REPLICA IDENTITY DEFAULT`);
+        await queryRunner.query(`ALTER TABLE "user_marketing_cta" REPLICA IDENTITY DEFAULT`);
         await queryRunner.query(`ALTER TABLE "user_marketing_cta" DROP CONSTRAINT "FK_52b027499322daf67dacb8fe368"`);
         await queryRunner.query(`ALTER TABLE "user_marketing_cta" DROP CONSTRAINT "FK_e7a0cce9b2eb428baded9231949"`);
         await queryRunner.query(`DROP TABLE "user_marketing_cta"`);
