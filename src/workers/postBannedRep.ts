a8681827213e27e23001162d97bb3764a82e1e7a
Ante Barić <ante@kickass.website>
2024-03-12 10:08:45 +0100

    feat: decrease reputation for community picks ban and all post types (#1742)

--- src/workers/postBannedRep.ts
+++ src/workers/postBannedRep.ts
@@ -5,20 +5,18 @@ import {
 } from './../entity/ReputationEvent';
 import { messageToJson, Worker } from './worker';
 import { PostReport } from '../entity/PostReport';
-import { COMMUNITY_PICKS_SOURCE, Post, PostType } from '../entity';
+import { Post } from '../entity';
 import { ChangeObject } from '../types';
 
 interface Data {
   post: ChangeObject<Post>;
 }
 
-const punishableTypes = [PostType.Article];
-
 const worker: Worker = {
   subscription: 'post-banned-rep',
   handler: async (message, con, logger): Promise<void> => {
     const data: Data = messageToJson(message);
-    const { id, authorId, scoutId, type } = data.post;
+    const { id, authorId, scoutId } = data.post;
     try {
       await con.transaction(async (transaction) => {
         const reports = await transaction
@@ -34,32 +32,26 @@ const worker: Worker = {
           }),
         );
 
-        // Skipping penalizing community picks during the beta phase
-        if (
-          data.post.sourceId !== COMMUNITY_PICKS_SOURCE &&
-          punishableTypes.includes(type)
-        ) {
-          const ownerProps = {
-            targetId: id,
-            targetType: ReputationType.Post,
-            reason: ReputationReason.PostBanned,
-          };
+        const ownerProps = {
+          targetId: id,
+          targetType: ReputationType.Post,
+          reason: ReputationReason.PostBanned,
+        };
 
-          if (authorId) {
-            const authorEvent = repo.create({
-              ...ownerProps,
-              grantToId: authorId,
-            });
-            events.push(authorEvent);
-          }
+        if (authorId) {
+          const authorEvent = repo.create({
+            ...ownerProps,
+            grantToId: authorId,
+          });
+          events.push(authorEvent);
+        }
 
-          if (scoutId) {
-            const scoutEvent = repo.create({
-              ...ownerProps,
-              grantToId: scoutId,
-            });
-            events.push(scoutEvent);
-          }
+        if (scoutId) {
+          const scoutEvent = repo.create({
+            ...ownerProps,
+            grantToId: scoutId,
+          });
+          events.push(scoutEvent);
         }
 
         await repo
