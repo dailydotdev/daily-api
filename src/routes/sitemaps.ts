1b94860aaf2ea6eda4d689573529d19ffaaaffe8
Ido Shamun <idoesh1@gmail.com>
2024-03-13 12:48:54 +0200

    feat: tags sitemap
    New sitemap that lists all our tag pages

--- src/routes/sitemaps.ts
+++ src/routes/sitemaps.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from 'fastify';
-import { Post, PostType } from '../entity';
+import { Keyword, Post, PostType } from '../entity';
 import createOrGetConnection from '../db';
 
 export default async function (fastify: FastifyInstance): Promise<void> {
@@ -15,6 +15,28 @@ export default async function (fastify: FastifyInstance): Promise<void> {
       .andWhere('NOT deleted')
       .andWhere('"createdAt" > current_timestamp - interval \'90 day\'')
       .orderBy('"createdAt"', 'DESC')
+      .limit(50_000);
+
+    const input = await query.stream();
+    const stream = input.map((row) => `${row.url}\n`);
+
+    return res
+      .type('text/plain')
+      .header('cache-control', 'public, max-age=14400, s-maxage=14400')
+      .send(stream);
+  });
+
+  fastify.get('/tags.txt', async (req, res) => {
+    const con = await createOrGetConnection();
+    const query = con
+      .createQueryBuilder()
+      .select(
+        `concat('${process.env.COMMENTS_PREFIX}', '/tags/', value)`,
+        'url',
+      )
+      .from(Keyword, 'k')
+      .where('status = :status', { status: 'allow' })
+      .orderBy('value', 'ASC')
       .limit(50_000);
 
     const input = await query.stream();
