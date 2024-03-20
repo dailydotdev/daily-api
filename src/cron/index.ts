2f5758595aaccfdb6e6042391543c0784df7a4c3
Ante Barić <ante@kickass.website>
2024-03-20 13:09:39 +0100

    feat: register daily digest cron (#1757)
    🤦

--- src/cron/index.ts
+++ src/cron/index.ts
@@ -10,6 +10,7 @@ import cleanZombieImages from './cleanZombieImages';
 import personalizedDigest from './personalizedDigest';
 import generateSearchInvites from './generateSearchInvites';
 import checkReferralReminder from './checkReferralReminder';
+import dailyDigest from './dailyDigest';
 
 export const crons: Cron[] = [
   updateViews,
@@ -23,4 +24,5 @@ export const crons: Cron[] = [
   personalizedDigest,
   generateSearchInvites,
   checkReferralReminder,
+  dailyDigest,
 ];
