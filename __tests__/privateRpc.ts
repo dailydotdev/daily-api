eb818bda0df51f6b49a5cdc582b8f41678f15b42
Ante Barić <ante@kickass.website>
2024-04-29 11:03:18 +0200

    feat: add create post endpoint (#1854)

---
+++ __tests__/privateRpc.ts
@@ -0,0 +1,262 @@
+import { saveFixtures } from './helpers';
+import { ArticlePost, Source, SourceType } from '../src/entity';
+import { sourcesFixture } from './fixture/source';
+import { DataSource } from 'typeorm';
+import createOrGetConnection from '../src/db';
+import { PostService } from '@dailydotdev/schema';
+import {
+  CallOptions,
+  Code,
+  ConnectError,
+  createPromiseClient,
+  createRouterTransport,
+} from '@connectrpc/connect';
+import privateRpc from '../src/routes/privateRpc';
+import { baseRpcContext } from '../src/common/connectRpc';
+
+let con: DataSource;
+
+beforeAll(async () => {
+  con = await createOrGetConnection();
+});
+
+beforeEach(async () => {
+  await saveFixtures(con, Source, [
+    ...sourcesFixture,
+    {
+      id: 'collections',
+      name: 'Collections',
+      image: 'http://image.com/collections',
+      handle: 'collections',
+      type: SourceType.Machine,
+    },
+  ]);
+});
+
+const mockTransport = createRouterTransport(privateRpc, {
+  router: {
+    interceptors: [
+      (next) => {
+        return async (req) => {
+          if (
+            req.header.get('Authorization') ===
+            `Service ${process.env.SERVICE_SECRET}`
+          ) {
+            req.contextValues.set(baseRpcContext, {
+              service: true,
+            });
+          }
+
+          return next(req);
+        };
+      },
+    ],
+  },
+});
+const mockClient = createPromiseClient(PostService, mockTransport);
+const defaultClientAuthOptions: CallOptions = {
+  headers: {
+    Authorization: `Service ${process.env.SERVICE_SECRET}`,
+  },
+};
+
+describe('PostService', () => {
+  it('should return not found when not authorized', async () => {
+    baseRpcContext.defaultValue = {
+      service: false,
+    };
+
+    await expect(
+      mockClient.create({
+        url: 'http://example.com/service/1',
+        sourceId: 'a',
+      }),
+    ).rejects.toThrow(
+      new ConnectError('unauthenticated', Code.Unauthenticated),
+    );
+  });
+
+  it('should create post', async () => {
+    const result = await mockClient.create(
+      {
+        url: 'http://example.com/service/1',
+        sourceId: 'a',
+      },
+      defaultClientAuthOptions,
+    );
+
+    expect(result).toEqual({
+      postId: expect.any(String),
+      url: 'http://example.com/service/1',
+    });
+    const post = await con
+      .getRepository(ArticlePost)
+      .findOneBy({ id: result.postId });
+    expect(post).toBeTruthy();
+  });
+
+  it('should return duplicate entry', async () => {
+    const { postId } = await mockClient.create(
+      {
+        url: 'http://example.com/service/1',
+        sourceId: 'a',
+      },
+      defaultClientAuthOptions,
+    );
+    const post = await con.getRepository(ArticlePost).findOneBy({ id: postId });
+    expect(post).toBeTruthy();
+
+    const result = await mockClient.create(
+      {
+        url: 'http://example.com/service/1',
+        sourceId: 'a',
+      },
+      defaultClientAuthOptions,
+    );
+
+    expect(result).toEqual({
+      postId,
+      url: 'http://example.com/service/1',
+    });
+  });
+
+  it('should throw on invalid source', async () => {
+    await expect(
+      mockClient.create(
+        {
+          url: 'http://example.com/service/1',
+          sourceId: 'sourceDoestNotExist',
+        },
+        defaultClientAuthOptions,
+      ),
+    ).rejects.toThrow(new ConnectError('source not found', Code.NotFound));
+  });
+
+  it('should clean url', async () => {
+    const result = await mockClient.create(
+      {
+        url: 'http://example.com/service/1?utm_source=foo',
+        sourceId: 'a',
+      },
+      defaultClientAuthOptions,
+    );
+
+    expect(result).toEqual({
+      postId: expect.any(String),
+      url: 'http://example.com/service/1',
+    });
+    const post = await con
+      .getRepository(ArticlePost)
+      .findOneBy({ id: result.postId });
+    expect(post).toBeTruthy();
+    expect(post!.url).toEqual('http://example.com/service/1');
+  });
+
+  it('should throw on invalid url', async () => {
+    await expect(
+      mockClient.create(
+        {
+          url: 'thisIsNotUrl',
+          sourceId: 'a',
+        },
+        defaultClientAuthOptions,
+      ),
+    ).rejects.toThrow(new ConnectError('invalid url', Code.InvalidArgument));
+  });
+
+  it('should throw on missing url', async () => {
+    await expect(
+      mockClient.create(
+        {
+          sourceId: 'a',
+        },
+        defaultClientAuthOptions,
+      ),
+    ).rejects.toThrow(new ConnectError('invalid url', Code.InvalidArgument));
+  });
+
+  it('should save yggdrasilId', async () => {
+    const result = await mockClient.create(
+      {
+        url: 'http://example.com/service/1',
+        sourceId: 'a',
+        yggdrasilId: 'a7edf0c8-aec7-4586-b411-b1dd431ce8d6',
+      },
+      defaultClientAuthOptions,
+    );
+
+    expect(result).toEqual({
+      postId: expect.any(String),
+      url: 'http://example.com/service/1',
+    });
+    const post = await con
+      .getRepository(ArticlePost)
+      .findOneBy({ id: result.postId });
+    expect(post).toBeTruthy();
+    expect(post!.yggdrasilId).toEqual('a7edf0c8-aec7-4586-b411-b1dd431ce8d6');
+  });
+
+  it('should return duplicate entry per yggdrasilId', async () => {
+    const { postId } = await mockClient.create(
+      {
+        url: 'http://example.com/service/1',
+        sourceId: 'a',
+        yggdrasilId: '95ba892c-d641-4b94-ba47-be03c4c6cc8b',
+      },
+      defaultClientAuthOptions,
+    );
+    const post = await con.getRepository(ArticlePost).findOneBy({ id: postId });
+    expect(post).toBeTruthy();
+
+    const result = await mockClient.create(
+      {
+        url: 'http://example.com/service/123',
+        sourceId: 'a',
+        yggdrasilId: '95ba892c-d641-4b94-ba47-be03c4c6cc8b',
+      },
+      defaultClientAuthOptions,
+    );
+
+    expect(result).toEqual({
+      postId,
+      url: 'http://example.com/service/1',
+    });
+  });
+
+  it('should require yggdrasilId for source collections', async () => {
+    await expect(
+      mockClient.create(
+        {
+          url: 'http://example.com/service/1',
+          sourceId: 'collections',
+        },
+        defaultClientAuthOptions,
+      ),
+    ).rejects.toThrow(
+      new ConnectError(
+        'yggdrasil id required for collections',
+        Code.InvalidArgument,
+      ),
+    );
+  });
+
+  it('should allow source collections without url', async () => {
+    const result = await mockClient.create(
+      {
+        sourceId: 'collections',
+        yggdrasilId: '95ba892c-d641-4b94-ba47-be03c4c6cc8b',
+      },
+      defaultClientAuthOptions,
+    );
+
+    expect(result).toEqual({
+      postId: expect.any(String),
+      url: '',
+    });
+    const post = await con
+      .getRepository(ArticlePost)
+      .findOneBy({ id: result.postId });
+    expect(post).toBeTruthy();
+    expect(post!.yggdrasilId).toEqual('95ba892c-d641-4b94-ba47-be03c4c6cc8b');
+  });
+});
---
+++ src/routes/privateRpc.ts
@@ -0,0 +1,110 @@
+import { Code, ConnectError, ConnectRouter } from '@connectrpc/connect';
+import { TypeOrmError } from '../errors';
+import { ArticlePost } from '../entity';
+import { generateShortId } from '../ids';
+import createOrGetConnection from '../db';
+import { isValidHttpUrl, standardizeURL } from '../common/links';
+import { baseRpcContext } from '../common/connectRpc';
+import {
+  CreatePostRequest,
+  CreatePostResponse,
+  PostService,
+} from '@dailydotdev/schema';
+import { DataSource, FindOptionsWhere } from 'typeorm';
+import { logger } from '../logger';
+
+const getDuplicatePost = async ({
+  req,
+  con,
+}: {
+  req: CreatePostRequest;
+  con: DataSource;
+}): Promise<CreatePostResponse> | never => {
+  try {
+    const existingFindBy: FindOptionsWhere<ArticlePost> = {};
+
+    if (req.yggdrasilId) {
+      existingFindBy.yggdrasilId = req.yggdrasilId;
+    } else {
+      existingFindBy.url = req.url;
+    }
+
+    const existingPost = await con
+      .getRepository(ArticlePost)
+      .findOneByOrFail(existingFindBy);
+
+    return new CreatePostResponse({
+      postId: existingPost.id,
+      url: existingPost.url,
+    });
+  } catch (error) {
+    logger.error({ err: error }, 'error while getting duplicate post');
+
+    throw new ConnectError('internal', Code.Internal);
+  }
+};
+
+const validateCreatePostRequest = (req: CreatePostRequest): never | void => {
+  if (req.sourceId === 'collections') {
+    if (!req.yggdrasilId) {
+      throw new ConnectError(
+        'yggdrasil id required for collections',
+        Code.InvalidArgument,
+      );
+    }
+
+    return;
+  }
+
+  if (!isValidHttpUrl(req.url)) {
+    throw new ConnectError('invalid url', Code.InvalidArgument);
+  }
+};
+
+export default function (router: ConnectRouter) {
+  router.rpc(PostService, PostService.methods.create, async (req, context) => {
+    if (!context.values.get(baseRpcContext).service) {
+      throw new ConnectError('unauthenticated', Code.Unauthenticated);
+    }
+
+    const con = await createOrGetConnection();
+
+    try {
+      req.url = standardizeURL(req.url);
+
+      validateCreatePostRequest(req);
+
+      const postId = await generateShortId();
+      const postEntity = con.getRepository(ArticlePost).create({
+        ...req,
+        id: postId,
+        shortId: postId,
+      });
+      const newPost = await con.getRepository(ArticlePost).insert(postEntity);
+
+      return {
+        postId: newPost.identifiers[0].id,
+        url: req.url,
+      };
+    } catch (error) {
+      logger.error({ err: error }, 'error while creating post');
+
+      if (error instanceof ConnectError) {
+        throw error;
+      }
+
+      if (error?.code === TypeOrmError.DUPLICATE_ENTRY) {
+        return await getDuplicatePost({ req, con });
+      }
+
+      if (
+        error?.code === TypeOrmError.FOREIGN_KEY &&
+        error?.detail?.includes('source')
+      ) {
+        throw new ConnectError('source not found', Code.NotFound);
+      }
+
+      throw new ConnectError(error.message, Code.Internal);
+    }
+  });
+}
