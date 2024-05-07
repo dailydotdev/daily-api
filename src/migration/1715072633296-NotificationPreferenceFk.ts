927440eb7da25a40ac78bb2ffa1da1b9f8de09e3
capJavert <ante@kickass.website>
2024-05-07 11:22:48 +0200

    feat: adjust relation

--- src/migration/1715072633296-NotificationPreferenceFk.ts
+++ src/migration/1715072633296-NotificationPreferenceFk.ts
@@ -7,10 +7,10 @@ export class NotificationPreferenceFk1715072633296 implements MigrationInterface
         await queryRunner.query(`
             DELETE FROM notification_preference WHERE "userId" IN (
                 SELECT "userId" FROM (SELECT DISTINCT "userId"
-                FROM notification_preference np) b LEFT JOIN "user" u ON b."userId" = u.id WHERE id IS NULL
+                FROM notification_preference np) b LEFT JOIN "public"."user" u ON b."userId" = u.id WHERE id IS NULL
             );`
         )
-        await queryRunner.query(`ALTER TABLE "notification_preference" ADD CONSTRAINT "FK_c8721bd56ae600308745ad49744" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "notification_preference" ADD CONSTRAINT "FK_c8721bd56ae600308745ad49744" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
