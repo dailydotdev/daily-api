d8964cb7e35e8e1d41442709e7cabe963125cd1a
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-05-16 14:00:05 +0200

    tests: add tests for webhook

--- __tests__/webhooks.ts
+++ __tests__/webhooks.ts
@@ -1,19 +1,18 @@
 import appFunc from '../src';
 import { FastifyInstance } from 'fastify';
 import { saveFixtures } from './helpers';
-import {
-  MarketingCta,
-  MarketingCtaStatus,
-  Source,
-  User,
-  UserMarketingCta,
-} from '../src/entity';
-import { sourcesFixture } from './fixture/source';
-import { usersFixture } from './fixture/user';
+import { MarketingCta, Source, User, UserMarketingCta } from '../src/entity';
+import { marketingCtaFixture, sourcesFixture, usersFixture } from './fixture';
 import { DataSource } from 'typeorm';
 import createOrGetConnection from '../src/db';
 import request from 'supertest';
 import { createHmac } from 'crypto';
+import {
+  deleteKeysByPattern,
+  getRedisObject,
+  setRedisObject,
+} from '../src/redis';
+import { StorageKey, StorageTopic, generateStorageKey } from '../src/config';
 
 let app: FastifyInstance;
 let con: DataSource;
@@ -28,34 +27,24 @@ afterAll(() => app.close());
 
 beforeEach(async () => {
   jest.resetAllMocks();
+  await deleteKeysByPattern(
+    generateStorageKey(StorageTopic.Boot, StorageKey.MarketingCta, '*'),
+  );
   await saveFixtures(con, Source, sourcesFixture);
   await saveFixtures(con, User, usersFixture);
-  await saveFixtures(con, MarketingCta, [
-    {
-      campaignId: 'worlds-best-campaign',
-      variant: 'card',
-      status: MarketingCtaStatus.Active,
-      createdAt: new Date('2024-03-13 12:00:00'),
-      flags: {
-        title: 'Join the best community in the world',
-        description: 'Join the best community in the world',
-        ctaUrl: 'http://localhost:5002',
-        ctaText: 'Join now',
-      },
-    },
-  ]);
-  await saveFixtures(
-    con,
-    UserMarketingCta,
-    usersFixture.map((user) => ({
-      marketingCtaId: 'worlds-best-campaign',
-      userId: user.id,
-      createdAt: new Date('2024-03-13 12:00:00'),
-    })),
-  );
+  await saveFixtures(con, MarketingCta, marketingCtaFixture);
 });
 
 describe('POST /webhooks/customerio/marketing_cta', () => {
+  const timestamp = Math.floor(Date.now() / 1000);
+  const payload = {
+    userId: '1',
+    marketingCtaId: 'worlds-best-campaign',
+  };
+  const hmac = createHmac('sha256', process.env.CIO_WEBHOOK_SECRET as string);
+  hmac.update(`v0:${timestamp}:${JSON.stringify(payload)}`);
+  const hash = hmac.digest().toString('hex');
+
   describe('webhook signature verification', () => {
     it('should return 403 when no x-cio-timestamp header', async () => {
       const { body } = await request(app.server)
@@ -102,18 +91,6 @@ describe('POST /webhooks/customerio/marketing_cta', () => {
     });
 
     it('should return 200 when signature is valid (dynamic)', async () => {
-      const timestamp = Math.floor(Date.now() / 1000);
-      const payload = {
-        userId: '1',
-        marketingCtaId: 'worlds-best-campaign',
-      };
-      const hmac = createHmac(
-        'sha256',
-        process.env.CIO_WEBHOOK_SECRET as string,
-      );
-      hmac.update(`v0:${timestamp}:${JSON.stringify(payload)}`);
-      const hash = hmac.digest().toString('hex');
-
       const { body } = await request(app.server)
         .post('/webhooks/customerio/marketing_cta')
         .set('x-cio-timestamp', timestamp.toString())
@@ -123,5 +100,70 @@ describe('POST /webhooks/customerio/marketing_cta', () => {
 
       expect(body.success).toEqual(true);
     });
+  });
+
+  it('should return 200 and insert user marketing cta', async () => {
+    const { body } = await request(app.server)
+      .post('/webhooks/customerio/marketing_cta')
+      .set('x-cio-timestamp', timestamp.toString())
+      .set('x-cio-signature', hash)
+      .send(payload)
+      .expect(200);
+
+    expect(body.success).toEqual(true);
+
+    const userMarketingCta = await con
+      .getRepository(UserMarketingCta)
+      .findOneBy({ userId: '1', marketingCtaId: 'worlds-best-campaign' });
+
+    expect(userMarketingCta).toMatchObject({
+      userId: '1',
+      marketingCtaId: 'worlds-best-campaign',
+    });
+
+    expect(
+      JSON.parse(
+        (await getRedisObject(
+          generateStorageKey(StorageTopic.Boot, StorageKey.MarketingCta, '1'),
+        )) as string,
+      ),
+    ).toMatchObject({
+      campaignId: 'worlds-best-campaign',
+      createdAt: '2024-05-13T12:00:00.000Z',
+      variant: 'card',
+      status: 'active',
+      flags: {
+        title: 'Join the best community in the world',
+        ctaUrl: 'http://localhost:5002',
+        ctaText: 'Join now',
+        description: 'Join the best community in the world',
+      },
+    });
+  });
+
+  it('should not change redis cache if existing cache exists', async () => {
+    const redisKey = generateStorageKey(
+      StorageTopic.Boot,
+      StorageKey.MarketingCta,
+      '1',
+    );
+    const { body } = await request(app.server)
+      .post('/webhooks/customerio/marketing_cta')
+      .set('x-cio-timestamp', timestamp.toString())
+      .set('x-cio-signature', hash)
+      .send(payload)
+      .expect(200);
+
+    await setRedisObject(redisKey, 'some value');
+    const userMarketingCta = await con
+      .getRepository(UserMarketingCta)
+      .findOneBy({ userId: '1', marketingCtaId: 'worlds-best-campaign' });
+
+    expect(userMarketingCta).toMatchObject({
+      userId: '1',
+      marketingCtaId: 'worlds-best-campaign',
+    });
+    expect(body.success).toEqual(true);
+    expect(await getRedisObject(redisKey)).toEqual('some value');
   });
 });
