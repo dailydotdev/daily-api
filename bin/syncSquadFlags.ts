d6029d4112d1f30f367d60c1ecf5c425389df8f8
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-05-20 19:44:15 +0800

    chore: script to update flags for squads

---
+++ bin/syncSquadFlags.ts
@@ -0,0 +1,62 @@
+import fastq from 'fastq';
+import '../src/config';
+import createOrGetConnection from '../src/db';
+import { Post, Source, SquadSource } from '../src/entity';
+import { updateFlagsStatement } from '../src/common';
+
+interface Result {
+  sourceId: string;
+  count: number;
+  views: number;
+  upvotes: number;
+}
+
+const QUEUE_CONCURRENCY = 1;
+
+(async () => {
+  const con = await createOrGetConnection();
+  const stream = await con
+    .getRepository(Post)
+    .createQueryBuilder('p')
+    .select('COUNT(p.*)', 'count')
+    .addSelect('SUM(p.views)', 'views')
+    .addSelect('SUM(p.upvotes)', 'upvotes')
+    .addSelect('s.id', 'sourceId')
+    .innerJoin(SquadSource, 's', 's.id = p."sourceId"')
+    .where(`s.type = 'squad'`)
+    .groupBy('s.id')
+    .stream();
+
+  let insertCount = 0;
+  const insertQueue = fastq.promise(
+    async ({ sourceId, views, upvotes, count }: Result) => {
+      console.log('processing stats for: ', sourceId);
+      await con.getRepository(SquadSource).update(
+        { id: sourceId },
+        {
+          flags: updateFlagsStatement<Source>({
+            totalViews: views,
+            totalUpvotes: upvotes,
+            totalPosts: count,
+          }),
+        },
+      );
+      console.log('stats updated for: ', sourceId);
+
+      insertCount += 1;
+    },
+    QUEUE_CONCURRENCY,
+  );
+
+  stream.on('data', (result: Result) => {
+    insertQueue.push(result);
+  });
+
+  await new Promise((resolve, reject) => {
+    stream.on('error', reject);
+    stream.on('end', resolve);
+  });
+  await insertQueue.drained();
+  console.log('update finished with a total of: ', insertCount);
+  process.exit();
+})();
