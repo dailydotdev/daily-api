48da7f0d7a6b6e93ae6fa2166e6434381299fb22
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-05-22 17:25:09 +0800

    refactor: enclose the stream within a transaction

--- bin/syncSquadFlags.ts
+++ bin/syncSquadFlags.ts
@@ -15,48 +15,50 @@ const QUEUE_CONCURRENCY = 1;
 
 (async () => {
   const con = await createOrGetConnection();
-  const stream = await con
-    .getRepository(Post)
-    .createQueryBuilder('p')
-    .select('COUNT(p.*)', 'count')
-    .addSelect('SUM(p.views)', 'views')
-    .addSelect('SUM(p.upvotes)', 'upvotes')
-    .addSelect('s.id', 'sourceId')
-    .innerJoin(SquadSource, 's', 's.id = p."sourceId"')
-    .where(`s.type = 'squad'`)
-    .groupBy('s.id')
-    .stream();
+  await con.transaction(async (manager) => {
+    console.log('initializing stream');
+    const stream = await manager
+      .getRepository(Post)
+      .createQueryBuilder('p')
+      .select('COUNT(p.*)', 'count')
+      .addSelect('SUM(p.views)', 'views')
+      .addSelect('SUM(p.upvotes)', 'upvotes')
+      .addSelect('s.id', 'sourceId')
+      .innerJoin(SquadSource, 's', 's.id = p."sourceId"')
+      .where(`s.type = 'squad'`)
+      .groupBy('s.id')
+      .stream();
 
-  let insertCount = 0;
-  const insertQueue = fastq.promise(
-    async ({ sourceId, views, upvotes, count }: Result) => {
-      console.log('processing stats for: ', sourceId);
-      await con.getRepository(SquadSource).update(
-        { id: sourceId },
-        {
-          flags: updateFlagsStatement<Source>({
-            totalViews: views,
-            totalUpvotes: upvotes,
-            totalPosts: count,
-          }),
-        },
-      );
-      console.log('stats updated for: ', sourceId);
+    let insertCount = 0;
+    const insertQueue = fastq.promise(
+      async ({ sourceId, views, upvotes, count }: Result) => {
+        await manager.getRepository(SquadSource).update(
+          { id: sourceId },
+          {
+            flags: updateFlagsStatement<Source>({
+              totalViews: views,
+              totalUpvotes: upvotes,
+              totalPosts: count,
+            }),
+          },
+        );
 
-      insertCount += 1;
-    },
-    QUEUE_CONCURRENCY,
-  );
+        insertCount += 1;
+      },
+      QUEUE_CONCURRENCY,
+    );
 
-  stream.on('data', (result: Result) => {
-    insertQueue.push(result);
+    stream.on('data', (result: Result) => {
+      insertQueue.push(result);
+    });
+
+    await new Promise((resolve, reject) => {
+      stream.on('error', reject);
+      stream.on('end', resolve);
+    });
+    await insertQueue.drained();
+    console.log('update finished with a total of: ', insertCount);
   });
 
-  await new Promise((resolve, reject) => {
-    stream.on('error', reject);
-    stream.on('end', resolve);
-  });
-  await insertQueue.drained();
-  console.log('update finished with a total of: ', insertCount);
   process.exit();
 })();
