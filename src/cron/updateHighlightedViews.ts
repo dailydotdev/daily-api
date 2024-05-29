7012871f01f02b94c1a5d7acb1b614b9595d9fb6
capJavert <ante@kickass.website>
2024-05-29 23:54:29 +0200

    feat: cron to refresh views

---
+++ src/cron/updateHighlightedViews.ts
@@ -0,0 +1,41 @@
+import { Cron } from './cron';
+import { PopularPost } from '../entity/PopularPost';
+import { PopularSource } from '../entity/PopularSource';
+import { PopularTag } from '../entity/PopularTag';
+import { PopularVideoSource } from '../entity/PopularVideoSource';
+import { TrendingPost } from '../entity/TrendingPost';
+import { TrendingSource } from '../entity/TrendingSource';
+import { TrendingTag } from '../entity/TrendingTag';
+import { PopularVideoPost } from '../entity/PopularVideoPost';
+
+const cron: Cron = {
+  name: 'update-highlighted-views',
+  handler: async (con, logger) => {
+    const viewsToRefresh = [
+      TrendingPost,
+      TrendingSource,
+      TrendingTag,
+      PopularPost,
+      PopularSource,
+      PopularTag,
+      PopularVideoPost,
+      PopularVideoSource,
+    ];
+
+    try {
+      await con.transaction(async (manager) => {
+        for (const viewToRefresh of viewsToRefresh) {
+          await manager.query(
+            `REFRESH MATERIALIZED VIEW ${con.getRepository(viewToRefresh).metadata.tableName}`,
+          );
+        }
+      });
+
+      logger.info({}, 'highlighted views updated');
+    } catch (err) {
+      logger.error({ err }, 'failed to update highlighted views');
+    }
+  },
+};
+
+export default cron;
