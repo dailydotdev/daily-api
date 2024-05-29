254c147eabedc80c6b9d95191f3074c37adc3892
capJavert <ante@kickass.website>
2024-05-29 17:18:31 +0200

    feat: add trending views

---
+++ src/entity/TrendingPost.ts
@@ -0,0 +1,34 @@
+import { DataSource, ViewColumn, ViewEntity } from 'typeorm';
+import { Post } from './posts';
+
+@ViewEntity({
+  materialized: true,
+  expression: (dataSource: DataSource) =>
+    dataSource
+      .createQueryBuilder()
+      .select(`s.id as sourceId`)
+      .addSelect('tagsStr')
+      .addSelect('createdAt')
+      .addSelect(
+        `log(10, upvotes - downvotes) + extract(epoch from ("createdAt" - now() + interval '7 days')) / 200000 r`,
+      )
+      .from(Post, 'p')
+      .where(
+        `not p.private and p."createdAt" > now() - interval '7 day' and upvotes > downvotes`,
+      )
+      .orderBy('r', 'DESC')
+      .limit(100),
+})
+export class TrendingPost {
+  @ViewColumn()
+  sourceId: string;
+
+  @ViewColumn()
+  tagsStr: string;
+
+  @ViewColumn()
+  createdAt: Date;
+
+  @ViewColumn()
+  r: number;
+}
---
+++ src/entity/TrendingSource.ts
@@ -0,0 +1,22 @@
+import { DataSource, ViewColumn, ViewEntity } from 'typeorm';
+import { TrendingPost } from './TrendingPost';
+
+@ViewEntity({
+  materialized: true,
+  expression: (dataSource: DataSource) =>
+    dataSource
+      .createQueryBuilder()
+      .select('sourceId')
+      .addSelect('avg(r) r')
+      .from(TrendingPost, 'tp')
+      .groupBy('sourceId')
+      .having('count(*) > 1')
+      .orderBy('r', 'DESC'),
+})
+export class TrendingSource {
+  @ViewColumn()
+  sourceId: string;
+
+  @ViewColumn()
+  r: number;
+}
