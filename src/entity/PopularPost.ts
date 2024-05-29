5a6d500cbbd7f9b306692c09c5722b83aec7f9ce
capJavert <ante@kickass.website>
2024-05-29 17:18:48 +0200

    feat: add popular base view

---
+++ src/entity/PopularPost.ts
@@ -0,0 +1,36 @@
+import { DataSource, ViewColumn, ViewEntity } from 'typeorm';
+import { Post } from './posts';
+
+@ViewEntity({
+  materialized: true,
+  expression: (dataSource: DataSource) =>
+    dataSource
+      .createQueryBuilder()
+      .select('sourceId')
+      .addSelect('tagsStr')
+      .addSelect('contentCuration')
+      .addSelect('createdAt')
+      .addSelect('upvotes - downvotes r')
+      .from(Post, 'p')
+      .where(
+        'not p.private and p."createdAt" > now() - interval \'60 day\' and upvotes > downvotes',
+      )
+      .orderBy('r', 'DESC')
+      .limit(1000),
+})
+export class PopularPost {
+  @ViewColumn()
+  sourceId: string;
+
+  @ViewColumn()
+  tagsStr: string;
+
+  @ViewColumn()
+  contentCuration: string;
+
+  @ViewColumn()
+  createdAt: Date;
+
+  @ViewColumn()
+  r: number;
+}
