5540b55e64b4eab7d45347a125e3cb8c67ee8528
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-05-29 20:21:31 +0800

    refactor: migration to not include updating users

--- src/migration/1716976350324-AlertFeedSettingsFeedback.ts
+++ src/migration/1716976350324-AlertFeedSettingsFeedback.ts
@@ -9,14 +9,6 @@ export class AlertFeedSettingsFeedback1716976350324
     await queryRunner.query(
       `ALTER TABLE "alerts" ADD "lastFeedSettingsFeedback" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`,
     );
-    await queryRunner.query(
-      `
-        UPDATE  public.alerts AS a
-        SET     "lastFeedSettingsFeedback" = u."createdAt"
-        FROM    public.user AS u
-        WHERE   u.id = a."userId"
-      `,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
