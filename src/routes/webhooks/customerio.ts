3a06b48969602fd6eaf3bed4ea4796cca0b2eca5
Ido Shamun <idoesh1@gmail.com>
2024-05-30 16:52:17 +0300

    feat: send cio events to analytics
    Receive customerio events via webhook and store them to our analytics warehouse

--- src/routes/webhooks/customerio.ts
+++ src/routes/webhooks/customerio.ts
@@ -4,6 +4,7 @@ import createOrGetConnection from '../../db';
 import { UserMarketingCta } from '../../entity';
 import { logger } from '../../logger';
 import { cachePrefillMarketingCta } from '../../common/redisCache';
+import { sendAnalyticsEvent } from '../../integrations/analytics';
 
 const verifyCIOSignature = (
   webhookSigningSecret: string,
@@ -37,62 +38,133 @@ type MarketingCtaPayload = {
   };
 };
 
+type ReportingEvent = {
+  Body: {
+    data: {
+      action_id?: number;
+      broadcast_id?: number;
+      customer_id: string;
+      identifiers: {
+        id: string;
+      };
+      delivery_id: string;
+      recipient?: string;
+    };
+    event_id: string;
+    trigger_event_id?: string;
+    object_type: string;
+    metric: string;
+    timestamp: number;
+  };
+};
+
 export const customerio = async (fastify: FastifyInstance): Promise<void> => {
-  fastify.addHook<MarketingCtaPayload>('preValidation', async (req, res) => {
-    const valid = verifyCIOSignature(process.env.CIO_WEBHOOK_SECRET, req);
-    if (!valid) {
-      return res.status(403).send({ error: 'Invalid signature' });
-    }
-  });
+  fastify.register(
+    async (fastify: FastifyInstance): Promise<void> => {
+      fastify.addHook<MarketingCtaPayload>(
+        'preValidation',
+        async (req, res) => {
+          const valid = verifyCIOSignature(process.env.CIO_WEBHOOK_SECRET, req);
+          if (!valid) {
+            return res.status(403).send({ error: 'Invalid signature' });
+          }
+        },
+      );
 
-  fastify.post<MarketingCtaPayload>('/marketing_cta', {
+      fastify.post<MarketingCtaPayload>('/', {
+        config: {
+          rawBody: true,
+        },
+        handler: async (req, res) => {
+          try {
+            const { userId, marketingCtaId } = req.body;
+
+            const con = await createOrGetConnection();
+            await con.getRepository(UserMarketingCta).upsert(
+              {
+                userId: userId,
+                marketingCtaId: marketingCtaId,
+              },
+              ['userId', 'marketingCtaId'],
+            );
+
+            await cachePrefillMarketingCta(con, userId);
+
+            return res.send({ success: true });
+          } catch (err) {
+            logger.error({ err }, 'Error processing CIO webhook');
+            return res.status(400).send({ success: false });
+          }
+        },
+      });
+
+      fastify.delete<MarketingCtaPayload>('/', {
+        config: {
+          rawBody: true,
+        },
+        handler: async (req, res) => {
+          try {
+            const { userId, marketingCtaId } = req.body;
+
+            const con = await createOrGetConnection();
+            await con.getRepository(UserMarketingCta).delete({
+              userId: userId,
+              marketingCtaId: marketingCtaId,
+            });
+
+            await cachePrefillMarketingCta(con, userId);
+
+            return res.send({ success: true });
+          } catch (err) {
+            logger.error({ err }, 'Error processing CIO webhook');
+            return res.status(400).send({ success: false });
+          }
+        },
+      });
+    },
+    { prefix: '/marketing_cta' },
+  );
+
+  fastify.post<ReportingEvent>('/reporting', {
     config: {
       rawBody: true,
     },
     handler: async (req, res) => {
-      try {
-        const { userId, marketingCtaId } = req.body;
-
-        const con = await createOrGetConnection();
-        await con.getRepository(UserMarketingCta).upsert(
-          {
-            userId: userId,
-            marketingCtaId: marketingCtaId,
-          },
-          ['userId', 'marketingCtaId'],
-        );
-
-        await cachePrefillMarketingCta(con, userId);
-
-        return res.send({ success: true });
-      } catch (err) {
-        logger.error({ err }, 'Error processing CIO webhook');
-        return res.status(400).send({ success: false });
+      const valid = verifyCIOSignature(
+        process.env.CIO_REPORTING_WEBHOOK_SECRET,
+        req,
+      );
+      if (!valid) {
+        req.log.warn('invalid signature');
+        return res.status(403).send({ error: 'Invalid signature' });
       }
-    },
-  });
 
-  fastify.delete<MarketingCtaPayload>('/marketing_cta', {
-    config: {
-      rawBody: true,
-    },
-    handler: async (req, res) => {
-      try {
-        const { userId, marketingCtaId } = req.body;
+      const payload = req.body;
+      const userId = payload.data.identifiers.id;
+      // Delete personal data
+      delete payload.data.identifiers;
+      delete payload.data.recipient;
 
-        const con = await createOrGetConnection();
-        await con.getRepository(UserMarketingCta).delete({
-          userId: userId,
-          marketingCtaId: marketingCtaId,
-        });
-
-        await cachePrefillMarketingCta(con, userId);
-
-        return res.send({ success: true });
-      } catch (err) {
-        logger.error({ err }, 'Error processing CIO webhook');
-        return res.status(400).send({ success: false });
+      const event = {
+        event_timestamp: new Date(payload.timestamp * 1000),
+        event_id: payload.event_id,
+        session_id: payload.event_id,
+        visit_id: payload.event_id,
+        user_id: userId,
+        event_name: `${payload.object_type} ${payload.metric}`,
+        app_platform: 'customerio',
+        extra: JSON.stringify(payload.data),
+      };
+      await sendAnalyticsEvent([event]);
+      if (req.meter) {
+        req.meter
+          .createCounter('cio_events', {
+            description: 'How many customerio events were sent to analytics',
+          })
+          .add(1);
       }
+
+      return res.send({ success: true });
     },
   });
 };
