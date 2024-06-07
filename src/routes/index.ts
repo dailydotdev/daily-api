9933e5c6354be42e2bac2bd06d0df1731dd29159
Chris Bongers <chrisbongers@gmail.com>
2024-06-07 08:07:35 +0200

    chore: temp fixes for FF issues (#1975)
    Temp send FF users to webapp as extension doesn't work
    And add message on old auth system to inform people.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -53,6 +53,17 @@ Allow: /devcards/
 Disallow: /`);
   });
 
+  fastify.get('/v1/auth/authorize', async (req, res) => {
+    return res.type('text/plain').send(
+      `Firefox has recently changed their approval process and in their wisdom have set us back to a 2022 version of the daily.dev extension.
+You can follow the discussion here.
+https://x.com/dailydotdev
+
+In the interim we suggest using the web version.
+https://app.daily.dev`,
+    );
+  });
+
   if (process.env.NODE_ENV === 'development') {
     fastify.register(localAds);
   }
