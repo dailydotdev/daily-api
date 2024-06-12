c5b9dc6f9544e410b76638bee250677e49f08b92
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-06-12 16:24:51 +0200

    feat: add `highestReputation`

--- src/schema/leaderboard.ts
+++ src/schema/leaderboard.ts
@@ -1,11 +1,35 @@
 import { IResolvers } from '@graphql-tools/utils';
 import { Context } from '../Context';
 import { traceResolvers } from './trace';
+import { GQLUser } from './users';
+import { User } from '../entity';
 
 // TODO: Rename this file
 
-export const typeDefs = /* GraphQL */ ``;
+export const typeDefs = /* GraphQL */ `
+  extend type Query {
+    """
+    Get the users with the highest reputation
+    """
+    highestReputation(
+      """
+      Limit the number of users returned
+      """
+      limit: Int
+    ): [User] @cacheControl(maxAge: 600)
+  }
+`;
 
 export const resolvers: IResolvers<unknown, Context> = traceResolvers({
-  Query: {},
+  Query: {
+    highestReputation: async (_, args, ctx): Promise<GQLUser[]> => {
+      return ctx.con
+        .createQueryBuilder()
+        .select('u')
+        .from(User, 'u')
+        .orderBy('u.reputation', 'DESC')
+        .limit(args.limit)
+        .getMany();
+    },
+  },
 });
