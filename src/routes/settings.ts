4b920739764a7a7d0183dab9a5e97dbc86656327
Ante Barić <ante@kickass.website>
2024-06-12 10:50:16 +0200

    feat: add onboarding checklist setting (#1983)

--- src/routes/settings.ts
+++ src/routes/settings.ts
@@ -20,6 +20,7 @@ export default async function (fastify: FastifyInstance): Promise<void> {
         optOutCompanion
         autoDismissNotifications
         campaignCtaPlacement
+        onboardingChecklistView
       }
     }`;
 
