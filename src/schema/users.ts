53bb226e258860f14597b3acd942504b5d2aa531
Chris Bongers <chrisbongers@gmail.com>
2024-06-26 11:48:57 +0200

    fix: add right select

--- src/schema/users.ts
+++ src/schema/users.ts
@@ -821,6 +821,7 @@ const getUserStreakQuery = async (id, ctx, info) => {
       .addSelect(
         `(date_trunc('day', "${builder.alias}"."lastViewAt" at time zone COALESCE(u.timezone, 'utc'))::date) AS "lastViewAtTz"`,
       )
+      .addSelect('u.id', 'userId')
       .addSelect('u.timezone', 'timezone')
       .innerJoin(User, 'u', `"${builder.alias}"."userId" = u.id`)
       .where(`"${builder.alias}"."userId" = :id`, {
