e16b35b2c9153b69f3d80f6f373da6e8d74cc35c
Chris Bongers <chrisbongers@gmail.com>
2024-07-12 15:58:07 +0200

    hotfix: unique select merge

--- src/graphorm/index.ts
+++ src/graphorm/index.ts
@@ -449,7 +449,9 @@ const obj = new GraphORM({
           isMany: false,
           customRelation: (ctx, parentAlias, childAlias, qb): QueryBuilder => {
             return qb
-              .where(`${childAlias}."userId" = :userId`, { userId: ctx.userId })
+              .where(`${childAlias}."userId" = :voteUserId`, {
+                voteUserId: ctx.userId,
+              })
               .andWhere(`${childAlias}."commentId" = "${parentAlias}".id`);
           },
         },
