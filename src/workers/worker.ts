7fcd8d6daedba8fbea3a743b632ae0eee790afa1
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-07-13 12:21:33 +0800

    fix: subscription type

--- src/workers/worker.ts
+++ src/workers/worker.ts
@@ -1,8 +1,9 @@
-import { FastifyLoggerInstance } from 'fastify';
+import { FastifyBaseLogger, FastifyLoggerInstance } from 'fastify';
 import { PubSub } from '@google-cloud/pubsub';
 import { DataSource } from 'typeorm';
 import { PubSubSchema } from '../common/typedPubsub';
 import { ExperimentAllocationClient } from '../growthbook';
+import { NotificationHandlerReturn } from './notifications/worker';
 
 export interface Message {
   messageId: string;
@@ -28,19 +29,39 @@ export interface TypedMessage<T> {
   data: T;
 }
 
-export interface BaseTypedWorker<T> {
+type NotificationWorkerHandler<T> = (
+  data: T,
+  con: DataSource,
+  logger: FastifyBaseLogger,
+) => Promise<NotificationHandlerReturn>;
+
+type BaseTypedWorkerHandler<T> = (
+  data: TypedMessage<T>,
+  con: DataSource,
+  logger: FastifyLoggerInstance,
+  pubsub: PubSub,
+) => Promise<void>;
+
+type WorkerHandlers<T> =
+  | NotificationWorkerHandler<T>
+  | BaseTypedWorkerHandler<T>;
+
+export interface BaseTypedWorker<
+  T,
+  Worker extends WorkerHandlers<T> = BaseTypedWorkerHandler<T>,
+> {
   subscription: string;
-  handler: (
-    data: TypedMessage<T>,
-    con: DataSource,
-    logger: FastifyLoggerInstance,
-    pubsub: PubSub,
-  ) => Promise<void>;
+  handler: Worker;
   maxMessages?: number;
 }
 
 export interface TypedWorker<T extends keyof PubSubSchema>
   extends BaseTypedWorker<PubSubSchema[T]> {}
+
+export interface TypedNotificationWorkerProps<
+  T extends keyof PubSubSchema,
+  D extends PubSubSchema[T] = PubSubSchema[T],
+> extends BaseTypedWorker<D, NotificationWorkerHandler<D>> {}
 
 export interface ExperimentWorker extends Worker {
   subscription: string;
