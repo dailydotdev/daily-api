c852f1eddaf6fbc0a960cf3ba7162376747186f0
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-07-19 00:24:48 +0800

    refactor: store single connection

--- src/queue/client.ts
+++ src/queue/client.ts
@@ -3,10 +3,18 @@ import { Connection as TemporalConnection } from '@temporalio/client/lib/connect
 
 const TEMPORAL_NAMESPACE = process.env.TEMPORAL_NAMESPACE || 'default';
 
+let client: Client;
+
 export const getTemporalClient = async (): Promise<Client> => {
+  if (client) {
+    return client;
+  }
+
   const connection = await TemporalConnection.connect({
     address: 'host.docker.internal:7233',
   });
 
-  return new Client({ connection, namespace: TEMPORAL_NAMESPACE });
+  client = new Client({ connection, namespace: TEMPORAL_NAMESPACE });
+
+  return client;
 };
