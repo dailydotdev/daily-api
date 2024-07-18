3904e08e4923785c503ffaccec62d1086e8eddca
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-07-18 21:21:35 +0800

    chore: usage of namespace

--- src/queue/client.ts
+++ src/queue/client.ts
@@ -1,10 +1,12 @@
 import { Client } from '@temporalio/client';
 import { Connection as TemporalConnection } from '@temporalio/client/lib/connection';
 
+const TEMPORAL_NAMESPACE = process.env.TEMPORAL_NAMESPACE || 'default';
+
 export const getTemporalClient = async (): Promise<Client> => {
   const connection = await TemporalConnection.connect({
     address: 'host.docker.internal:7233',
   });
 
-  return new Client({ connection });
+  return new Client({ connection, namespace: TEMPORAL_NAMESPACE });
 };
