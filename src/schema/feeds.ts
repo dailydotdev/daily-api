171b2191e4e7c8736849275dcfaf7d58a916020b
Luca Pagliaro <luca@sentiero.digital>
2024-07-22 16:12:24 +0200

    fix: remove formatting;

--- src/schema/feeds.ts
+++ src/schema/feeds.ts
@@ -1203,22 +1203,22 @@ const legacySimilarPostsResolver = randomPostsResolver(
     let similarPostsQuery;
     if (tags?.length > 0) {
       similarPostsQuery = `select post.id
-                               from post
-                                      inner join (select count(*)           as similar,
-                                                         min(k.occurrences) as occurrences,
-                                                         pk."postId"
-                                                  from post_keyword pk
-                                                         inner join keyword k on pk.keyword = k.value
-                                                  where k.value in (:...tags)
-                                                    and k.status = 'allow'
-                                                  group by pk."postId") k
-                                                 on k."postId" = post.id
-                               where post.id != :postId
-                                 and post."createdAt" >= now() - interval '6 month'
-                                 and post.visible = true and post.deleted = false
-                               order by (pow(post.upvotes, k.similar) * 1000 /
-                                 k.occurrences) desc
-                                 limit 25`;
+                           from post
+                                  inner join (select count(*)           as similar,
+                                                     min(k.occurrences) as occurrences,
+                                                     pk."postId"
+                                              from post_keyword pk
+                                                     inner join keyword k on pk.keyword = k.value
+                                              where k.value in (:...tags)
+                                                and k.status = 'allow'
+                                              group by pk."postId") k
+                                             on k."postId" = post.id
+                           where post.id != :postId
+                             and post."createdAt" >= now() - interval '6 month'
+                             and post.visible = true and post.deleted = false
+                           order by (pow(post.upvotes, k.similar) * 1000 /
+                             k.occurrences) desc
+                             limit 25`;
     } else {
       similarPostsQuery = `select post.id
                                from post
