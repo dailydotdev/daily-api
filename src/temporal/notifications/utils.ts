6a2f31ff30fc534a5793625c468e8f1759a9783d
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-07-23 20:55:32 +0800

    fix: better logging

--- src/temporal/notifications/utils.ts
+++ src/temporal/notifications/utils.ts
@@ -6,6 +6,7 @@ import {
 } from '../common';
 import { BookmarkReminderParams, bookmarkReminderWorkflow } from './workflows';
 import { getTemporalClient } from '../client';
+import { logger } from '../../logger';
 
 interface ReminderWorkflowParams extends BookmarkReminderParams {
   remindAt: number;
@@ -23,27 +24,35 @@ export const getReminderWorkflowId = ({
   ]);
 
 export const runReminderWorkflow = async (params: ReminderWorkflowParams) => {
-  const workflowId = getReminderWorkflowId(params);
-  const client = await getTemporalClient();
-  const delay = params.remindAt - Date.now();
+  try {
+    const workflowId = getReminderWorkflowId(params);
+    const client = await getTemporalClient();
+    const delay = params.remindAt - Date.now();
 
-  return client.workflow.start(bookmarkReminderWorkflow, {
-    args: [params],
-    workflowId,
-    taskQueue: WorkflowQueue.Notification,
-    startDelay: delay,
-  });
+    return client.workflow.start(bookmarkReminderWorkflow, {
+      args: [params],
+      workflowId,
+      taskQueue: WorkflowQueue.Notification,
+      startDelay: delay,
+    });
+  } catch (error) {
+    logger.info('error running workflow', error);
+  }
 };
 
 export const cancelReminderWorkflow = async (
   params: ReminderWorkflowParams,
 ) => {
-  const client = await getTemporalClient();
-  const workflowId = getReminderWorkflowId(params);
-  const handle = client.workflow.getHandle(workflowId);
-  const description = await handle.describe();
+  try {
+    const client = await getTemporalClient();
+    const workflowId = getReminderWorkflowId(params);
+    const handle = client.workflow.getHandle(workflowId);
+    const description = await handle.describe();
 
-  if (description.status.name === 'RUNNING') {
-    await handle.terminate();
+    if (description.status.name === 'RUNNING') {
+      await handle.terminate();
+    }
+  } catch (error) {
+    logger.info('error cancelling workflow', error);
   }
 };
