ac75b25b99ee3ca3e55236de50ce4dee77470d47
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-07-23 19:26:34 +0800

    fix: git use single connection for worker

--- src/temporal/worker.ts
+++ src/temporal/worker.ts
@@ -1,7 +1,18 @@
 import { NativeConnection } from '@temporalio/worker';
 import { TEMPORAL_ADDRESS } from './config';
 
-export const getTemporalWorkerConnection = () =>
-  NativeConnection.connect({
+let connection: NativeConnection;
+
+export const getTemporalWorkerConnection = async () => {
+  if (connection) {
+    return connection;
+  }
+
+  const con = await NativeConnection.connect({
     address: TEMPORAL_ADDRESS,
   });
+
+  connection = con;
+
+  return con;
+};
