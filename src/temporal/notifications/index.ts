6e1a3cbefad1ba5613d6dee64df8df53bed2ab72
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-07-24 18:54:42 +0800

    revert: namespace for registering workers

--- src/temporal/notifications/index.ts
+++ src/temporal/notifications/index.ts
@@ -3,12 +3,14 @@ import { createActivities } from './activities';
 import createOrGetConnection from '../../db';
 import { WorkflowQueue } from '../common';
 import { getTemporalWorkerConnection } from '../worker';
+import { TEMPORAL_NAMESPACE } from '../config';
 
 export async function run() {
   const connection = await getTemporalWorkerConnection();
   const dbCon = await createOrGetConnection();
   const worker = await Worker.create({
     connection,
+    namespace: TEMPORAL_NAMESPACE,
     workflowsPath: require.resolve('./workflows'),
     taskQueue: WorkflowQueue.Notification,
     activities: createActivities({ con: dbCon }),
