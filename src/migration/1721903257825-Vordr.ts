ead76b335fd5b062d6ee6decb061fffb572c9942
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-07-29 15:54:39 +0200

    feat: add vordr db trigger This sets the vordr flag on all comments for the user, when the vordr flag on the user is changed

--- src/migration/1721903257825-Vordr.ts
+++ src/migration/1721903257825-Vordr.ts
@@ -9,6 +9,25 @@ export class Vordr1721903257825 implements MigrationInterface {
 
     await queryRunner.query(`CREATE INDEX "IDX_user_flags_vordr" ON post USING HASH (((flags->'vordr')::boolean))`);
     await queryRunner.query(`CREATE INDEX "IDX_comment_flags_vordr" ON post USING HASH (((flags->'vordr')::boolean))`);
+
+    await queryRunner.query(`CREATE OR REPLACE FUNCTION comment_flags_update_vordr()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Check if the 'vordr' key in the flags JSONB column has changed
+    IF (NEW.flags ->> 'vordr')::boolean IS DISTINCT FROM (OLD.flags ->> 'vordr')::boolean THEN
+        UPDATE comment
+        SET flags = jsonb_set(flags, '{vordr}', to_jsonb((NEW.flags ->> 'vordr')::boolean))
+        WHERE "userId" = NEW.id;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;`);
+
+    await queryRunner.query(`CREATE TRIGGER user_flags_vordr
+AFTER UPDATE OF flags ON "user"
+FOR EACH ROW
+WHEN ((OLD.flags ->> 'vordr')::boolean IS DISTINCT FROM ((NEW.flags ->> 'vordr'))::boolean)
+EXECUTE FUNCTION comment_flags_update_vordr();`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
@@ -17,5 +36,8 @@ export class Vordr1721903257825 implements MigrationInterface {
 
     await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "flags"`);
     await queryRunner.query(`ALTER TABLE "comment" DROP COLUMN "flags"`);
+
+    await queryRunner.query(`DROP TRIGGER user_flags_vordr ON "user"`);
+    await queryRunner.query(`DROP FUNCTION comment_flags_update_vordr`);
   }
 }
