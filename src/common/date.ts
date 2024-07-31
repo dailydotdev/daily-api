b30129ec25fab2c8a89d5c9ed1ec686a4321ed2b
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-08-01 01:58:14 +0200

    feat: add function for checking if it is weekend

--- src/common/date.ts
+++ src/common/date.ts
@@ -10,3 +10,17 @@ export enum DayOfWeek {
 
 export const DEFAULT_TIMEZONE = 'Etc/UTC';
 export const DEFAULT_WEEK_START = DayOfWeek.Monday;
+
+export const isWeekend = (
+  date: Date | string | number,
+  startOfWeek: DayOfWeek = DEFAULT_WEEK_START,
+): boolean => {
+  const day = new Date(date).getDay();
+  switch (startOfWeek) {
+    case DayOfWeek.Sunday:
+      return day === DayOfWeek.Friday || day === DayOfWeek.Saturday;
+    case DayOfWeek.Monday:
+    default:
+      return day === DayOfWeek.Saturday || day === DayOfWeek.Sunday;
+  }
+};
