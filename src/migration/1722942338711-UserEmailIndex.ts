84b3c77072c9569428b1a6bef004f9288dfef883
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-08-06 13:41:56 +0200

    fix: drop concurrently, only create index if not exists

--- src/migration/1722942338711-UserEmailIndex.ts
+++ src/migration/1722942338711-UserEmailIndex.ts
@@ -2,17 +2,14 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class UserEmailIndex1722942338711 implements MigrationInterface {
   name = 'UserEmailIndex1722942338711'
-  transaction = false
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `CREATE INDEX CONCURRENTLY "IDX_user_loweremail" ON "user" ((lower(email)));`,
-    );
-    await queryRunner.query(`DROP INDEX CONCURRENTLY IF EXISTS "public"."user_email_index";`);
+    await queryRunner.query(`CREATE INDEX IF NOT EXISTS "IDX_user_loweremail" ON "user" ((lower(email)));`);
+    await queryRunner.query(`DROP INDEX IF EXISTS "public"."user_email_index";`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`CREATE INDEX CONCURRENTLY "user_email_index" ON "user" ("email");`);
-    await queryRunner.query(`DROP INDEX CONCURRENTLY IF EXISTS "public"."IDX_user_loweremail";`);
+    await queryRunner.query(`CREATE INDEX IF NOT EXISTS "user_email_index" ON "user" ("email");`);
+    await queryRunner.query(`DROP INDEX IF EXISTS "public"."IDX_user_loweremail";`);
   }
 }
