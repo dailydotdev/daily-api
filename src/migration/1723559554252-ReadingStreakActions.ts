96ccc4c7ed21e57e06a5fb2e6e5817fd7919c06c
Luca Pagliaro <luca@sentiero.digital>
2024-08-13 16:33:07 +0200

    feat: add migration file

---
+++ src/migration/1723559554252-ReadingStreakActions.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class ReadingStreakActions1723559554252 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
