e74b4e51110062566c2b6cadc3b96c7783960254
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-08-26 15:00:48 +0200

    fix: don't check for null company

--- src/cron/cleanZombieUserCompany.ts
+++ src/cron/cleanZombieUserCompany.ts
@@ -1,5 +1,5 @@
 import { Cron } from './cron';
-import { IsNull, LessThan } from 'typeorm';
+import { LessThan } from 'typeorm';
 import { subHours } from 'date-fns';
 import { UserCompany } from '../entity/UserCompany';
 
@@ -10,7 +10,6 @@ export const cleanZombieUserCompany: Cron = {
     const timeThreshold = subHours(new Date(), 1);
     const { affected } = await con.getRepository(UserCompany).delete({
       verified: false,
-      companyId: IsNull(),
       updatedAt: LessThan(timeThreshold),
     });
     logger.info({ count: affected }, 'zombies user companies cleaned! 🧟');
