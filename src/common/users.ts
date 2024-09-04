f1168a20dcc9f64f1505da3797c2d15770a13c3e
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-09-04 17:01:09 +0800

    refactor: usage of date-fns

--- src/common/users.ts
+++ src/common/users.ts
@@ -6,7 +6,7 @@ import {
   UserStreakAction,
   UserStreakActionType,
 } from '../entity';
-import { differenceInDays, isSameDay, max } from 'date-fns';
+import { differenceInDays, isSameDay, max, startOfDay } from 'date-fns';
 import { DataSource, EntityManager, In, Not } from 'typeorm';
 import { CommentMention, Comment, View, Source, SourceMember } from '../entity';
 import { getTimezonedStartOfISOWeek, getTimezonedEndOfISOWeek } from './utils';
@@ -530,16 +530,10 @@ export enum LogoutReason {
 }
 
 const getAbsoluteDifferenceInDays: typeof differenceInDays = (date1, date2) => {
-  const startOfDay = (date: Date) =>
-    new Date(date.getFullYear(), date.getMonth(), date.getDate());
+  const day1 = startOfDay(date1);
+  const day2 = startOfDay(date2);
 
-  const normalizedDate1 = startOfDay(new Date(date1));
-  const normalizedDate2 = startOfDay(new Date(date2));
-
-  const timeDiff = Math.abs(
-    normalizedDate1.getTime() - normalizedDate2.getTime(),
-  );
-
+  const timeDiff = Math.abs(day1.getTime() - day2.getTime());
   const diffInDays = timeDiff / (1000 * 60 * 60 * 24);
 
   // Round down to the nearest whole number since we want full days
