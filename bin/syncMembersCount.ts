23b6a53119ac9b0fbcadb7e15345d17de7fd0b14
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-09-11 17:12:11 +0800

    feat: sync members count script

---
+++ bin/syncMembersCount.ts
@@ -0,0 +1,53 @@
+import fastq from 'fastq';
+import '../src/config';
+import createOrGetConnection from '../src/db';
+import { Source, SourceMember, SquadSource } from '../src/entity';
+import { updateFlagsStatement } from '../src/common';
+
+interface Result {
+  sourceId: string;
+  count: number;
+}
+
+const QUEUE_CONCURRENCY = 1;
+
+(async () => {
+  const con = await createOrGetConnection();
+  await con.transaction(async (manager) => {
+    console.log('initializing stream');
+    const builder = manager
+      .getRepository(SourceMember)
+      .createQueryBuilder('sm')
+      .select('COUNT(sm.*)', 'count')
+      .addSelect('sm."sourceId"')
+      .where(`sm.role != 'blocked'`)
+      .groupBy('sm."sourceId"');
+
+    const stream = await builder.stream();
+
+    let insertCount = 0;
+    const insertQueue = fastq.promise(async ({ sourceId, count }: Result) => {
+      await manager
+        .getRepository(SquadSource)
+        .update(
+          { id: sourceId },
+          { flags: updateFlagsStatement<Source>({ totalMembers: count }) },
+        );
+
+      insertCount += 1;
+    }, QUEUE_CONCURRENCY);
+
+    stream.on('data', (result: Result) => {
+      insertQueue.push(result);
+    });
+
+    await new Promise((resolve, reject) => {
+      stream.on('error', reject);
+      stream.on('end', resolve);
+    });
+    await insertQueue.drained();
+    console.log('update finished with a total of: ', insertCount);
+  });
+
+  process.exit();
+})();
