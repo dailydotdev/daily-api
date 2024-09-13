c6b30386b7e270f431c6d1227f3787a43f2602ef
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-09-13 19:35:35 +0800

    fix: migration rollback function

--- src/migration/1726147441577-SourceMembersCountFix.ts
+++ src/migration/1726147441577-SourceMembersCountFix.ts
@@ -35,9 +35,33 @@ export class SourceMembersCountFix1726147441577 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+      CREATE OR REPLACE FUNCTION increment_squad_members_count()
+        RETURNS TRIGGER
+        LANGUAGE PLPGSQL
+        AS
+      $$
+      BEGIN
+        UPDATE  source
+        SET     flags = jsonb_set(
+                          flags,
+                          '{totalMembers}',
+                          to_jsonb(COALESCE(CAST(flags->>'totalMembers' AS INTEGER), 0) + 1)
+                        )
+        WHERE   id = NEW."sourceId"
+        AND     type = 'squad';
+        RETURN NEW;
+      END;
+      $$
+    `);
     queryRunner.query(
-      'DROP TRIGGER IF EXISTS increment_squad_members_count ON post',
+      `
+      CREATE OR REPLACE TRIGGER increment_squad_members_count
+      AFTER INSERT ON "source_member"
+      FOR EACH ROW
+      WHEN (NEW.role = 'member')
+      EXECUTE PROCEDURE increment_squad_members_count()
+    `,
     );
-    queryRunner.query('DROP FUNCTION IF EXISTS increment_squad_members_count');
   }
 }
