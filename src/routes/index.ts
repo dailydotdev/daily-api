e62fc01d65f3a5b0621dd651bc23c3eff6c14a6f
Luca Pagliaro <pagliaroluca@gmail.com>
2024-09-18 11:31:29 +0200

    fix: removed ending slash
    Co-authored-by: Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -52,7 +52,7 @@ export default async function (fastify: FastifyInstance): Promise<void> {
   fastify.get('/robots.txt', (req, res) => {
     return res.type('text/plain').send(`User-agent: *
 Allow: /devcards/
-Allow: /graphql/
+Allow: /graphql
 Allow: /boot
 Disallow: /`);
   });
