9c11d62dbfaa669aaa0ed46a54cfb6f828b31c65
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-09-26 16:07:19 +0200

    fix: tests

--- __tests__/cron/updateSourcePublicThreshold.ts
+++ __tests__/cron/updateSourcePublicThreshold.ts
@@ -5,7 +5,7 @@ import { saveFixtures } from '../helpers';
 import { expectSuccessfulCron } from '../helpers';
 import { DataSource } from 'typeorm';
 import createOrGetConnection from '../../src/db';
-import { Source } from '../../src/entity';
+import { Source, SourceType } from '../../src/entity';
 import { updateFlagsStatement } from '../../src/common';
 
 let con: DataSource;
@@ -32,7 +32,7 @@ describe('updateSourcePublicThreshold', () => {
       { flags: updateFlagsStatement({ totalMembers: 2 }) },
     );
     await expectSuccessfulCron(cron);
-    const source = await con.getRepository(Source).findOneBy({ id: 'a' });
+    const source = await repo.findOneBy({ id: 'a' });
     expect(source!.flags.publicThreshold).toBeFalsy();
   });
 
@@ -43,7 +43,47 @@ describe('updateSourcePublicThreshold', () => {
       { flags: updateFlagsStatement({ totalPosts: 2 }) },
     );
     await expectSuccessfulCron(cron);
-    const source = await con.getRepository(Source).findOneBy({ id: 'a' });
+    const source = await repo.findOneBy({ id: 'a' });
+    expect(source!.flags.publicThreshold).toBeFalsy();
+  });
+
+  it('should not update public threshold if type is not Squad', async () => {
+    const repo = con.getRepository(Source);
+    await repo.update(
+      { id: 'a' },
+      { flags: updateFlagsStatement({ totalMembers: 3, totalPosts: 3 }) },
+    );
+    await expectSuccessfulCron(cron);
+    const source = await repo.findOneBy({ id: 'a' });
+    expect(source!.flags.publicThreshold).toBeFalsy();
+  });
+
+  it('should not update public threshold if image is null', async () => {
+    const repo = con.getRepository(Source);
+    await repo.update(
+      { id: 'a' },
+      {
+        type: SourceType.Squad,
+        flags: updateFlagsStatement({ totalMembers: 3, totalPosts: 3 }),
+      },
+    );
+    await expectSuccessfulCron(cron);
+    const source = await repo.findOneBy({ id: 'a' });
+    expect(source!.flags.publicThreshold).toBeFalsy();
+  });
+
+  it('should not update public threshold if description is null', async () => {
+    const repo = con.getRepository(Source);
+    await repo.update(
+      { id: 'a' },
+      {
+        type: SourceType.Squad,
+        image: 'not null',
+        flags: updateFlagsStatement({ totalMembers: 3, totalPosts: 3 }),
+      },
+    );
+    await expectSuccessfulCron(cron);
+    const source = await repo.findOneBy({ id: 'a' });
     expect(source!.flags.publicThreshold).toBeFalsy();
   });
 
@@ -51,10 +91,15 @@ describe('updateSourcePublicThreshold', () => {
     const repo = con.getRepository(Source);
     await repo.update(
       { id: 'a' },
-      { flags: updateFlagsStatement({ totalMembers: 3, totalPosts: 3 }) },
+      {
+        type: SourceType.Squad,
+        image: 'not null',
+        description: 'not null',
+        flags: updateFlagsStatement({ totalMembers: 3, totalPosts: 3 }),
+      },
     );
     await expectSuccessfulCron(cron);
-    const source = await con.getRepository(Source).findOneBy({ id: 'a' });
+    const source = await repo.findOneBy({ id: 'a' });
     expect(source!.flags.publicThreshold).toBeTruthy();
   });
 });
