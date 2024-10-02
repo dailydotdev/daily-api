8410a0305e789aa4b38cd6c3f4c63ee4efeb8fd5
Ante Barić <ante@kickass.website>
2024-10-02 09:17:58 +0200

    feat(strict-ts): apply to telemetry (#2173)

--- src/telemetry/common.ts
+++ src/telemetry/common.ts
@@ -1,14 +1,36 @@
 import { FastifyRequest } from 'fastify';
 import type { MetricOptions } from '@opentelemetry/api';
+import { unwrapArray } from '../common';
 
 export const channelName = 'fastify.initialization';
 
 // Try to get the app version from the header, then query param, then default to unknown
-export const getAppVersion = (req: FastifyRequest): string => {
-  return req.headers['x-app-version'] || req.query['v'] || 'unknown';
+export const getAppVersion = (req: AppVersionRequest): string => {
+  const headerVersion = unwrapArray(req.headers['x-app-version']);
+
+  if (headerVersion) {
+    return headerVersion;
+  }
+
+  const queryVersion = unwrapArray(req.query['v']);
+
+  if (queryVersion) {
+    return queryVersion;
+  }
+
+  return 'unknown';
 };
 
 export const SEMATTRS_DAILY_APPS_VERSION = 'dailydev.apps.version';
 export const SEMATTRS_DAILY_APPS_USER_ID = 'dailydev.apps.userId';
 
 export type CounterOptions = { name: string } & MetricOptions;
+
+export type AppVersionRequest = FastifyRequest<{
+  Headers: {
+    'x-app-version'?: string | string[];
+  };
+  Querystring: {
+    v?: string | string[];
+  };
+}>;
