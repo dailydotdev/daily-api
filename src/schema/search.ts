026319f7e7fefd913398e0fa3663b14a809d12c1
capJavert <dev@kickass.website>
2024-10-02 09:18:38 +0200

    Revert "chore: temporary search logging added (#2145)"
    This reverts commit 6008a7efcb7946cbfcd353d6a76cb622f380d8cc.

--- src/schema/search.ts
+++ src/schema/search.ts
@@ -440,14 +440,6 @@ export const resolvers: IResolvers<unknown, BaseContext> = traceResolvers<
           hits: [],
         };
       }
-
-      ctx.log.info(
-        {
-          query,
-        },
-        'Searching user suggestions',
-      );
-
       const searchQuery = ctx.con
         .createQueryBuilder()
         .select(`id, name as title, username as subtitle, image`)
