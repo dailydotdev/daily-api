78905d06244e5909f7875cc5741e9349868a7379
capJavert <dev@kickass.website>
2024-10-14 16:48:09 +0200

    feat: reuse manager and pool connect calls

--- src/common/queryReadReplica.ts
+++ src/common/queryReadReplica.ts
@@ -1,11 +1,11 @@
 import { DataSource, QueryRunner } from 'typeorm';
+import { queryDataSource } from './queryDataSource';
 
 export const queryReadReplica = async <T>(
   con: DataSource,
   callback: ({ queryRunner }: { queryRunner: QueryRunner }) => Promise<T>,
 ): Promise<T> => {
-  const queryRunner = con.createQueryRunner('slave');
-  const result = await callback({ queryRunner });
-  await queryRunner.release();
-  return result;
+  return queryDataSource(con, callback, {
+    mode: 'slave',
+  });
 };
