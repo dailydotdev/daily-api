b2b2104df6cabbf17df08aac5412336a9e807d56
Chris Bongers <chrisbongers@gmail.com>
2024-10-14 11:21:25 +0200

    Update src/notifications/generate.ts

--- src/notifications/generate.ts
+++ src/notifications/generate.ts
@@ -35,7 +35,7 @@ const systemTitle = () => undefined;
 const getPostOrSharedPostTitle = (
   ctx: NotificationPostContext,
 ): string | null | undefined => {
-  if (ctx.post?.title?.length > 0) {
+  if (ctx.post?.title?.length) {
     return ctx.post.title;
   }
 
