e28740ff1c20b90cd0c40375b85432237893b7eb
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-11-01 12:43:17 +0100

    feat: add `topReaderBadge` to alerts and create new query to fetch top reader badges (#2376)

--- src/schema/users.ts
+++ src/schema/users.ts
@@ -411,10 +411,6 @@ export const typeDefs = /* GraphQL */ `
     Content preference in regards to current user
     """
     contentPreference: ContentPreference
-    """
-    User's top reader badges
-    """
-    topReader: [UserTopReader]
   }
 
   """
@@ -870,9 +866,14 @@ export const typeDefs = /* GraphQL */ `
     companies: [UserCompany] @auth
 
     """
+    Get the latest top reader badges for the user
+    """
+    topReaderBadge(limit: Int): [UserTopReader] @auth
+
+    """
     Get the top reader badge based on badge ID
     """
-    topReaderBadge(id: ID!): UserTopReader
+    topReaderBadgeById(id: ID!): UserTopReader
   }
 
   extend type Mutation {
@@ -1697,6 +1698,29 @@ export const resolvers: IResolvers<unknown, BaseContext> = traceResolvers<
       );
     },
     topReaderBadge: async (
+      _,
+      { limit = 5 }: { limit: number },
+      ctx: AuthContext,
+      info: GraphQLResolveInfo,
+    ) => {
+      return graphorm.query<GQLUserTopReader>(
+        ctx,
+        info,
+        (builder) => {
+          builder.queryBuilder = builder.queryBuilder
+            .andWhere(`${builder.alias}.userId = :userId`, {
+              userId: ctx.userId,
+            })
+            .orderBy({
+              '"issuedAt"': 'DESC',
+            })
+            .limit(limit);
+          return builder;
+        },
+        true,
+      );
+    },
+    topReaderBadgeById: async (
       _,
       { id }: { id: string },
       ctx: AuthContext,
