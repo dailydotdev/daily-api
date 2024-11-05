0db51cb158d8ded35f8bf17ce90232deb6fc44a7
capJavert <dev@kickass.website>
2024-11-05 11:36:20 +0100

    feat: additional notification

--- src/common/contentPreference.ts
+++ src/common/contentPreference.ts
@@ -55,6 +55,7 @@ export const entityToNotificationTypeMap: Record<
   [ContentPreferenceType.Source]: [
     NotificationType.SourcePostAdded,
     NotificationType.SquadPostAdded,
+    NotificationType.SquadMemberJoined,
   ],
 };
 
