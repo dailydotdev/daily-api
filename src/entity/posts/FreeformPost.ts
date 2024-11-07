0f3b63349c3bf40ac38200c311fe3fdfe2c78f74
denisb0 <54359969+denisb0@users.noreply.github.com>
2024-11-07 12:04:26 +0200

    feat: reduce freeform post minimum content length

--- src/entity/posts/FreeformPost.ts
+++ src/entity/posts/FreeformPost.ts
@@ -2,7 +2,7 @@ import { ChildEntity, Column } from 'typeorm';
 import { Post, PostType } from './Post';
 
 // Minimun content length required for new posts to trigger content-requested
-export const FREEFORM_POST_MINIMUM_CONTENT_LENGTH = 50;
+export const FREEFORM_POST_MINIMUM_CONTENT_LENGTH = 5;
 
 // Minimun content length required for updated posts to trigger content-requested
 export const FREEFORM_POST_MINIMUM_CHANGE_LENGTH = 200;
