f8aa7b2f13fce433fd33de6ae77c441ecde2d158
Chris Bongers <chrisbongers@gmail.com>
2024-11-12 17:40:39 +0200

    fix: add types

--- src/types.ts
+++ src/types.ts
@@ -59,6 +59,8 @@ declare global {
       SUBMIT_ARTICLE_THRESHOLD: string;
       SLACK_SIGNING_SECRET: string;
       API_CONFIG_FEATURE_KEY: string;
+      PADDLE_API_KEY: string;
+      PADDLE_WEBHOOK_SECRET: string;
     }
   }
 }
