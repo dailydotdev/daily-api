5d47e24e2c3c951665aa3d0b144b98cae1b21436
Chris Bongers <chrisbongers@gmail.com>
2024-11-14 08:50:54 +0200

    Update src/paddle.ts

--- src/paddle.ts
+++ src/paddle.ts
@@ -8,8 +8,4 @@ export enum SubscriptionCycles {
 export const isPlusMember = (cycle: SubscriptionCycles | undefined): boolean =>
   !!cycle?.length || false;
 
-export const planTypes = remoteConfig.vars.pricingIds || {
-  pri_01jcdp5ef4yhv00p43hr2knrdg: SubscriptionCycles.Monthly,
-  pri_01jcdn6enr5ap3ekkddc6fv6tq: SubscriptionCycles.Yearly,
-  pri_01gsz8x8sawmvhz1pv30nge1ke: SubscriptionCycles.Yearly,
-};
+export const planTypes = remoteConfig.vars.pricingIds || {};
