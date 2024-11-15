90057f3a1f6973220fa8d6d6e6085ab77301b454
Chris Bongers <chrisbongers@gmail.com>
2024-11-15 16:32:25 +0200

    fix: add all events

--- src/routes/webhooks/paddle.ts
+++ src/routes/webhooks/paddle.ts
@@ -5,6 +5,10 @@ import {
   Paddle,
   SubscriptionCanceledEvent,
   SubscriptionCreatedEvent,
+  SubscriptionItemNotification,
+  SubscriptionUpdatedEvent,
+  TransactionItemNotification,
+  TransactionPaidEvent,
 } from '@paddle/paddle-node-sdk';
 import createOrGetConnection from '../../db';
 import { updateSubscriptionFlags } from '../../common';
@@ -19,6 +23,24 @@ import {
 const paddleInstance = new Paddle(process.env.PADDLE_API_KEY, {
   environment: process.env.PADDLE_ENVIRONMENT as Environment,
 });
+
+const extractSubscriptionType = (
+  items:
+    | SubscriptionItemNotification[]
+    | TransactionItemNotification[]
+    | undefined,
+): string => {
+  if (!items) {
+    return '';
+  }
+  return items.reduce((acc, item) => {
+    const pricingIds = remoteConfig.vars?.pricingIds;
+    if (item.price?.id && pricingIds?.[item.price.id]) {
+      acc = pricingIds?.[item.price.id] || '';
+    }
+    return acc;
+  }, '');
+};
 
 const updateUserSubscription = async ({
   data,
@@ -39,13 +61,9 @@ const updateUserSubscription = async ({
     logger.error({ type: 'paddle' }, 'User ID missing in payload');
     return false;
   }
-  const subscriptionType = data.data?.items.reduce((acc, item) => {
-    const pricingIds = remoteConfig.vars?.pricingIds;
-    if (item.price?.id && pricingIds?.[item.price.id]) {
-      acc = pricingIds?.[item.price.id] || '';
-    }
-    return acc;
-  }, '');
+
+  const subscriptionType = extractSubscriptionType(data.data?.items);
+
   if (!subscriptionType) {
     logger.error(
       {
@@ -71,17 +89,45 @@ const updateUserSubscription = async ({
 };
 
 const logPaddleAnalyticsEvent = async (
-  data: SubscriptionCreatedEvent | SubscriptionCanceledEvent | undefined,
+  data:
+    | SubscriptionUpdatedEvent
+    | SubscriptionCanceledEvent
+    | TransactionPaidEvent
+    | undefined,
+  eventName: AnalyticsEventName,
 ) => {
   if (!data) {
     return;
   }
 
+  const customData = data.data?.customData as { user_id: string };
+  const cycle = extractSubscriptionType(data.data?.items);
+  const cost = data.data?.items?.[0]?.price?.unitPrice?.amount;
+  const currency = data.data?.items?.[0]?.price?.unitPrice?.currencyCode;
+  const userId = customData?.user_id || data?.data.id || '';
+  const payment =
+    'payments' in data.data &&
+    data.data?.payments?.reduce((acc, item) => {
+      if (item.status === 'captured') {
+        acc = item?.methodDetails?.type || '';
+      }
+      return acc;
+    }, '');
+
+  const extra = {
+    cycle,
+    cost,
+    currency,
+    payment,
+  };
+
   await sendAnalyticsEvent([
     {
-      event_name: AnalyticsEventName.CancelSubscription,
+      event_name: eventName,
       event_timestamp: new Date(),
-      user_id: 'eventData?.userId',
+      app_platform: 'api',
+      user_id: userId,
+      extra: JSON.stringify(extra),
     },
   ]);
 };
@@ -117,8 +163,23 @@ export const paddle = async (fastify: FastifyInstance): Promise<void> => {
                   data: eventData,
                   state: false,
                 });
-              case EventName.SubscriptionCanceled:
-                await logPaddleAnalyticsEvent(eventData);
+                await logPaddleAnalyticsEvent(
+                  eventData,
+                  AnalyticsEventName.CancelSubscription,
+                );
+                break;
+              case EventName.SubscriptionUpdated:
+                await logPaddleAnalyticsEvent(
+                  eventData,
+                  AnalyticsEventName.ChangeBillingCycle,
+                );
+                break;
+              case EventName.TransactionPaid:
+                await logPaddleAnalyticsEvent(
+                  eventData,
+                  AnalyticsEventName.ReceivePayment,
+                );
+                break;
               default:
                 logger.info({ type: 'paddle' }, eventData?.eventType);
             }
