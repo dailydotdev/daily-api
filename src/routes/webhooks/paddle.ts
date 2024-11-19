361acd0e461cd2d7fd72c7e5d185e48911948d1a
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-11-19 14:42:13 +0100

    fix: lint

--- src/routes/webhooks/paddle.ts
+++ src/routes/webhooks/paddle.ts
@@ -190,7 +190,7 @@ const logPaddleAnalyticsEvent = async (
   ]);
 };
 
-const concatText = (a: string, b: string) => [a, b].filter(Boolean).join(`\n`)
+const concatText = (a: string, b: string) => [a, b].filter(Boolean).join(`\n`);
 const notifyNewPaddleTransaction = async ({
   data,
 }: TransactionCompletedEvent) => {
