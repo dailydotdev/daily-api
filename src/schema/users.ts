b823bb0c45d362a24d43752607e5da75770e18e5
Lee Hansel Solevilla <13744167+sshanzel@users.noreply.github.com>
2024-11-20 22:24:10 +0800

    refactor: user streak to include last view with timezone (#2471)
    Expose the column that contains the timezone-adjusted column for last
    view at.

--- src/schema/users.ts
+++ src/schema/users.ts
@@ -669,6 +669,7 @@ export const typeDefs = /* GraphQL */ `
     total: Int
     current: Int
     lastViewAt: DateTime
+    lastViewAtTz: DateTime
     weekStart: Int
   }
 
@@ -1204,7 +1205,8 @@ const getUserStreakQuery = async (
     ...builder,
     queryBuilder: builder.queryBuilder
       .addSelect(
-        `(date_trunc('day', "${builder.alias}"."lastViewAt" at time zone COALESCE(u.timezone, 'utc'))::date) AS "lastViewAtTz"`,
+        `(date_trunc('day', "${builder.alias}"."lastViewAt" at time zone COALESCE(u.timezone, 'utc'))::date)`,
+        'lastViewAtTz',
       )
       .addSelect('u.id', 'userId')
       .addSelect('u.timezone', 'timezone')
