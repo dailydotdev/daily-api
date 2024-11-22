9bb2e2ff581155efa931713d47f13aff8df7665a
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-11-22 20:33:12 +0800

    fix: validation when rejecting a post

--- src/schema/posts.ts
+++ src/schema/posts.ts
@@ -2342,6 +2342,18 @@ export const resolvers: IResolvers<unknown, BaseContext> = traceResolvers<
         throw new ForbiddenError('Access denied!');
       }
 
+      if (status === SourcePostModerationStatus.Rejected && !rejectionReason) {
+        throw new ValidationError('Rejection reason is required');
+      }
+
+      if (
+        status === SourcePostModerationStatus.Rejected &&
+        rejectionReason?.toLowerCase()?.includes('other') &&
+        !moderatorMessage
+      ) {
+        throw new ValidationError('Moderator message is required');
+      }
+
       const moderatedById = ctx.userId;
       const isRejectedWithReason =
         status === SourcePostModerationStatus.Rejected && !!rejectionReason;
