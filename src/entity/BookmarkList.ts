a53df8c66e05773f6188c4f96300b2301b459f01
Luca Pagliaro <luca@sentiero.digital>
2024-12-04 11:27:22 +0100

    feat: updated entity

--- src/entity/BookmarkList.ts
+++ src/entity/BookmarkList.ts
@@ -1,4 +1,15 @@
-import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  Index,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
+import type { User } from './user';
+import { Bookmark } from './Bookmark';
 
 @Entity()
 export class BookmarkList {
@@ -9,6 +20,27 @@ export class BookmarkList {
   @Index()
   userId: string;
 
+  @ManyToOne('User', {
+    lazy: true,
+    onDelete: 'CASCADE',
+  })
+  user: Promise<User>;
+
   @Column({ type: 'text' })
   name: string;
+
+  @Column({ type: 'text', nullable: true })
+  icon: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany('Bookmark', 'list', {
+    lazy: true,
+    onDelete: 'CASCADE',
+  })
+  bookmarks: Promise<Bookmark[]>;
 }
