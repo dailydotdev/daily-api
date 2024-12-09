2af13343813b340b83bd03ef4ac936d22518272a
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-12-09 10:59:54 +0100

    refactor: use dataloader for storing user settings per request (#2506)

--- src/graphorm/index.ts
+++ src/graphorm/index.ts
@@ -16,7 +16,6 @@ import {
   SettingsFlagsPublic,
   UserStats,
   UserSubscriptionFlags,
-  Settings,
 } from '../entity';
 import {
   SourceMemberRoles,
@@ -82,14 +81,17 @@ const checkIfTitleIsClickbait = (value?: string): boolean => {
 const createSmartTitleField = ({ field }: { field: string }): GraphORMField => {
   return {
     select: field,
-    transform: (value: string, ctx: Context, parent): string => {
+    transform: async (value: string, ctx: Context, parent) => {
       const typedParent = parent as {
-        ['i18nTitle']: I18nRecord;
-        ['smartTitle']: I18nRecord;
-        clickbaitShieldEnabled?: boolean;
+        i18nTitle: I18nRecord;
+        smartTitle: I18nRecord;
         clickbaitProbability?: string;
         [key: string]: unknown;
       };
+
+      const settings = await ctx.dataLoader.userSettings.load({
+        userId: ctx.userId!,
+      });
 
       const i18nValue = typedParent.i18nTitle?.[ctx.contentLanguage];
       const altValue = getSmartTitle(
@@ -97,7 +99,7 @@ const createSmartTitleField = ({ field }: { field: string }): GraphORMField => {
         typedParent.smartTitle,
       );
       const clickbaitShieldEnabled =
-        typedParent?.clickbaitShieldEnabled ?? true;
+        settings?.flags?.clickbaitShieldEnabled ?? true;
 
       const clickbaitTitleDetected = checkIfTitleIsClickbait(
         typedParent.clickbaitProbability,
@@ -272,24 +274,10 @@ const obj = new GraphORM({
     },
   },
   Post: {
-    additionalQuery: (ctx, alias, qb) => {
-      qb = qb
+    additionalQuery: (ctx, alias, qb) =>
+      qb
         .andWhere(`"${alias}"."deleted" = false`)
-        .andWhere(`"${alias}"."visible" = true`);
-
-      if (ctx.userId && ctx.isPlus) {
-        qb = qb
-          .innerJoin(Settings, 's', 's."userId" = :userId', {
-            userId: ctx.userId,
-          })
-          .addSelect(
-            `"s"."flags"->'clickbaitShieldEnabled'`,
-            'clickbaitShieldEnabled',
-          );
-      }
-
-      return qb;
-    },
+        .andWhere(`"${alias}"."visible" = true`),
     requiredColumns: [
       'id',
       'shortId',
