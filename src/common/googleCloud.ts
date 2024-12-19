ef1308f673d33bf02a99939b7349d73150dee54c
Lee Hansel Solevilla <sshanzel@yahoo.com>
2024-12-19 17:51:07 +0800

    chore: updated query based on dave

--- src/common/googleCloud.ts
+++ src/common/googleCloud.ts
@@ -33,49 +33,68 @@ export const downloadJsonFile = async <T>({
   return JSON.parse(result);
 };
 
+export enum UserActiveState {
+  Active = '1',
+  InactiveSince6wAgo = '2',
+  InactiveSince12wAgo = '3',
+  NeverActive = '4',
+}
+
 export const userActiveStateQuery = `
   with d as (
-    select uss.primary_user_id,
+    select u.primary_user_id,
     min(last_app_timestamp) as last_app_timestamp,
     min(registration_timestamp) as registration_timestamp,
-
-      min(
-        case when period_end between date(@run_date - interval 6*7 day) and @run_date then '1. active_last_6w'
-          when period_end between date(@run_date - interval 12*7 day) and date(@run_date - interval 6*7 + 1 day) then '2. active_7w_12w'
-          when date(u.last_app_timestamp) <  date(@run_date - interval 12*7 day) then '3. active_12w+'
-          when date(u.registration_timestamp) <  date(@run_date - interval 12*7 day) then '3. active_12w+'
-          else '4. never_active' end
+    min(
+        case
+          when period_end is null then '4'
+          when period_end between date(@previous_date - interval 6*7 day) and @previous_date then '1'
+          when period_end between date(@previous_date - interval 12*7 day) and date(@previous_date - interval 6*7 + 1 day) then '2'
+          when date(u.last_app_timestamp) <  date(@previous_date - interval 12*7 day) then '3'
+          when date(u.registration_timestamp) <  date(@previous_date - interval 12*7 day) then '3'
+          else '4' end
       ) as previous_state,
-
       min(
-        case when period_end between date(@previous_date - interval 6*7 day) and @previous_date then '1. active_last_6w'
-            when period_end between date(@previous_date - interval 12*7 day) and date(@previous_date - interval 6*7 + 1 day) then '2. active_7w_12w'
-            when date(u.last_app_timestamp) <  date(@previous_date - interval 12*7 day) then '3. active_12w+'
-            when date(u.registration_timestamp) <  date(@previous_date - interval 12*7 day) then '3. active_12w+'
-            else '4. never_active' end
+        case
+            when period_end is null then '4'
+            when period_end between date(@run_date - interval 6*7 day) and @run_date then '1'
+            when period_end between date(@run_date - interval 12*7 day) and date(@run_date - interval 6*7 + 1 day) then '2'
+            when date(u.last_app_timestamp) <  date(@run_date - interval 12*7 day) then '3'
+            when date(u.registration_timestamp) <  date(@run_date - interval 12*7 day) then '3'
+            else '4' end
       ) as current_state,
-
-
+      min(
+        case
+          when period_end is null then '4. never_active'
+          when period_end between date(@previous_date - interval 6*7 day) and @previous_date then '1. active_last_6w'
+          when period_end between date(@previous_date - interval 12*7 day) and date(@previous_date - interval 6*7 + 1 day) then '2. active_7w_12w'
+          when date(u.last_app_timestamp) <  date(@previous_date - interval 12*7 day) then '3. active_12w+'
+          when date(u.registration_timestamp) <  date(@previous_date - interval 12*7 day) then '3. active_12w+'
+          else '4. never_active' end
+      ) as previous_state_name,
+      min(
+        case
+            when period_end is null then '4. never_active'
+            when period_end between date(@run_date - interval 6*7 day) and @run_date then '1. active_last_6w'
+            when period_end between date(@run_date - interval 12*7 day) and date(@run_date - interval 6*7 + 1 day) then '2. active_7w_12w'
+            when date(u.last_app_timestamp) <  date(@run_date - interval 12*7 day) then '3. active_12w+'
+            when date(u.registration_timestamp) <  date(@run_date - interval 12*7 day) then '3. active_12w+'
+            else '4. never_active' end
+      ) as current_state_name,
     from analytics.user as u
     left join analytics.user_state_sparse as uss on uss.primary_user_id = u.primary_user_id
-      and uss.period_end between date(@run_date - interval 12* 7 day) and @previous_date
+      and uss.period_end between date(@previous_date - interval 12* 7 day) and @run_date
       and uss.period = 'daily'
       and uss.app_active_state = 'active'
       and uss.registration_state = 'registered'
     where u.registration_timestamp is not null
-    and u.last_app_timestamp is not null
-    and u.is_spam = false
+    and date(u.registration_timestamp) < @run_date
     group by 1
   )
-  --  select previous_state, current_state, count(*) as ctr
-  --  from d
-  --  where current_state != previous_state
-  --  group by 1,2
-  --  order by 1,2
-
   select *
   from d
-  where previous_state != current_state
+  where current_state != previous_state
+  and previous_state != '4'
 `;
 
 export const getUserActiveStateQuery = (runDate: Date): Query => {
@@ -91,15 +110,17 @@ export interface GetUsersActiveState {
   reactivateUsers: string[];
 }
 
-export interface UserActiveState {
-  current_state: string;
-  previous_state: string;
+export interface UserActiveStateData {
+  current_state: UserActiveState;
+  previous_state: UserActiveState;
   primary_user_id: string;
 }
 
 const bigquery = new BigQuery();
 
-export const queryFromBq = async (query: Query): Promise<UserActiveState[]> => {
+export const queryFromBq = async (
+  query: Query,
+): Promise<UserActiveStateData[]> => {
   const [job] = await bigquery.createQueryJob(query);
   const [rows] = await job.getQueryResults();
 
@@ -118,20 +139,18 @@ export const getUsersActiveState = async (
   // sort users from bq into active, inactive, downgrade, and reactivate
   for (const user of usersFromBq) {
     if (
-      user.current_state.includes('active_7w_12w') &&
-      user.previous_state.includes('active_last_6w')
+      user.current_state === UserActiveState.InactiveSince6wAgo &&
+      user.previous_state === UserActiveState.Active
     ) {
       downgradeUsers.push(user.primary_user_id);
     } else if (
-      user.current_state.includes('active_last_6w') &&
-      !user.previous_state.includes('active_last_6w')
+      user.current_state === UserActiveState.Active &&
+      user.previous_state !== UserActiveState.Active
     ) {
       reactivateUsers.push(user.primary_user_id);
     } else if (
-      (user.current_state.includes('active_12w+') &&
-        !user.previous_state.includes('active_12w+')) ||
-      (user.current_state.includes('never_active') &&
-        !user.previous_state.includes('never_active'))
+      user.current_state === UserActiveState.InactiveSince12wAgo &&
+      user.previous_state !== UserActiveState.InactiveSince12wAgo
     ) {
       inactiveUsers.push(user.primary_user_id);
     }
