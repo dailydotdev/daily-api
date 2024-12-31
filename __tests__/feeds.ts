1006b7e04b59d6376dddbf4872aee9d359b080cc
Chris Bongers <chrisbongers@gmail.com>
2024-12-31 12:47:10 +0200

    fix: test cases

--- __tests__/feeds.ts
+++ __tests__/feeds.ts
@@ -2187,31 +2187,6 @@ describe('mutation updateFeedAdvancedSettings', () => {
     expect(res.data).toMatchSnapshot();
   });
 
-  it('should not update custom feed advanced settings if not plus', async () => {
-    loggedUser = '1';
-
-    await saveFeedFixtures();
-    await saveFixtures(con, Feed, [{ id: '1-ucfas', userId: '1' }]);
-
-    await testMutationErrorCode(
-      client,
-      {
-        mutation: MUTATION,
-        variables: {
-          feedId: '1-ucfas',
-          settings: [
-            { id: 1, enabled: false },
-            { id: 2, enabled: true },
-            { id: 3, enabled: true },
-            { id: 4, enabled: false },
-            { id: 7, enabled: false },
-          ],
-        },
-      },
-      'UNAUTHENTICATED',
-    );
-  });
-
   it('should update custom feed advanced settings', async () => {
     loggedUser = '1';
     isPlus = true;
