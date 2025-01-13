e0df4661887ef9901a8381678bc95d3274b6547a
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-01-13 17:38:17 +0100

    test: add test case for vordr word list with spaces

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -18,7 +18,11 @@ jest.mock('../src/remoteConfig', () => ({
   remoteConfig: {
     init: jest.fn(),
     vars: {
-      vordrWords: ['vordrwillcatchyou', 'andvordrwillhavefun'],
+      vordrWords: [
+        'vordrwillcatchyou',
+        'andvordrwillhavefun',
+        'and vordr will win',
+      ],
       vordrIps: ['192.0.2.0/24', '198.51.100.0/24', '203.0.113.0/24'],
       ignoredWorkEmailDomains: ['igored.com', 'ignored.org'],
       rateLimitReputationThreshold: 1,
