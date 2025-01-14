084c2a04df69ca4e2b624974a8681799bd20cbc0
Luca Pagliaro <luca@sentiero.digital>
2025-01-14 12:30:03 +0100

    feat: add filter for blocked following/followed users

--- src/schema/contentPreference.ts
+++ src/schema/contentPreference.ts
@@ -16,6 +16,7 @@ import {
   followEntity,
   unblockEntity,
   unfollowEntity,
+  whereNotUserBlocked,
 } from '../common/contentPreference';
 import { GQLEmptyResponse, offsetPageGenerator } from './common';
 import graphorm from '../graphorm';
@@ -300,6 +301,14 @@ export const resolvers: IResolvers<unknown, BaseContext> = traceResolvers<
             .offset(page.offset)
             .addOrderBy(`${builder.alias}."createdAt"`, 'DESC');
 
+          if (ctx.userId) {
+            builder.queryBuilder.andWhere(
+              whereNotUserBlocked(builder.queryBuilder, {
+                userId: ctx.userId,
+              }),
+            );
+          }
+
           return builder;
         },
         undefined,
@@ -353,6 +362,15 @@ export const resolvers: IResolvers<unknown, BaseContext> = traceResolvers<
             .limit(page.limit)
             .offset(page.offset)
             .addOrderBy(`${builder.alias}."createdAt"`, 'DESC');
+
+          if (ctx.userId) {
+            builder.queryBuilder.andWhere(
+              whereNotUserBlocked(builder.queryBuilder, {
+                userId: ctx.userId,
+                columnName: `referenceId`,
+              }),
+            );
+          }
 
           return builder;
         },
