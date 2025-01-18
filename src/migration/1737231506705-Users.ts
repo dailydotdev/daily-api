8124b9282d375a47942fa04fdf97fc26021becdc
Amar Trebinjac <amartrebinjac@gmail.com>
2025-01-18 22:29:46 +0100

    feat: Add bluesky support

---
+++ src/migration/1737231506705-Users.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Users1737231506705 implements MigrationInterface {
+  name = 'Users1737231506705';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "bluesky" character varying(100)`,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "users_bluesky_unique" ON "user" ("bluesky") `,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX "public"."users_bluesky_unique"`);
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "bluesky"`);
+  }
+}
