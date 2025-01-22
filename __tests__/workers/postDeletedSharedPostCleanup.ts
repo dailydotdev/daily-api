ec656edc4ff158e4e61db35d4f9288f7f2eda02d
Chris Bongers <chrisbongers@gmail.com>
2025-01-22 12:21:39 +0200

    fix: don't show shared post deleted on feed

---
+++ __tests__/workers/postDeletedSharedPostCleanup.ts
@@ -0,0 +1,49 @@
+import { expectSuccessfulBackground, saveFixtures } from '../helpers';
+import worker from '../../src/workers/postDeletedSharedPostCleanup';
+import { DataSource } from 'typeorm';
+import createOrGetConnection from '../../src/db';
+import { Post, SharePost, Source } from '../../src/entity';
+import { postsFixture, sharedPostsFixture } from '../fixture/post';
+import { sourcesFixture } from '../fixture';
+
+let con: DataSource;
+beforeEach(async () => {
+  con = await createOrGetConnection();
+  jest.resetAllMocks();
+  await saveFixtures(con, Source, sourcesFixture);
+  await saveFixtures(
+    con,
+    Post,
+    postsFixture.map((item) => {
+      return {
+        ...item,
+        id: `pdspc-${item.id}`,
+        deleted: true,
+      };
+    }),
+  );
+  await saveFixtures(
+    con,
+    SharePost,
+    sharedPostsFixture.map((item) => {
+      return {
+        ...item,
+        id: `pdspc-${item.id}`,
+        sharedPostId: `pdspc-p1`,
+      };
+    }),
+  );
+});
+
+it('should set shared post to not show on feed if post gets deleted', async () => {
+  await expectSuccessfulBackground(worker, {
+    post: {
+      id: 'pdspc-p1',
+    },
+  });
+  const sharedPost = await con.getRepository(SharePost).findOneBy({
+    id: 'pdspc-squadP1',
+  });
+  expect(sharedPost?.showOnFeed).toBe(false);
+  expect(sharedPost?.flags?.showOnFeed).toEqual(false);
+});
---
+++ src/workers/postDeletedSharedPostCleanup.ts
@@ -0,0 +1,55 @@
+import { messageToJson, Worker } from './worker';
+import { Post, SharePost } from '../entity/';
+import { ChangeObject } from '../types';
+import { updateFlagsStatement } from '../common';
+
+interface Data {
+  post: ChangeObject<Post>;
+}
+
+const worker: Worker = {
+  subscription: 'api.post-deleted-shared-post-cleanup',
+  handler: async (message, con, logger): Promise<void> => {
+    const data: Data = messageToJson(message);
+    const { post } = data;
+
+    const databasePost = await con
+      .getRepository(Post)
+      .findOneBy({ id: post?.id });
+    console.log('del: ', databasePost?.deleted);
+    if (!databasePost || !databasePost?.deleted) {
+      return logger.info(
+        {
+          data,
+          messageId: message.messageId,
+        },
+        'failed to cleanup shared post due to post deletion error',
+      );
+    }
+    
+    await con
+      .getRepository(SharePost)
+      .createQueryBuilder()
+      .update()
+      .where({
+        sharedPostId: post.id,
+      })
+      .set({
+        showOnFeed: false,
+        flags: updateFlagsStatement<Post>({
+          showOnFeed: false,
+        }),
+      })
+      .execute();
+
+    logger.info(
+      {
+        data,
+        messageId: message.messageId,
+      },
+      'set shared post to not show on feed due to post deletion',
+    );
+  },
+};
+
+export default worker;
