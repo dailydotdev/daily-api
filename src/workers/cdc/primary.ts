f88f8efced57d4d7ddd98b43c1318142db9ef451
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-01-27 10:58:42 +0100

    Revert "feat: clear title translation when post title is changed (#2618)"
    This reverts commit d16c933d441e6f5d8386f92567f9acd7c1fa83ac.

--- src/workers/cdc/primary.ts
+++ src/workers/cdc/primary.ts
@@ -11,7 +11,6 @@ import {
   Alerts,
   UserTopReader,
   SquadSource,
-  clearPostTranslations,
 } from '../../entity';
 import { messageToJson, Worker } from '../worker';
 import {
@@ -588,10 +587,6 @@ const onPostChange = async (
           { id: data.payload.before!.id },
           { metadataChangedAt: new Date() },
         );
-    }
-
-    if (isChanged(data.payload.before!, data.payload.after!, 'title')) {
-      await clearPostTranslations(con, data.payload.after!.id, 'title');
     }
   } else if (data.payload.op === 'd') {
     await notifyPostBannedOrRemoved(logger, data.payload.before!);
