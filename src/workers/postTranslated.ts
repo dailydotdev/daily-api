cf0b9be305bd7645e20cc09d5d914ba819ee1f9c
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-01-27 12:29:34 +0100

    fix: escape post translated JSON (#2619)

--- src/workers/postTranslated.ts
+++ src/workers/postTranslated.ts
@@ -22,10 +22,14 @@ export const postTranslated: TypedWorker<'kvasir.v1.post-translated'> = {
         .set({
           translation: () => `jsonb_set(
           translation,
-          '{${language}}',
-          '${JSON.stringify(translations)}'::jsonb,
+          :language,
+          :translations::jsonb,
           true
         )`,
+        })
+        .setParameters({
+          language: [language],
+          translations: JSON.stringify(translations),
         })
         .where('id = :id', { id })
         .execute();
