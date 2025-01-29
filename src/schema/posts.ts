8cf135e51df63f5b9450abcfb6b3bfb85b6b17fb
Chris Bongers <chrisbongers@gmail.com>
2025-01-29 15:48:10 +0200

    fix: linting 2

--- src/schema/posts.ts
+++ src/schema/posts.ts
@@ -2345,7 +2345,7 @@ export const resolvers: IResolvers<unknown, BaseContext> = traceResolvers<
       ctx: AuthContext,
       info,
     ): Promise<GQLPost> => {
-      const [post]: [post: Post] = await Promise.all([
+      const [post, __, ___] = await Promise.all([
         ctx.con.getRepository(Post).findOneByOrFail({ id }),
         ensureSourcePermissions(ctx, sourceId, SourcePermissions.Post),
         ensurePostRateLimit(ctx.con, ctx.userId),
