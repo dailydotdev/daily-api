cc8070c15ccde6de9aa19126ce50c147c0bdc199
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-02-27 15:11:04 +0100

    feat: verify and log notification from Apple

--- src/routes/webhooks/apple.ts
+++ src/routes/webhooks/apple.ts
@@ -1,18 +1,52 @@
+import { readFileSync } from 'node:fs';
 import type { FastifyInstance, FastifyRequest } from 'fastify';
 import { logger } from '../../logger';
+import {
+  Environment,
+  SignedDataVerifier,
+} from '@apple/app-store-server-library';
+import { isProd } from '../../common';
+import { env } from 'node:process';
+
+const bundleId = env.APPLE_APP_BUNDLE_ID;
+const appAppleId = env.APPLE_APP_APPLE_ID;
+const enableOnlineChecks = true;
+const environment = isProd ? Environment.PRODUCTION : Environment.SANDBOX;
+
+const appleRootCAs: Buffer[] = [
+  readFileSync('/usr/local/share/ca-certificates/AppleIncRootCertificate.cer'),
+  readFileSync('/usr/local/share/ca-certificates/AppleRootCA-G2.cer'),
+  readFileSync('/usr/local/share/ca-certificates/AppleRootCA-G3.cer'),
+];
+
+const verifier = new SignedDataVerifier(
+  appleRootCAs,
+  enableOnlineChecks,
+  environment,
+  bundleId,
+  appAppleId,
+);
+
+interface AppleNotificationRequest {
+  signedPayload: string;
+}
 
 export const apple = async (fastify: FastifyInstance): Promise<void> => {
   // Endpoint for receiving App Store Server Notifications V2
-  fastify.post('/notifications', async (req: FastifyRequest) => {
-    logger.info(
-      {
-        payload: req.body,
-        headers: req.headers,
-      },
-      'Received Apple App Store Server Notification',
-    );
-    return {
-      received: true,
-    };
-  });
+  fastify.post(
+    '/notifications',
+    async (request: FastifyRequest<{ Body: AppleNotificationRequest }>) => {
+      const { signedPayload } = request.body;
+      const notification =
+        await verifier.verifyAndDecodeNotification(signedPayload);
+
+      logger.info(
+        { notification },
+        'Received Apple App Store Server Notification',
+      );
+      return {
+        received: true,
+      };
+    },
+  );
 };
