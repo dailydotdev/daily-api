51656b74d070446c9266fe8d5e3c332889adeb2c
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-03-12 18:41:20 +0100

    fix: add missed file

---
+++ src/plusSubscription.ts
@@ -0,0 +1,48 @@
+import { updateSubscriptionFlags } from './common';
+import createOrGetConnection from './db';
+import {
+  UserSubscriptionStatus,
+  User,
+  type UserSubscriptionFlags,
+} from './entity';
+
+export const updateUserSubscription = async ({
+  userId,
+  data,
+  status,
+}: {
+  userId: string;
+  data?: Omit<UserSubscriptionFlags, 'status'>;
+  status?: UserSubscriptionStatus;
+}) => {
+  if (!data) {
+    return;
+  }
+
+  // Remove appAccountToken from data, to make sure we don't accidentally update it
+  if (data.appAccountToken) {
+    delete data.appAccountToken;
+  }
+
+  const con = await createOrGetConnection();
+
+  // Convert all keys in data to null when stutus is expired
+  const subscriptionFlags = Object.fromEntries(
+    Object.entries(data).map(([key, value]) => [
+      key,
+      status === UserSubscriptionStatus.Expired ? null : value,
+    ]),
+  );
+
+  await con.getRepository(User).update(
+    {
+      id: userId,
+    },
+    {
+      subscriptionFlags: updateSubscriptionFlags({
+        ...subscriptionFlags,
+        status,
+      }),
+    },
+  );
+};
