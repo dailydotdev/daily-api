18c87c57142b5d49c83d15caa35b29b4828510a7
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-03-12 22:14:51 +0100

    fix: add a few more notification types

--- src/routes/webhooks/apple.ts
+++ src/routes/webhooks/apple.ts
@@ -100,13 +100,23 @@ const getSubscriptionStatus = (
   switch (notificationType) {
     case NotificationTypeV2.SUBSCRIBED:
     case NotificationTypeV2.DID_RENEW:
-    case NotificationTypeV2.DID_CHANGE_RENEWAL_PREF:
+    case NotificationTypeV2.DID_CHANGE_RENEWAL_PREF: // Upgrade/Downgrade
+    case NotificationTypeV2.REFUND_REVERSED:
       return UserSubscriptionStatus.Active;
-    case NotificationTypeV2.DID_CHANGE_RENEWAL_STATUS:
+    case NotificationTypeV2.DID_CHANGE_RENEWAL_STATUS: // Disable/Enable Auto-Renew
       return subtype === Subtype.AUTO_RENEW_ENABLED
         ? UserSubscriptionStatus.Active
         : UserSubscriptionStatus.Cancelled;
+    case NotificationTypeV2.DID_FAIL_TO_RENEW:
+      // When user fails to renew and there is no grace period
+      if (isNullOrUndefined(subtype)) {
+        return UserSubscriptionStatus.Cancelled;
+      }
+    case NotificationTypeV2.GRACE_PERIOD_EXPIRED:
+      return UserSubscriptionStatus.Cancelled;
+    case NotificationTypeV2.REFUND:
     case NotificationTypeV2.EXPIRED:
+    case NotificationTypeV2.REVOKE: // We don't support Family Sharing, but to be on the safe side
       return UserSubscriptionStatus.Expired;
     default:
       return null;
