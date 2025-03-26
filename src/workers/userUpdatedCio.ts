693781b8bf022bf64e0b0e0ce721c9c7b3a9de20
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-03-26 15:54:21 +0100

    fix: add logs for api.user-updated-cio worker (#2715)

--- src/workers/userUpdatedCio.ts
+++ src/workers/userUpdatedCio.ts
@@ -5,35 +5,41 @@ import { cio, identifyUser } from '../cio';
 const worker: TypedWorker<'user-updated'> = {
   subscription: 'api.user-updated-cio',
   handler: async (message, con, log) => {
-    if (!process.env.CIO_SITE_ID) {
-      return;
+    try {
+      if (!process.env.CIO_SITE_ID) {
+        return;
+      }
+
+      const {
+        data: { newProfile: user, user: oldUser },
+      } = message;
+
+      if (
+        user.id === ghostUser.id ||
+        !user.infoConfirmed ||
+        !user.emailConfirmed
+      ) {
+        return;
+      }
+
+      if (
+        (user.notificationEmail && !oldUser.notificationEmail) ||
+        (user.acceptedMarketing && !oldUser.acceptedMarketing)
+      ) {
+        await resubscribeUser(cio, user.id);
+      }
+
+      await identifyUser({
+        con,
+        cio,
+        user,
+      });
+      log.info({ userId: user.id }, 'updated user profile in customerio');
+    } catch (_err) {
+      const err = _err as Error;
+      log.error({ err }, 'failed to update user profile in customerio');
+      throw err;
     }
-
-    const {
-      data: { newProfile: user, user: oldUser },
-    } = message;
-
-    if (
-      user.id === ghostUser.id ||
-      !user.infoConfirmed ||
-      !user.emailConfirmed
-    ) {
-      return;
-    }
-
-    if (
-      (user.notificationEmail && !oldUser.notificationEmail) ||
-      (user.acceptedMarketing && !oldUser.acceptedMarketing)
-    ) {
-      await resubscribeUser(cio, user.id);
-    }
-
-    await identifyUser({
-      con,
-      cio,
-      user,
-    });
-    log.info({ userId: user.id }, 'updated user profile in customerio');
   },
 };
 
