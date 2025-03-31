5f2cf90868e9874e441fe820a0b0b71a56005a12
Ido Shamun <idoesh1@gmail.com>
2025-03-31 18:32:48 +0300

    fix: add missing files 🙈

---
+++ src/integrations/freyja/clients.ts
@@ -0,0 +1,62 @@
+import { RequestInit } from 'node-fetch';
+import { IFreyjaClient, type FunnelState } from './types';
+import { GarmrNoopService, IGarmrService, GarmrService } from '../garmr';
+import { fetchOptions as globalFetchOptions } from '../../http';
+import { fetchParse } from '../retry';
+
+export class FreyjaClient implements IFreyjaClient {
+  private readonly fetchOptions: RequestInit;
+  private readonly garmr: IGarmrService;
+
+  constructor(
+    private readonly url: string,
+    options?: {
+      fetchOptions?: RequestInit;
+      garmr?: IGarmrService;
+    },
+  ) {
+    const {
+      fetchOptions = globalFetchOptions,
+      garmr = new GarmrNoopService(),
+    } = options || {};
+
+    this.fetchOptions = fetchOptions;
+    this.garmr = garmr;
+  }
+
+  createSession(
+    userId: string,
+    funnelId: string,
+    version?: number,
+  ): Promise<FunnelState> {
+    return this.garmr.execute(() => {
+      return fetchParse(`${this.url}/api/sessions`, {
+        ...this.fetchOptions,
+        method: 'POST',
+        body: JSON.stringify({ userId, funnelId, version }),
+      });
+    });
+  }
+
+  getSession(sessionId: string): Promise<FunnelState> {
+    return this.garmr.execute(() => {
+      return fetchParse(`${this.url}/api/sessions/${sessionId}`, {
+        ...this.fetchOptions,
+        method: 'GET',
+      });
+    });
+  }
+}
+
+const garmrFreyjaService = new GarmrService({
+  service: FreyjaClient.name,
+  breakerOpts: {
+    halfOpenAfter: 5 * 1000,
+    threshold: 0.1,
+    duration: 10 * 1000,
+  },
+});
+
+export const freyjaClient = new FreyjaClient(process.env.FREYJA_ORIGIN!, {
+  garmr: garmrFreyjaService,
+});
---
+++ src/integrations/freyja/index.ts
@@ -0,0 +1,2 @@
+export * from './types';
+export { FreyjaClient, freyjaClient } from './clients';
