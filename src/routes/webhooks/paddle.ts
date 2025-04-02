6f2030f5e30177d0ce8095fdee3bcfc34f77cf9b
Lee Hansel Solevilla <13744167+sshanzel@users.noreply.github.com>
2025-04-03 00:01:59 +0800

    refactor: unnecessary check for property

--- src/routes/webhooks/paddle.ts
+++ src/routes/webhooks/paddle.ts
@@ -277,11 +277,12 @@ const notifyNewPaddleTransaction = async ({
 }: {
   event: TransactionCompletedEvent;
 }) => {
-  const { user_id, gifter_id } = (data?.customData ?? {}) as PaddleCustomData;
+  const { customData, subscriptionId } = data ?? {};
+  const { user_id, gifter_id } = (customData ?? {}) as PaddleCustomData;
   const purchasedById = gifter_id ?? user_id;
   const subscriptionForId = await getUserId({
     userId: user_id,
-    subscriptionId: 'subscriptionId' in data && data.subscriptionId,
+    subscriptionId,
   });
   const con = await createOrGetConnection();
   const flags = (
