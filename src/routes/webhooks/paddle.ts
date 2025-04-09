0da492a71c1b14887127e1d52e9e9cafb899ae69
Ante Barić <ante@kickass.website>
2025-04-09 08:43:19 +0200

    feat: adjust transaction processing (#2752)

--- src/routes/webhooks/paddle.ts
+++ src/routes/webhooks/paddle.ts
@@ -8,9 +8,9 @@ import {
   type SubscriptionCreatedEvent,
   type SubscriptionUpdatedEvent,
   type TransactionPaymentFailedEvent,
-  type TransactionReadyEvent,
   type TransactionUpdatedEvent,
   type TransactionPayoutTotalsNotification,
+  type TransactionPaidEvent,
 } from '@paddle/paddle-node-sdk';
 import createOrGetConnection from '../../db';
 import {
@@ -857,10 +857,10 @@ export const processTransactionCreated = async ({
   }
 };
 
-export const processTransactionReady = async ({
+export const processTransactionPaid = async ({
   event,
 }: {
-  event: TransactionReadyEvent;
+  event: TransactionPaidEvent;
 }) => {
   if (isCoreTransaction({ event })) {
     const transactionData = getPaddleTransactionData({ event });
@@ -880,7 +880,10 @@ export const processTransactionReady = async ({
         event,
         transaction,
         nextStatus,
-        validStatus: [UserTransactionStatus.Created],
+        validStatus: [
+          UserTransactionStatus.Created,
+          UserTransactionStatus.Processing,
+        ],
         data: transactionData,
       })
     ) {
@@ -989,8 +992,9 @@ export const processTransactionUpdated = async ({
 
       switch (event.data.status) {
         case 'draft':
-          return UserTransactionStatus.Created;
         case 'ready':
+          return UserTransactionStatus.Created;
+        case 'billed':
           return UserTransactionStatus.Processing;
         default:
           return undefined;
@@ -1049,8 +1053,8 @@ export const paddle = async (fastify: FastifyInstance): Promise<void> => {
                 });
 
                 break;
-              case EventName.TransactionReady:
-                await processTransactionReady({
+              case EventName.TransactionPaid:
+                await processTransactionPaid({
                   event: eventData,
                 });
 
