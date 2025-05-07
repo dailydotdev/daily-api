a27a992f8559ab749cbd6efc59cb3b055cdce645
capJavert <dev@kickass.website>
2025-05-07 15:06:57 +0200

    feat: add value sort index

--- src/migration/1746564832985-AwardIdIndex.ts
+++ src/migration/1746564832985-AwardIdIndex.ts
@@ -7,15 +7,23 @@ export class AwardIdIndex1746564832985 implements MigrationInterface {
     await queryRunner.query(
       `CREATE INDEX IF NOT EXISTS "idx_user_post_flags_awardId" ON "user_post" (("flags"->>'awardId'))`,
     );
+
     await queryRunner.query(
       `CREATE INDEX IF NOT EXISTS "idx_user_comment_flags_awardId" ON "user_comment" (("flags"->>'awardId'))`,
+    );
+
+    await queryRunner.query(
+      `CREATE INDEX IF NOT EXISTS "idx_product_value_desc" ON product ("value" DESC)`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX IF EXISTS "idx_product_value_desc"`);
+
     await queryRunner.query(
       `DROP INDEX IF EXISTS "idx_user_comment_flags_awardId"`,
     );
+
     await queryRunner.query(
       `DROP INDEX IF EXISTS "idx_user_post_flags_awardId"`,
     );
