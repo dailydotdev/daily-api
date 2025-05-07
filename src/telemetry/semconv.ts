bdc9eb98570eb7bc403b78d2a3849d1d658b9891
Ante Barić <ante@kickass.website>
2025-05-07 11:41:22 +0200

    revert: update dependencies telemetry (#2797)

--- src/telemetry/semconv.ts
+++
@@ -1,7 +0,0 @@
-export const ATTR_MESSAGING_DESTINATION_NAME = 'messaging.destination.name';
-
-export const ATTR_MESSAGING_MESSAGE_BODY_SIZE = 'messaging.message.body.size';
-
-export const ATTR_MESSAGING_MESSAGE_ID = 'messaging.message.id';
-
-export const ATTR_MESSAGING_SYSTEM = 'messaging.system';
