ffc388755bafd4373d143d13e50ae21f98ec9011
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-05-22 13:08:03 +0200

    feat: query single organization

--- __tests__/organizations.ts
+++ __tests__/organizations.ts
@@ -119,3 +119,83 @@ describe('query organizations', () => {
     });
   });
 });
+
+describe('query organization', () => {
+  const QUERY = /* GraphQL */ `
+    query Organization($id: ID!) {
+      organization(id: $id) {
+        role
+        referralToken
+        organization {
+          id
+          name
+          image
+          seats
+        }
+      }
+    }
+  `;
+
+  it('should not authorize when not logged-in', () =>
+    testQueryErrorCode(
+      client,
+      { query: QUERY, variables: { id: 'org-1' } },
+      'UNAUTHENTICATED',
+    ));
+
+  it('should return not found when organization does not exist', async () => {
+    loggedUser = '1';
+
+    testQueryErrorCode(
+      client,
+      { query: QUERY, variables: { id: 'non-existing' } },
+      'NOT_FOUND',
+    );
+  });
+
+  it('should return not found when user is not member of organization', async () => {
+    loggedUser = '1';
+
+    testQueryErrorCode(
+      client,
+      { query: QUERY, variables: { id: 'org-1' } },
+      'NOT_FOUND',
+    );
+  });
+
+  it('should return organization user is a member of', async () => {
+    loggedUser = '1';
+
+    await con.getRepository(Feed).save({
+      id: loggedUser,
+      userId: loggedUser,
+    });
+
+    await con.getRepository(ContentPreferenceOrganization).save({
+      userId: loggedUser,
+      referenceId: 'org-1',
+      organizationId: 'org-1',
+      feedId: loggedUser,
+      status: ContentPreferenceStatus.Follow,
+      flags: {
+        role: OrganizationMemberRoles.Owner,
+        referralToken: 'ref-token-1',
+      },
+    });
+
+    const { data } = await client.query(QUERY, {
+      variables: { id: 'org-1' },
+    });
+    expect(data).toMatchObject({
+      organization: {
+        role: 'owner',
+        referralToken: 'ref-token-1',
+        organization: {
+          id: 'org-1',
+          name: 'Organization 1',
+          seats: 1,
+        },
+      },
+    });
+  });
+});
