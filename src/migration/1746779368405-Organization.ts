99d18bec78c10f08b8b133855b0a3324eac80dfa
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-05-22 11:25:44 +0200

    feat: organization table

---
+++ src/migration/1746779368405-Organization.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Organization1746779368405 implements MigrationInterface {
+  name = 'Organization1746779368405'
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE TABLE "organization" ("id" text NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "name" text NOT NULL, "image" text, "seats" smallint NOT NULL DEFAULT '1', "subscriptionFlags" jsonb NOT NULL DEFAULT '{}', CONSTRAINT "PK_organization_organization_id" PRIMARY KEY ("id"))`);
+    await queryRunner.query(`ALTER TABLE "content_preference" ADD "organizationId" text`);
+    await queryRunner.query(`ALTER TABLE "content_preference" ADD CONSTRAINT "FK_content_preference_organization_id" FOREIGN KEY ("organizationId") REFERENCES "organization"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+    await queryRunner.query(`CREATE INDEX "IDX_content_preference_organization_id" ON "content_preference" ("organizationId") `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX "public"."IDX_content_preference_organization_id"`);
+    await queryRunner.query(`ALTER TABLE "content_preference" DROP CONSTRAINT "FK_content_preference_organization_id"`);
+    await queryRunner.query(`ALTER TABLE "content_preference" DROP COLUMN "organizationId"`);
+    await queryRunner.query(`DROP TABLE "organization"`);
+  }
+}
