f6c3480849ee343c0e376d1984d381a948ecf2e6
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-05-22 11:25:44 +0200

    feat: organization table

---
+++ src/entity/Organization.ts
@@ -0,0 +1,44 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import type { SubscriptionCycles } from '../paddle';
+import type { SubscriptionProvider, SubscriptionStatus } from '../common/plus';
+
+export type OrganizationSubscriptionFlags = Partial<{
+  subscriptionId: string;
+  cycle: SubscriptionCycles;
+  createdAt: Date;
+  provider: SubscriptionProvider;
+  status: SubscriptionStatus;
+}>;
+
+@Entity()
+export class Organization {
+  @PrimaryColumn({
+    type: 'text',
+    primaryKeyConstraintName: 'PK_organization_organization_id',
+  })
+  id: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column({ type: 'text' })
+  name: string;
+
+  @Column({ type: 'text', nullable: true })
+  image: string;
+
+  @Column({ type: 'smallint', default: 1 })
+  seats: number;
+
+  @Column({ type: 'jsonb', default: {} })
+  subscriptionFlags?: OrganizationSubscriptionFlags;
+}
