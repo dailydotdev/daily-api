f28667a9db851bd177b0d3511d264d17068a1acd
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-05-29 02:37:25 +0200

    fix: update type

--- src/schema/users.ts
+++ src/schema/users.ts
@@ -212,7 +212,7 @@ export interface GQLUser {
   language?: ContentLanguage | null;
   topReader?: GQLUserTopReader;
   coresRole: CoresRole;
-  isPlus: boolean;
+  isPlus?: boolean;
 }
 
 export interface GQLView {
