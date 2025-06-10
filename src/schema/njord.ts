3f26d7606eed42e12e428c0d5ac76a9355531270
Chris Bongers <chrisbongers@gmail.com>
2025-06-10 12:27:31 +0200

    feat: Squad awarding (#2853)

--- src/schema/njord.ts
+++ src/schema/njord.ts
@@ -3,17 +3,18 @@ import type { IResolvers } from '@graphql-tools/utils';
 import { traceResolvers } from './trace';
 import {
   awardComment,
+  type AwardInput,
   awardPost,
+  awardSquad,
   AwardType,
   awardUser,
-  type AwardInput,
   type GetBalanceResult,
   type TransactionCreated,
 } from '../common/njord';
 import { ForbiddenError, ValidationError } from 'apollo-server-errors';
 import { getLimit, toGQLEnum } from '../common';
 import { z } from 'zod';
-import { ProductType, type Product } from '../entity/Product';
+import { type Product, ProductType } from '../entity/Product';
 import type { Connection, ConnectionArguments } from 'graphql-relay';
 import { offsetPageGenerator } from './common';
 import graphorm from '../graphorm';
@@ -112,6 +113,8 @@ export const typeDefs = /* GraphQL */ `
   type UserTransactionFlagsPublic {
     note: String
     error: String
+    sourceId: String
+    sourceName: String
   }
 
   type UserTransaction {
@@ -128,6 +131,7 @@ export const typeDefs = /* GraphQL */ `
     balance: UserBalance!
     createdAt: DateTime!
     valueIncFees: Int!
+    sourceName: String
   }
 
   type UserTransactionSummary {
@@ -499,6 +503,8 @@ export const resolvers: IResolvers<unknown, BaseContext> = traceResolvers<
           return awardUser(props, ctx);
         case AwardType.Comment:
           return awardComment(props, ctx);
+        case AwardType.Squad:
+          return awardSquad(props, ctx);
         default:
           throw new ForbiddenError('Can not award this entity');
       }
