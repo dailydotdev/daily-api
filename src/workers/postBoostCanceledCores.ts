6c97b0b78756445dec751cf6b2e2a4f393cf3749
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-07-02 22:52:07 +0800

    tmp: do not refund cancelation until we hit deployment

--- src/workers/postBoostCanceledCores.ts
+++ src/workers/postBoostCanceledCores.ts
@@ -1,89 +1,97 @@
-import { randomUUID } from 'node:crypto';
-import { systemUser } from '../common';
-import { transferCores, usdToCores } from '../common/njord';
-import { Post } from '../entity';
-import {
-  UserTransaction,
-  UserTransactionProcessor,
-  UserTransactionStatus,
-  UserTransactionType,
-} from '../entity/user/UserTransaction';
+// import { randomUUID } from 'node:crypto';
+// import { systemUser } from '../common';
+// import { transferCores, usdToCores } from '../common/njord';
+// import { Post } from '../entity';
+// import {
+//   UserTransaction,
+//   UserTransactionProcessor,
+//   UserTransactionStatus,
+//   UserTransactionType,
+// } from '../entity/user/UserTransaction';
 import { TypedWorker } from './worker';
 
 const worker: TypedWorker<'api.v1.post-boost-canceled'> = {
   subscription: 'api.post-boost-canceled-cores',
   handler: async (message, con, logger): Promise<void> => {
     const { data } = message;
-    const { userId, postId, refundAmountUsd, campaignId } = data;
-    const toRefund = parseFloat(refundAmountUsd);
+    const { campaignId } = data;
+    // const { userId, postId, refundAmountUsd, campaignId } = data;
+    // const toRefund = parseFloat(refundAmountUsd);
 
-    if (toRefund < 0) {
-      logger.error(
-        {
-          data,
-          messageId: message.messageId,
-        },
-        'Cannot accept negative value for refund',
-      );
-      return;
-    }
+    logger.info({
+      data,
+      transactionId: campaignId,
+      messageId: message.messageId,
+      note: 'testing: post boost canceled',
+    });
 
-    try {
-      const { transactionId } = await con.transaction(async (entityManager) => {
-        const post = await entityManager
-          .getRepository(Post)
-          .findOneBy({ id: postId });
+    // if (toRefund < 0) {
+    //   logger.error(
+    //     {
+    //       data,
+    //       messageId: message.messageId,
+    //     },
+    //     'Cannot accept negative value for refund',
+    //   );
+    //   return;
+    // }
 
-        if (!post) {
-          return {};
-        }
+    // try {
+    //   const { transactionId } = await con.transaction(async (entityManager) => {
+    //     const post = await entityManager
+    //       .getRepository(Post)
+    //       .findOneBy({ id: postId });
 
-        const userTransaction = await entityManager
-          .getRepository(UserTransaction)
-          .save(
-            entityManager.getRepository(UserTransaction).create({
-              id: randomUUID(),
-              processor: UserTransactionProcessor.Njord,
-              receiverId: userId,
-              status: UserTransactionStatus.Success,
-              productId: null,
-              senderId: systemUser.id,
-              value: usdToCores(toRefund),
-              valueIncFees: 0,
-              fee: 0,
-              flags: { note: 'Post boost canceled' },
-              referenceId: campaignId,
-              referenceType: UserTransactionType.PostBoost,
-            }),
-          );
+    //     if (!post) {
+    //       return {};
+    //     }
 
-        await transferCores({
-          ctx: { userId },
-          transaction: userTransaction,
-          entityManager,
-        });
+    //     const userTransaction = await entityManager
+    //       .getRepository(UserTransaction)
+    //       .save(
+    //         entityManager.getRepository(UserTransaction).create({
+    //           id: randomUUID(),
+    //           processor: UserTransactionProcessor.Njord,
+    //           receiverId: userId,
+    //           status: UserTransactionStatus.Success,
+    //           productId: null,
+    //           senderId: systemUser.id,
+    //           value: usdToCores(toRefund),
+    //           valueIncFees: 0,
+    //           fee: 0,
+    //           flags: { note: 'Post boost canceled' },
+    //           referenceId: campaignId,
+    //           referenceType: UserTransactionType.PostBoost,
+    //         }),
+    //       );
 
-        return { transactionId: userTransaction.id };
-      });
+    //     await transferCores({
+    //       ctx: { userId },
+    //       transaction: userTransaction,
+    //       entityManager,
+    //     });
 
-      logger.info(
-        {
-          data,
-          transactionId,
-          messageId: message.messageId,
-        },
-        'post boost canceled, cores transferred',
-      );
-    } catch (err) {
-      logger.error(
-        {
-          data,
-          messageId: message.messageId,
-          err,
-        },
-        'failed to transfer cores for post boost cancelation',
-      );
-    }
+    //     return { transactionId: userTransaction.id };
+    //   });
+
+    //   logger.info(
+    //     {
+    //       data,
+    //       transactionId,
+    //       messageId: message.messageId,
+    //     },
+    //     'post boost canceled, cores transferred',
+    //   );
+    // } catch (err) {
+    //   logger.error(
+    //     {
+    //       data,
+    //       messageId: message.messageId,
+    //       err,
+    //     },
+    //     'failed to transfer cores for post boost cancelation',
+    //   );
+    // }
   },
 };
 
