83cc73494ec2c4cd4b08371ef79ed7ec23f9b1fd
capJavert <dev@kickass.website>
2025-07-14 19:59:12 +0200

    fix: null tests

--- __tests__/cron/dailyDigest.ts
+++ __tests__/cron/dailyDigest.ts
@@ -591,6 +591,31 @@ describe('dailyDigest cron', () => {
     expect(notifyGeneratePersonalizedDigest).toHaveBeenCalledTimes(4);
   });
 
+  it('should schedule generation for brief if lastSendDate is null', async () => {
+    const usersToSchedule = usersFixture;
+
+    await con.getRepository(UserPersonalizedDigest).save(
+      usersToSchedule.map((item) => ({
+        userId: item.id,
+        preferredDay,
+        preferredHour: fakePreferredHour,
+        type: UserPersonalizedDigestType.Brief,
+        flags: {
+          sendType,
+        },
+        lastSendDate: null as unknown as Date,
+      })),
+    );
+
+    jest.spyOn(briefFeedClient, 'getBriefLastUpdate').mockResolvedValue({
+      updatedAt: new Date('2024-09-11T06:00:42.680Z'),
+    });
+
+    await expectSuccessfulCron(cron);
+
+    expect(notifyGeneratePersonalizedDigest).toHaveBeenCalledTimes(4);
+  });
+
   it('should schedule generation for other digest if brief feed is not updated', async () => {
     const usersToSchedule = usersFixture;
 
--- __tests__/cron/personalizedDigest.ts
+++ __tests__/cron/personalizedDigest.ts
@@ -513,6 +513,36 @@ describe('personalizedDigest cron', () => {
     expect(notifyGeneratePersonalizedDigest).toHaveBeenCalledTimes(4);
   });
 
+  it('should schedule generation for brief if lastSendDate is null', async () => {
+    const { fakePreferredDay, fakePreferredHour } = fakeSendDate(
+      new Date('2024-09-11T10:32:42.680Z'),
+      9,
+    );
+
+    const usersToSchedule = usersFixture;
+
+    await con.getRepository(UserPersonalizedDigest).save(
+      usersToSchedule.map((item) => ({
+        userId: item.id,
+        preferredDay: fakePreferredDay,
+        preferredHour: fakePreferredHour,
+        type: UserPersonalizedDigestType.Brief,
+        flags: {
+          sendType,
+        },
+        lastSendDate: null as unknown as Date,
+      })),
+    );
+
+    jest.spyOn(briefFeedClient, 'getBriefLastUpdate').mockResolvedValue({
+      updatedAt: new Date('2024-09-11T06:00:42.680Z'),
+    });
+
+    await expectSuccessfulCron(cron);
+
+    expect(notifyGeneratePersonalizedDigest).toHaveBeenCalledTimes(4);
+  });
+
   it('should schedule generation for other digest if brief feed is not updated', async () => {
     const { fakePreferredDay, fakePreferredHour } = fakeSendDate(
       new Date('2024-09-11T10:32:42.680Z'),
