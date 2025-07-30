98fba6b6055b2eaedcb26e86bab39326fef0e813
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-07-30 16:57:54 +0200

    fix: always reject submissions (#2952)

--- src/schema/submissions.ts
+++ src/schema/submissions.ts
@@ -1,17 +1,10 @@
-import { DataSource } from 'typeorm';
-import { Submission, User } from './../entity';
+import { User } from './../entity';
 import { IResolvers } from '@graphql-tools/utils';
 import { traceResolvers } from './trace';
-import { AuthContext, BaseContext, Context } from '../Context';
-import { getExistingPost, isValidHttpUrl, standardizeURL } from '../common';
-import { getPostByUrl, GQLPost } from './posts';
+import { BaseContext } from '../Context';
+import { GQLPost } from './posts';
 import { SubmissionFailErrorMessage } from '../errors';
-import { checkWithVordr, VordrFilterType } from '../common/vordr';
-import { submissionAccessThreshold, submissionLimit } from '../config';
-
-interface GQLArticleSubmission {
-  url: string;
-}
+import { submissionAccessThreshold } from '../config';
 
 type GQLSubmitArticleResponse = {
   result: 'succeed' | 'exists' | 'rejected';
@@ -63,155 +56,46 @@ export const typeDefs = /* GraphQL */ `
     Information regarding the access of submitting community links
     """
     submissionAvailability: SubmissionAvailability!
+      @deprecated(
+        reason: "This feature is deprecated, please use the new submission process"
+      )
   }
   extend type Mutation {
     """
     Submit an article to surface on users feed
     """
-    submitArticle(url: String!): SubmitArticle @auth
+    submitArticle(url: String!): SubmitArticle
+      @auth
+      @deprecated(
+        reason: "This feature is deprecated, please use the new submission process"
+      )
   }
 `;
 
 export const hasSubmissionAccess = (user: User) =>
   user.reputation >= submissionAccessThreshold;
 
-const getSubmissionsToday = (con: DataSource, user: User) => {
-  const timezone = user.timezone ?? 'utc';
-  const atTimezone = `at time zone '${timezone}'`;
-
-  return con
-    .getRepository(Submission)
-    .createQueryBuilder()
-    .select('*')
-    .where(
-      `date_trunc('day', timezone('UTC', "createdAt") ${atTimezone})::timestamptz = date_trunc('day', now() ${atTimezone})::timestamptz`,
-    )
-    .andWhere('"userId" = :id', { id: user.id })
-    .execute();
-};
-
 export const resolvers: IResolvers<unknown, BaseContext> = traceResolvers<
   unknown,
   BaseContext
 >({
   Query: {
-    submissionAvailability: async (
-      _,
-      __,
-      ctx: Context,
-    ): Promise<GQLSubmissionAvailability> => {
-      const user = ctx.userId
-        ? await ctx.getRepository(User).findOneBy({ id: ctx.userId })
-        : null;
-
-      if (!user) {
-        return {
-          limit: submissionLimit,
-          hasAccess: false,
-          todaySubmissionsCount: 0,
-        };
-      }
-
-      const submissionsToday = await getSubmissionsToday(ctx.con, user);
-
+    // Feature deprecated, will be removed in the future
+    submissionAvailability: async (): Promise<GQLSubmissionAvailability> => {
       return {
-        limit: submissionLimit,
-        todaySubmissionsCount: submissionsToday.length,
-        hasAccess: hasSubmissionAccess(user),
+        limit: 0,
+        hasAccess: false,
+        todaySubmissionsCount: 0,
       };
     },
   },
   Mutation: {
-    submitArticle: async (
-      _,
-      { url }: GQLArticleSubmission,
-      ctx: AuthContext,
-      info,
-    ): Promise<GQLSubmitArticleResponse> => {
-      const user = await ctx
-        .getRepository(User)
-        .findOneByOrFail({ id: ctx.userId });
-
-      const submissionRepo = ctx.con.getRepository(Submission);
-
-      if (!hasSubmissionAccess(user)) {
-        return {
-          result: 'rejected',
-          reason: SubmissionFailErrorMessage.ACCESS_DENIED,
-        };
-      }
-
-      const limit = ctx.isTeamMember ? Infinity : submissionLimit;
-      const submissionsToday = await getSubmissionsToday(ctx.con, user);
-
-      if (submissionsToday.length >= limit) {
-        return {
-          result: 'rejected',
-          reason: SubmissionFailErrorMessage.LIMIT_REACHED,
-        };
-      }
-
-      if (!isValidHttpUrl(url)) {
-        return {
-          result: 'rejected',
-          reason: SubmissionFailErrorMessage.INVALID_URL,
-        };
-      }
-
-      const { url: cleanUrl, canonicalUrl } = standardizeURL(url);
-
-      const existingPost = await getExistingPost(ctx.con, {
-        url: cleanUrl,
-        canonicalUrl,
-      });
-
-      if (existingPost) {
-        if (existingPost.deleted || !existingPost.visible) {
-          return {
-            result: 'rejected',
-            reason: SubmissionFailErrorMessage.POST_DELETED,
-          };
-        }
-
-        return {
-          result: 'exists',
-          post: await getPostByUrl(ctx, info, {
-            url: cleanUrl,
-            canonicalUrl,
-          }),
-        };
-      }
-
-      const existingSubmission = await submissionRepo.findOneBy({
-        url: cleanUrl,
-      });
-
-      if (existingSubmission) {
-        return {
-          result: 'rejected',
-          reason:
-            SubmissionFailErrorMessage[
-              `EXISTS_${existingSubmission.status}` as keyof typeof SubmissionFailErrorMessage
-            ],
-        };
-      }
-
-      const createdSubmission = submissionRepo.create({
-        url: cleanUrl,
-        userId: ctx.userId,
-      });
-
-      createdSubmission.flags = {
-        ...createdSubmission.flags,
-        vordr: await checkWithVordr(
-          { id: createdSubmission.id, type: VordrFilterType.Submission },
-          ctx,
-        ),
+    // Feature deprecated, will be removed in the future
+    submitArticle: async (): Promise<GQLSubmitArticleResponse> => {
+      return {
+        result: 'rejected',
+        reason: SubmissionFailErrorMessage.COMMUNITY_PICKS_DEPRECATED,
       };
-
-      const submission = await submissionRepo.save(createdSubmission);
-
-      return { result: 'succeed', submission };
     },
   },
 });
