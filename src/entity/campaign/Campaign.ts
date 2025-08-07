7340dcd22768e8aefc63dfb366121ff9da4dcee7
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-08-08 00:32:21 +0800

    feat: campaign entity

---
+++ src/entity/campaign/Campaign.ts
@@ -0,0 +1,64 @@
+import {
+  Column,
+  Entity,
+  Index,
+  ManyToOne,
+  PrimaryColumn,
+  TableInheritance,
+} from 'typeorm';
+import { User } from '../user';
+
+export enum CampaignType {
+  Post = 'post',
+  Source = 'source',
+}
+
+export enum CampaignState {
+  Pending = 'pending',
+  Active = 'active',
+  Completed = 'completed',
+  Cancelled = 'cancelled',
+}
+
+export interface CampaignFlags {
+  budget: number;
+  spend: number;
+  impressions: number;
+  clicks: number;
+  users: number;
+}
+
+@Entity()
+@TableInheritance({ column: { type: 'varchar', name: 'type' } })
+export class Campaign {
+  @PrimaryColumn({ type: 'uuid', generated: 'uuid' })
+  id: string;
+
+  @Column({ type: 'text' })
+  referenceId: string;
+
+  @Column({ type: 'text' })
+  userId: string;
+
+  @ManyToOne('User', {
+    lazy: true,
+    onDelete: 'CASCADE',
+  })
+  user: Promise<User>;
+
+  @Column({ type: 'text' })
+  @Index('IDX_campaign_type')
+  type: CampaignType;
+
+  @Column({ default: () => 'now()' })
+  createdAt: Date;
+
+  @Column()
+  endedAt: Date;
+
+  @Column({ type: 'text' })
+  state: CampaignState;
+
+  @Column({ type: 'jsonb', default: {} })
+  flags: CampaignFlags;
+}
---
+++ src/migration/1754583578514-CampaignEntity.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CampaignEntity1754583578514 implements MigrationInterface {
+  name = 'CampaignEntity1754583578514';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "campaign" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "referenceId" text NOT NULL, "userId" character varying NOT NULL, "type" text NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "endedAt" TIMESTAMP NOT NULL, "state" text NOT NULL, "flags" jsonb NOT NULL DEFAULT '{}', "postId" text, "sourceId" text, CONSTRAINT "PK_0ce34d26e7f2eb316a3a592cdc4" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_campaign_type" ON "campaign" ("type") `,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "campaign" ADD CONSTRAINT "FK_8e2dc400e55e237feba0869bc02" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "campaign" ADD CONSTRAINT "FK_9074c52d57e727dda6591943b10" FOREIGN KEY ("postId") REFERENCES "post"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "campaign" ADD CONSTRAINT "FK_a8102fd41bef084f19474e97953" FOREIGN KEY ("sourceId") REFERENCES "source"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "campaign" ADD CONSTRAINT "FK_8e2dc400e55e237feba0869bc02" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "campaign" ADD CONSTRAINT "FK_9074c52d57e727dda6591943b10" FOREIGN KEY ("postId") REFERENCES "post"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "campaign" ADD CONSTRAINT "FK_a8102fd41bef084f19474e97953" FOREIGN KEY ("sourceId") REFERENCES "source"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(`DROP INDEX "public"."IDX_campaign_type"`);
+    await queryRunner.query(`DROP TABLE "campaign"`);
+  }
+}
