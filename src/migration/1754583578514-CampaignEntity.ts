cffc1e6510f646eb1fd2a32cd063fb2af4e34b4b
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-08-08 18:55:24 +0800

    refactor: entity

--- src/migration/1754583578514-CampaignEntity.ts
+++ src/migration/1754583578514-CampaignEntity.ts
@@ -19,9 +19,19 @@ export class CampaignEntity1754583578514 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "campaign" ADD CONSTRAINT "FK_a8102fd41bef084f19474e97953" FOREIGN KEY ("sourceId") REFERENCES "source"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
     );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_campaign_state_created_at_sort" ON "campaign" ((CASE WHEN state = 'active' THEN 0 ELSE 1 END), "createdAt" DESC) `,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "campaign" ADD "updatedAt" TIMESTAMP NOT NULL DEFAULT now()`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_campaign_state_created_at_sort"`,
+    );
+    await queryRunner.query(`ALTER TABLE "campaign" DROP COLUMN "updatedAt"`);
     await queryRunner.query(
       `ALTER TABLE "campaign" ADD CONSTRAINT "FK_8e2dc400e55e237feba0869bc02" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
     );
