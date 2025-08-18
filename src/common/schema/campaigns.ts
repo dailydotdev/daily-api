729b0a7835f0a6ab85d4761eeb8b41b6717fbafb
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-08-19 00:29:16 +0800

    refactor: unnecessary check

--- src/common/schema/campaigns.ts
+++ src/common/schema/campaigns.ts
@@ -9,12 +9,5 @@ export const CAMPAIGN_VALIDATION_SCHEMA = z.object({
     .refine((value) => value % 1000 === 0, {
       message: 'Budget must be divisible by 1000',
     }),
-  duration: z
-    .number()
-    .int()
-    .min(1)
-    .max(30)
-    .refine((value) => value % 1 === 0, {
-      message: 'Duration must be a whole number',
-    }),
+  duration: z.number().int().min(1).max(30),
 });
