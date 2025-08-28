73577a3aa8ead9c9fc2228fd821a2b3efe933d7e
capJavert <dev@kickass.website>
2025-08-28 12:21:44 +0200

    feat: upgrade migration

--- src/migration/1756375302904-ChMigration.ts
+++ src/migration/1756375302904-ChMigration.ts
@@ -5,11 +5,11 @@ export class ChMigration1756375302904 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "migrations_ch" ("id" bigint NOT NULL, "name" character varying NOT NULL, "timestamp" TIMESTAMP NOT NULL DEFAULT now(), "dirty" boolean NOT NULL, CONSTRAINT "PK_b0fa9708b4cd4f2ade3bf4d3e56" PRIMARY KEY ("id"))`,
+      `CREATE TABLE IF NOT EXISTS "migrations_ch" ("id" bigint NOT NULL, "name" character varying NOT NULL, "timestamp" TIMESTAMP NOT NULL DEFAULT now(), "dirty" boolean NOT NULL, CONSTRAINT "PK_b0fa9708b4cd4f2ade3bf4d3e56" PRIMARY KEY ("id"))`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "migrations_ch"`);
+    await queryRunner.query(`DROP TABLE IF EXISTS "migrations_ch"`);
   }
 }
