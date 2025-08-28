493d9b652ca01caf1b10dc530df28c27eddf1613
capJavert <dev@kickass.website>
2025-08-28 17:46:18 +0200

    feat: post analytics cron tests

--- __tests__/cron/postAnalyticsClickhouse.ts
+++ __tests__/cron/postAnalyticsClickhouse.ts
@@ -1,10 +1,224 @@
 import { crons } from '../../src/cron/index';
 import { postAnalyticsClickhouseCron as cron } from '../../src/cron/postAnalyticsClickhouse';
+import { expectSuccessfulCron } from '../helpers';
+import { postAnalyticsFixture } from '../fixture/postAnalytics';
+import {
+  mockClickhouseClientOnce,
+  mockClickhouseQueryJSONOnce,
+} from '../common/clickhouse';
+import createOrGetConnection from '../../src/db';
+import type { DataSource } from 'typeorm';
+import { PostAnalytics } from '../../src/entity/posts/PostAnalytics';
+import { deleteRedisKey, getRedisHash } from '../../src/redis';
+import { generateStorageKey, StorageTopic } from '../../src/config';
+import { format, startOfToday } from 'date-fns';
+
+let con: DataSource;
+
+beforeAll(async () => {
+  con = await createOrGetConnection();
+});
+
+const cronConfigRedisKey = generateStorageKey(
+  StorageTopic.Cron,
+  cron.name,
+  'config',
+);
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  await deleteRedisKey(cronConfigRedisKey);
+});
 
 describe('postAnalyticsClickhouse cron', () => {
   it('should be registered', () => {
     const registeredWorker = crons.find((item) => item.name === cron.name);
 
     expect(registeredWorker).toBeDefined();
+  });
+
+  it('should sync post analytics data', async () => {
+    const clickhouseClientMock = mockClickhouseClientOnce();
+
+    const queryJSONSpy = mockClickhouseQueryJSONOnce(
+      clickhouseClientMock,
+      postAnalyticsFixture.map((item, index) => {
+        return {
+          ...item,
+          updatedAt: new Date(Date.now() + index * 1000).toISOString(),
+          id: `pap-${index}`,
+        };
+      }),
+    );
+
+    const now = new Date();
+
+    await expectSuccessfulCron(cron);
+
+    expect(queryJSONSpy).toHaveBeenCalledTimes(1);
+    expect(queryJSONSpy).toHaveBeenCalledWith({
+      query: expect.stringContaining('SELECT'),
+      format: 'JSONEachRow',
+      query_params: {
+        lastRunAt: format(startOfToday(), 'yyyy-MM-dd HH:mm:ss'),
+      },
+    });
+
+    const postAnalytics = await con.getRepository(PostAnalytics).find({
+      order: {
+        updatedAt: 'ASC',
+      },
+    });
+
+    expect(postAnalytics.length).toBe(postAnalyticsFixture.length);
+
+    postAnalytics.forEach((item, index) => {
+      expect(item).toEqual({
+        id: `pap-${index}`,
+        updatedAt: expect.any(Date),
+        createdAt: expect.any(Date),
+        ...postAnalyticsFixture[index],
+        awards: 0,
+        comments: 0,
+        coresEarned: 0,
+        downvotes: 0,
+        reputation: 0,
+        upvotes: 0,
+      } as PostAnalytics);
+    });
+
+    const cronConfig = await getRedisHash(cronConfigRedisKey);
+
+    expect(cronConfig).toBeDefined();
+    expect(cronConfig.lastRunAt).toBeDefined();
+    expect(new Date(cronConfig.lastRunAt).getTime()).toBeGreaterThan(
+      now.getTime(),
+    );
+  });
+
+  it('should use lastRunAt from previous run', async () => {
+    let clickhouseClientMock = mockClickhouseClientOnce();
+
+    mockClickhouseQueryJSONOnce(
+      clickhouseClientMock,
+      postAnalyticsFixture.map((item, index) => {
+        return {
+          ...item,
+          updatedAt: new Date(Date.now() + index * 1000).toISOString(),
+          id: `pap-${index}`,
+        };
+      }),
+    );
+
+    await expectSuccessfulCron(cron);
+
+    const postAnalytics = await con.getRepository(PostAnalytics).find({
+      order: {
+        updatedAt: 'ASC',
+      },
+    });
+
+    expect(postAnalytics.length).toBe(postAnalyticsFixture.length);
+
+    postAnalytics.forEach((item, index) => {
+      expect(item).toEqual({
+        id: `pap-${index}`,
+        updatedAt: expect.any(Date),
+        createdAt: expect.any(Date),
+        ...postAnalyticsFixture[index],
+        awards: 0,
+        comments: 0,
+        coresEarned: 0,
+        downvotes: 0,
+        reputation: 0,
+        upvotes: 0,
+      } as PostAnalytics);
+    });
+
+    const lastCronConfig = await getRedisHash(cronConfigRedisKey);
+
+    clickhouseClientMock = mockClickhouseClientOnce();
+
+    const queryJSONSpy = mockClickhouseQueryJSONOnce(
+      clickhouseClientMock,
+      postAnalyticsFixture.map((item, index) => {
+        return {
+          ...item,
+          updatedAt: new Date(Date.now() + index * 1000).toISOString(),
+          id: `pap-${index}`,
+        };
+      }),
+    );
+
+    await expectSuccessfulCron(cron);
+
+    expect(queryJSONSpy).toHaveBeenCalledTimes(1);
+    expect(queryJSONSpy).toHaveBeenCalledWith({
+      query: expect.stringContaining('SELECT'),
+      format: 'JSONEachRow',
+      query_params: {
+        lastRunAt: format(
+          new Date(lastCronConfig.lastRunAt),
+          'yyyy-MM-dd HH:mm:ss',
+        ),
+      },
+    });
+
+    const cronConfig = await getRedisHash(cronConfigRedisKey);
+
+    expect(cronConfig).toBeDefined();
+    expect(cronConfig.lastRunAt).toBeDefined();
+    expect(new Date(cronConfig.lastRunAt).getTime()).toBeGreaterThan(
+      new Date(lastCronConfig.lastRunAt).getTime(),
+    );
+  });
+
+  it('should upsert new post analytics data on repeated runs', async () => {
+    let clickhouseClientMock = mockClickhouseClientOnce();
+
+    mockClickhouseQueryJSONOnce(
+      clickhouseClientMock,
+      postAnalyticsFixture.map((item, index) => {
+        return {
+          ...item,
+          updatedAt: new Date(Date.now() + index * 1000).toISOString(),
+          id: `pap-${index}`,
+        };
+      }),
+    );
+
+    await expectSuccessfulCron(cron);
+
+    clickhouseClientMock = mockClickhouseClientOnce();
+
+    mockClickhouseQueryJSONOnce(
+      clickhouseClientMock,
+      postAnalyticsFixture.map((item, index) => {
+        return {
+          ...item,
+          updatedAt: new Date(Date.now() + index * 1000).toISOString(),
+          id: `pap-${index}`,
+          impressions: item.impressions + 10,
+        };
+      }),
+    );
+
+    await expectSuccessfulCron(cron);
+
+    const postAnalytics = await con.getRepository(PostAnalytics).find({
+      select: ['id', 'impressions'],
+      order: {
+        updatedAt: 'ASC',
+      },
+    });
+
+    expect(postAnalytics.length).toBe(postAnalyticsFixture.length);
+
+    postAnalytics.forEach((item, index) => {
+      expect(item).toEqual({
+        id: `pap-${index}`,
+        impressions: postAnalyticsFixture[index].impressions + 10,
+      } as PostAnalytics);
+    });
   });
 });
--- __tests__/cron/postAnalyticsHistoryDayClickhouse.ts
+++ __tests__/cron/postAnalyticsHistoryDayClickhouse.ts
@@ -1,10 +1,225 @@
 import { crons } from '../../src/cron/index';
 import { postAnalyticsHistoryDayClickhouseCron as cron } from '../../src/cron/postAnalyticsHistoryDayClickhouse';
 
+import { expectSuccessfulCron } from '../helpers';
+import { postAnalyticsFixture } from '../fixture/postAnalytics';
+import {
+  mockClickhouseClientOnce,
+  mockClickhouseQueryJSONOnce,
+} from '../common/clickhouse';
+import createOrGetConnection from '../../src/db';
+import type { DataSource } from 'typeorm';
+import { deleteRedisKey, getRedisHash } from '../../src/redis';
+import { generateStorageKey, StorageTopic } from '../../src/config';
+import { format, startOfToday } from 'date-fns';
+import { PostAnalyticsHistory } from '../../src/entity/posts/PostAnalyticsHistory';
+
+let con: DataSource;
+
+beforeAll(async () => {
+  con = await createOrGetConnection();
+});
+
+const cronConfigRedisKey = generateStorageKey(
+  StorageTopic.Cron,
+  cron.name,
+  'config',
+);
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  await deleteRedisKey(cronConfigRedisKey);
+});
+
 describe('postAnalyticsHistoryDayClickhouse cron', () => {
   it('should be registered', () => {
     const registeredWorker = crons.find((item) => item.name === cron.name);
 
     expect(registeredWorker).toBeDefined();
+  });
+
+  it('should sync post analytics data', async () => {
+    const clickhouseClientMock = mockClickhouseClientOnce();
+    const date = format(new Date(), 'yyyy-MM-dd');
+
+    const queryJSONSpy = mockClickhouseQueryJSONOnce(
+      clickhouseClientMock,
+      postAnalyticsFixture.map((item, index) => {
+        return {
+          updatedAt: new Date(Date.now() + index * 1000).toISOString(),
+          id: `pap-${index}`,
+          date,
+          impressions: item.impressions,
+        };
+      }),
+    );
+
+    const now = new Date();
+
+    await expectSuccessfulCron(cron);
+
+    expect(queryJSONSpy).toHaveBeenCalledTimes(1);
+    expect(queryJSONSpy).toHaveBeenCalledWith({
+      query: expect.stringContaining('SELECT'),
+      format: 'JSONEachRow',
+      query_params: {
+        date,
+        lastRunAt: format(startOfToday(), 'yyyy-MM-dd HH:mm:ss'),
+      },
+    });
+
+    const postAnalytics = await con.getRepository(PostAnalyticsHistory).find({
+      order: {
+        updatedAt: 'ASC',
+      },
+    });
+
+    expect(postAnalytics.length).toBe(postAnalyticsFixture.length);
+
+    postAnalytics.forEach((item, index) => {
+      expect(item).toEqual({
+        id: `pap-${index}`,
+        updatedAt: expect.any(Date),
+        createdAt: expect.any(Date),
+        impressions: postAnalyticsFixture[index].impressions,
+        date,
+      } as PostAnalyticsHistory);
+    });
+
+    const cronConfig = await getRedisHash(cronConfigRedisKey);
+
+    expect(cronConfig).toBeDefined();
+    expect(cronConfig.lastRunAt).toBeDefined();
+    expect(new Date(cronConfig.lastRunAt).getTime()).toBeGreaterThan(
+      now.getTime(),
+    );
+  });
+
+  it('should use lastRunAt from previous run', async () => {
+    let clickhouseClientMock = mockClickhouseClientOnce();
+    const date = format(new Date(), 'yyyy-MM-dd');
+
+    mockClickhouseQueryJSONOnce(
+      clickhouseClientMock,
+      postAnalyticsFixture.map((item, index) => {
+        return {
+          updatedAt: new Date(Date.now() + index * 1000).toISOString(),
+          id: `pap-${index}`,
+          date,
+          impressions: item.impressions,
+        };
+      }),
+    );
+
+    await expectSuccessfulCron(cron);
+
+    const postAnalytics = await con.getRepository(PostAnalyticsHistory).find({
+      order: {
+        updatedAt: 'ASC',
+      },
+    });
+
+    expect(postAnalytics.length).toBe(postAnalyticsFixture.length);
+
+    postAnalytics.forEach((item, index) => {
+      expect(item).toEqual({
+        id: `pap-${index}`,
+        updatedAt: expect.any(Date),
+        createdAt: expect.any(Date),
+        impressions: postAnalyticsFixture[index].impressions,
+        date,
+      } as PostAnalyticsHistory);
+    });
+
+    const lastCronConfig = await getRedisHash(cronConfigRedisKey);
+
+    clickhouseClientMock = mockClickhouseClientOnce();
+
+    const queryJSONSpy = mockClickhouseQueryJSONOnce(
+      clickhouseClientMock,
+      postAnalyticsFixture.map((item, index) => {
+        return {
+          updatedAt: new Date(Date.now() + index * 1000).toISOString(),
+          id: `pap-${index}`,
+          date,
+          impressions: item.impressions,
+        };
+      }),
+    );
+
+    await expectSuccessfulCron(cron);
+
+    expect(queryJSONSpy).toHaveBeenCalledTimes(1);
+    expect(queryJSONSpy).toHaveBeenCalledWith({
+      query: expect.stringContaining('SELECT'),
+      format: 'JSONEachRow',
+      query_params: {
+        date,
+        lastRunAt: format(
+          new Date(lastCronConfig.lastRunAt),
+          'yyyy-MM-dd HH:mm:ss',
+        ),
+      },
+    });
+
+    const cronConfig = await getRedisHash(cronConfigRedisKey);
+
+    expect(cronConfig).toBeDefined();
+    expect(cronConfig.lastRunAt).toBeDefined();
+    expect(new Date(cronConfig.lastRunAt).getTime()).toBeGreaterThan(
+      new Date(lastCronConfig.lastRunAt).getTime(),
+    );
+  });
+
+  it('should upsert new post analytics data on repeated runs', async () => {
+    let clickhouseClientMock = mockClickhouseClientOnce();
+    const date = format(new Date(), 'yyyy-MM-dd');
+
+    mockClickhouseQueryJSONOnce(
+      clickhouseClientMock,
+      postAnalyticsFixture.map((item, index) => {
+        return {
+          updatedAt: new Date(Date.now() + index * 1000).toISOString(),
+          id: `pap-${index}`,
+          date,
+          impressions: item.impressions,
+        };
+      }),
+    );
+
+    await expectSuccessfulCron(cron);
+
+    clickhouseClientMock = mockClickhouseClientOnce();
+
+    mockClickhouseQueryJSONOnce(
+      clickhouseClientMock,
+      postAnalyticsFixture.map((item, index) => {
+        return {
+          updatedAt: new Date(Date.now() + index * 1000).toISOString(),
+          id: `pap-${index}`,
+          date,
+          impressions: item.impressions + 10,
+        };
+      }),
+    );
+
+    await expectSuccessfulCron(cron);
+
+    const postAnalytics = await con.getRepository(PostAnalyticsHistory).find({
+      select: ['id', 'impressions', 'date'],
+      order: {
+        updatedAt: 'ASC',
+      },
+    });
+
+    expect(postAnalytics.length).toBe(postAnalyticsFixture.length);
+
+    postAnalytics.forEach((item, index) => {
+      expect(item).toEqual({
+        id: `pap-${index}`,
+        date,
+        impressions: postAnalyticsFixture[index].impressions + 10,
+      } as PostAnalyticsHistory);
+    });
   });
 });
