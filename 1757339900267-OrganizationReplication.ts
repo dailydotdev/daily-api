78161fbedd05c442eafe0fd127407f23790bb872
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-09-08 16:04:38 +0200

    fix: also alter opportunity_match for future

--- 1757339900267-OrganizationReplication.ts
+++ 1757339900267-OrganizationReplication.ts
@@ -7,9 +7,17 @@ export class OrganizationReplication1757339900267
     queryRunner.query(/* sql */ `
       ALTER TABLE "public"."organization" REPLICA IDENTITY FULL;
     `);
+
+    queryRunner.query(/* sql */ `
+      ALTER TABLE "public"."opportunity_match" REPLICA IDENTITY FULL;
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(/* sql */ `
+      ALTER TABLE "public"."opportunity_match" REPLICA IDENTITY DEFAULT;
+    `);
+
     queryRunner.query(/* sql */ `
       ALTER TABLE "public"."organization" REPLICA IDENTITY DEFAULT;
     `);
