d238ddc359b5aeebc770903fc7d266ce16ffa6ef
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-09-10 13:45:22 +0200

    feat: getCandidatePreferences query (#3086)

--- src/common/schema/userCandidate.ts
+++ src/common/schema/userCandidate.ts
@@ -1,7 +1,9 @@
 import z from 'zod';
 
 export const userCandidateCVSchema = z.object({
-  bucket: z.string(),
-  blob: z.string(),
-  lastModified: z.date(),
+  bucket: z.string().optional(),
+  blob: z.string().optional(),
+  lastModified: z.date().optional(),
 });
+
+export type UserCandidateCV = z.infer<typeof userCandidateCVSchema>;
