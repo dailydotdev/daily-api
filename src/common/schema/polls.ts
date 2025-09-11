1e893f9f283932db0f2129a8cc491e922de323dc
Amar Trebinjac <36768584+AmarTrebinjac@users.noreply.github.com>
2025-09-11 20:55:18 +0900

    feat: create poll (#3070)

---
+++ src/common/schema/polls.ts
@@ -0,0 +1,24 @@
+import z from 'zod';
+
+export const pollCreationSchema = z.object({
+  title: z.string().max(250),
+  sourceId: z.string(),
+  duration: z
+    .union([
+      z.literal(1),
+      z.literal(3),
+      z.literal(7),
+      z.literal(14),
+      z.literal(30),
+    ])
+    .optional(),
+  options: z
+    .array(
+      z.object({
+        text: z.string().max(35),
+        order: z.number().min(0).max(4),
+      }),
+    )
+    .min(2)
+    .max(4),
+});
