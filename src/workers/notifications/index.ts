520813ac18ea3b8111d07c049dc21fb5e01fb356
Amar Trebinjac <36768584+AmarTrebinjac@users.noreply.github.com>
2025-09-19 15:52:04 +0200

    feat: poll workers (#3141)
    Co-authored-by: Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>

--- src/workers/notifications/index.ts
+++ src/workers/notifications/index.ts
@@ -36,6 +36,8 @@ import { userBriefReadyNotification } from './userBriefReadyNotification';
 import { userFollowNotification } from './userFollowNotification';
 import { candidateOpportunityMatchNotification } from './candidateOpportunityMatchNotification';
 import { campaignPostAnalyticsNotification } from './campaignPostAnalyticsNotification';
+import { pollResultNotification } from './pollResultNotification';
+import { pollResultAuthorNotification } from './pollResultAuthorNotification';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type AnyNotificationWorker = NotificationWorker | TypedNotificationWorker<any>;
@@ -120,6 +122,8 @@ const notificationWorkers: AnyNotificationWorker[] = [
   userFollowNotification,
   candidateOpportunityMatchNotification,
   campaignPostAnalyticsNotification,
+  pollResultNotification,
+  pollResultAuthorNotification,
 ];
 
 export const workers = [...notificationWorkers.map(notificationWorkerToWorker)];
