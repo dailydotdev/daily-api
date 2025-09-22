d9cad9732aa760f465176cecc24cfa6a56be9a2f
Chris Bongers <chrisbongers@gmail.com>
2025-09-22 15:33:03 +0200

    chore: migrate another worker

--- src/workers/notifications/articleNewCommentPostCommented.ts
+++ src/workers/notifications/articleNewCommentPostCommented.ts
@@ -1,20 +1,9 @@
 import { messageToJson, TypedNotificationWorker } from '../worker';
 import { articleNewCommentHandler } from './utils';
 
-export interface Data {
-  userId: string;
-  commentId: string;
-  postId: string;
-}
-
 export const articleNewCommentPostCommented: TypedNotificationWorker<'post-commented'> =
   {
     subscription: 'api.article-new-comment-notification.post-commented',
-    handler: ({ commentId }, con) => {
-      return articleNewCommentHandler(con, commentId);
-    },
-    parseMessage(message) {
-      // TODO: Clean this once we move all workers to TypedWorkers
-      return messageToJson(message);
-    },
+    handler: ({ commentId }, con) => articleNewCommentHandler(con, commentId),
+    parseMessage: (message) => messageToJson(message), // TODO: Clean this once we move all workers to TypedWorkers
   };
