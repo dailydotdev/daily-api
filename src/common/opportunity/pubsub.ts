34b98626a380de19c94252383586a2a6c5a488bb
Chris Bongers <chrisbongers@gmail.com>
2025-10-02 10:05:32 +0200

    fix: cleanup isUpdate

--- src/common/opportunity/pubsub.ts
+++ src/common/opportunity/pubsub.ts
@@ -268,7 +268,6 @@ export const notifyJobOpportunity = async ({
 }: {
   con: DataSource;
   logger: FastifyBaseLogger;
-  isUpdate: boolean;
   opportunityId: string;
 }) => {
   const [opportunity, organization, keywords] = await queryReadReplica(
