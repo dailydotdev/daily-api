83e1f6c71528afb8879bf3e9f80d47ba12192861
nensidosari <36197304+nensidosari@users.noreply.github.com>
2025-10-07 09:57:39 +0200

    chore: cleanup show_squad_unread_posts (#3045)

--- src/schema/sources.ts
+++ src/schema/sources.ts
@@ -322,10 +322,6 @@ export const typeDefs = /* GraphQL */ `
     Whether the source pinned posts are collapsed or not
     """
     collapsePinnedPosts: Boolean
-    """
-    Whether the source has unread posts for member
-    """
-    hasUnreadPosts: Boolean
   }
 
   type SourceMember {
@@ -909,16 +905,6 @@ export const typeDefs = /* GraphQL */ `
     expandPinnedPosts(
       """
       Source id to expand posts in
-      """
-      sourceId: ID!
-    ): EmptyResponse! @auth
-
-    """
-    Clear unread posts flag for user
-    """
-    clearUnreadPosts(
-      """
-      Source id to clear unread posts in
       """
       sourceId: ID!
     ): EmptyResponse! @auth
@@ -2640,26 +2626,6 @@ export const resolvers: IResolvers<unknown, BaseContext> = traceResolvers<
       ctx: AuthContext,
     ) => {
       return togglePinnedPosts(ctx, sourceId, false);
-    },
-    clearUnreadPosts: async (
-      _,
-      { sourceId }: { sourceId: string },
-      ctx: AuthContext,
-    ) => {
-      const source = await ensureSourcePermissions(ctx, sourceId);
-
-      const result = await ctx.con.getRepository(SourceMember).update(
-        { sourceId: source.id, userId: ctx.userId },
-        {
-          flags: updateFlagsStatement<SourceMember>({
-            hasUnreadPosts: false,
-          }),
-        },
-      );
-
-      return {
-        _: !!result.affected,
-      };
     },
   },
   Source: {
