2c33f0043e8c5bade8e9cb9b8eed024f41a3883d
Lee Hansel Solevilla <sshanzel@yahoo.com>
2025-10-08 17:26:31 +0800

    refactor: usage of zod transform

--- src/common/schema/autocompletes.ts
+++ src/common/schema/autocompletes.ts
@@ -1,9 +1,8 @@
 import z from 'zod';
-import { normalizeContentForDeduplication } from '../../entity/posts/hooks';
 import { AutocompleteType } from '../../entity';
 
 export const autocompleteSchema = z.object({
   type: z.enum(AutocompleteType),
-  query: z.string().transform(normalizeContentForDeduplication),
+  query: z.string().trim().lowercase().normalize(),
   limit: z.number().min(1).max(50).default(20),
 });
