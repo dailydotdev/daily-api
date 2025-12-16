0140717d5cde612538b55357f6e944fe285a8890
Chris Bongers <chrisbongers@gmail.com>
2025-12-16 12:02:34 +0200

    Update src/migration/1765793807971-AddOpportunityLocationTable.ts
    Co-authored-by: Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>

--- src/migration/1765793807971-AddOpportunityLocationTable.ts
+++ src/migration/1765793807971-AddOpportunityLocationTable.ts
@@ -6,40 +6,54 @@ export class AddOpportunityLocationTable1765793807971
   name = 'AddOpportunityLocationTable1765793807971';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `CREATE TABLE "opportunity_location" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "opportunityId" uuid NOT NULL, "locationId" uuid NOT NULL, "type" integer NOT NULL, CONSTRAINT "PK_opportunity_location_id" PRIMARY KEY ("id")); COMMENT ON COLUMN "opportunity_location"."type" IS 'LocationType from protobuf schema'`,
-    );
-    await queryRunner.query(
-      `CREATE INDEX "IDX_opportunity_location_opportunityId" ON "opportunity_location" ("opportunityId") `,
-    );
-    await queryRunner.query(
-      `CREATE INDEX "IDX_opportunity_location_locationId" ON "opportunity_location" ("locationId") `,
-    );
-    await queryRunner.query(`ALTER TABLE "opportunity" DROP COLUMN "location"`);
-    await queryRunner.query(
-      `ALTER TABLE "opportunity_location" ADD CONSTRAINT "FK_opportunity_location_opportunity_opportunityId" FOREIGN KEY ("opportunityId") REFERENCES "opportunity"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "opportunity_location" ADD CONSTRAINT "FK_opportunity_location_dataset_location_locationId" FOREIGN KEY ("locationId") REFERENCES "dataset_location"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
-    );
+    await queryRunner.query(/* sql */ `
+      CREATE TABLE "opportunity_location" (
+        "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+        "opportunityId" uuid NOT NULL,
+        "locationId" uuid NOT NULL,
+        "type" integer NOT NULL,
+        CONSTRAINT "PK_opportunity_location_id" PRIMARY KEY ("id"),
+        CONSTRAINT "FK_opportunity_location_opportunity_opportunityId"
+          FOREIGN KEY ("opportunityId")
+          REFERENCES "opportunity"("id")
+          ON DELETE CASCADE
+          ON UPDATE NO ACTION,
+        CONSTRAINT "FK_opportunity_location_dataset_location_locationId"
+          FOREIGN KEY ("locationId")
+          REFERENCES "dataset_location"("id")
+          ON DELETE CASCADE
+          ON UPDATE NO ACTION
+      )
+    `);
+
+    await queryRunner.query(/* sql */ `
+      COMMENT ON COLUMN "opportunity_location"."type" IS 'LocationType from protobuf schema'
+    `);
+
+    await queryRunner.query(/* sql */ `
+      CREATE INDEX IF NOT EXISTS "IDX_opportunity_location_opportunityId"
+        ON "opportunity_location" ("opportunityId")
+    `);
+
+    await queryRunner.query(/* sql */ `
+      CREATE INDEX IF NOT EXISTS "IDX_opportunity_location_locationId"
+        ON "opportunity_location" ("locationId")
+    `);
+
+    await queryRunner.query(/* sql */ `
+      ALTER TABLE "opportunity"
+        DROP COLUMN "location"
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "opportunity_location" DROP CONSTRAINT "FK_opportunity_location_dataset_location_locationId"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "opportunity_location" DROP CONSTRAINT "FK_opportunity_location_opportunity_opportunityId"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "opportunity" ADD "location" jsonb DEFAULT '[]'`,
-    );
-    await queryRunner.query(
-      `DROP INDEX "public"."IDX_opportunity_location_locationId"`,
-    );
-    await queryRunner.query(
-      `DROP INDEX "public"."IDX_opportunity_location_opportunityId"`,
-    );
-    await queryRunner.query(`DROP TABLE "opportunity_location"`);
+    await queryRunner.query(/* sql */ `
+      ALTER TABLE "opportunity"
+        ADD "location" jsonb DEFAULT '[]'
+    `);
+
+    await queryRunner.query(/* sql */ `
+      DROP TABLE "opportunity_location"
+    `);
   }
 }
