f5cbeeebae935767ed64c4cc968214857b8bf7e3
Chris Bongers <chrisbongers@gmail.com>
2026-01-16 14:55:14 +0200

    fix: query rr

--- src/schema/opportunity.ts
+++ src/schema/opportunity.ts
@@ -1344,26 +1344,27 @@ export const resolvers: IResolvers<unknown, BaseContext> = traceResolvers<
 
       return opportunity;
     },
-    opportunityByIdPublic: async (_, { id }: { id: string }, ctx: Context) => {
-      const opportunity = await ctx.con
-        .getRepository(OpportunityJob)
-        .createQueryBuilder('o')
-        .leftJoinAndSelect('o.organization', 'org')
-        .where('o.id = :id', { id })
-        .getOne();
+    opportunityByIdPublic: async (_, { id }: { id: string }, ctx: Context) =>
+      queryReadReplica(ctx.con, async ({ queryRunner }) => {
+        const opportunity = await queryRunner.manager
+          .getRepository(OpportunityJob)
+          .createQueryBuilder('o')
+          .leftJoinAndSelect('o.organization', 'org')
+          .where('o.id = :id', { id })
+          .getOne();
 
-      if (!opportunity) {
-        return null;
-      }
+        if (!opportunity) {
+          return null;
+        }
 
-      const organization = await opportunity.organization;
-      return {
-        id: opportunity.id,
-        title: opportunity.title,
-        organization: organization ? { name: organization.name } : null,
-        flags: { plan: opportunity.flags?.plan ?? null },
-      };
-    },
+        const organization = await opportunity.organization;
+        return {
+          id: opportunity.id,
+          title: opportunity.title,
+          organization: organization ? { name: organization.name } : null,
+          flags: { plan: opportunity.flags?.plan ?? null },
+        };
+      }),
     getOpportunityMatch: async (
       _,
       { id }: { id: string },
