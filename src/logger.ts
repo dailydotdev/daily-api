072a2d73bedc8b718d2c915dc18852bb59287470
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2026-02-13 15:30:36 +0100

    chore: add option to switch logging format

--- src/logger.ts
+++ src/logger.ts
@@ -3,12 +3,32 @@ import { env } from 'node:process';
 import { createGcpLoggingPinoConfig } from '@google-cloud/pino-logging-gcp-config';
 import pino, { type LoggerOptions } from 'pino';
 
+const DEFAULT_SEVERITY_NUMBER_MAP = {
+  10: 1, // TRACE
+  20: 5, // DEBUG
+  30: 9, // INFO
+  40: 13, // WARN
+  50: 17, // ERROR
+  60: 21, // FATAL
+};
+
 const pinoLoggerOptions: LoggerOptions = {
-  level: env.LOG_LEVEL || 'info',
+  level: process.env.LOG_LEVEL || 'info',
+  formatters: {
+    level(severity, level) {
+      return {
+        severityText: severity,
+        severityNumber:
+          DEFAULT_SEVERITY_NUMBER_MAP[
+            level as keyof typeof DEFAULT_SEVERITY_NUMBER_MAP
+          ] || DEFAULT_SEVERITY_NUMBER_MAP[30], // default to INFO
+      };
+    },
+  },
 };
 
 export const loggerConfig: LoggerOptions =
-  env.NODE_ENV === 'production'
+  env.NODE_ENV === 'production' && env.OTEL_LOGGER_FORMAT === 'gcp'
     ? createGcpLoggingPinoConfig(
         {
           serviceContext: {
