e24dca53d7d6a207073df8dd5ccb02b17a801b05
rebelchris <chrisbongers@gmail.com>
2026-03-05 09:14:30 +0000

    fix(boot): suppress CDC for extension lastExtensionUse updates

--- src/routes/boot.ts
+++ src/routes/boot.ts
@@ -226,6 +226,7 @@ const updateLastExtensionUse = async ({
       flags: updateFlagsStatement<User>({
         lastExtensionUse: new Date(),
       }),
+      inc: Math.floor(Date.now() / 1000),
     },
   );
 };
