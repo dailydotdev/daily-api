ccd4e067ee0f1803cf98f818997aa5b9cfb8e8e3
Chris Bongers <chrisbongers@gmail.com>
2026-03-05 13:11:39 +0200

    fix(boot): filter extension usage updates from CDC (#3672)
    Co-authored-by: Ante Barić <ante@kickass.website>

--- __tests__/boot.ts
+++ __tests__/boot.ts
@@ -541,6 +541,7 @@ describe('logged in boot', () => {
     expect(
       new Date(user.flags.lastExtensionUse as Date).getTime(),
     ).toBeGreaterThanOrEqual(requestStart);
+    expect(user.inc).toBeGreaterThan(0);
   });
 
   it('should not set lastExtensionUse when app header is not extension', async () => {
