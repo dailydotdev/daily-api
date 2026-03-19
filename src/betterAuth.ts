28aa8e3a6b394e86d24d1653347ee78c215eb106
Chris Bongers <chrisbongers@gmail.com>
2026-03-19 13:37:15 +0200

    fix: google public key

--- src/betterAuth.ts
+++ src/betterAuth.ts
@@ -4,8 +4,7 @@ import { captcha, emailOTP } from 'better-auth/plugins';
 import type { Pool } from 'pg';
 import * as argon2 from 'argon2';
 import { z } from 'zod';
-import { decodeProtectedHeader, jwtVerify } from 'jose';
-import { getGooglePublicKey } from '@better-auth/core/social-providers';
+import { decodeProtectedHeader, importJWK, jwtVerify } from 'jose';
 import { AppDataSource } from './data-source';
 import { logger } from './logger';
 import { triggerTypedEvent } from './common/typedPubsub';
@@ -17,6 +16,18 @@ import { User } from './entity/user/User';
 import { fetchOptions } from './http';
 import { retryFetch } from './integrations/retry';
 import { cookies, extractRootDomain } from './cookies';
+
+const GOOGLE_CERTS_URL = 'https://www.googleapis.com/oauth2/v3/certs';
+
+const getGooglePublicKey = async (kid: string) => {
+  const res = await fetch(GOOGLE_CERTS_URL);
+  const { keys } = (await res.json()) as {
+    keys: Array<{ kid: string; alg: string }>;
+  };
+  const jwk = keys.find((key) => key.kid === kid);
+  if (!jwk) throw new Error(`JWK with kid ${kid} not found`);
+  return importJWK(jwk, jwk.alg);
+};
 
 const BETTER_AUTH_SECRET_MIN_LENGTH = 32;
 const turnstileSecretKey = process.env.TURNSTILE_SECRET_KEY;
