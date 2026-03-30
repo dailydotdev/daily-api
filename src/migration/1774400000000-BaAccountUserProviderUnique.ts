bd11cca6b215a042b4f4750f80c5ebe4d93ed93d
Chris Bongers <chrisbongers@gmail.com>
2026-03-30 12:27:05 +0200

    fix: migration unique check

--- src/migration/1774400000000-BaAccountUserProviderUnique.ts
+++ src/migration/1774400000000-BaAccountUserProviderUnique.ts
@@ -1,13 +1,11 @@
 import type { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class BaAccountUserProviderUnique1774400000000
-  implements MigrationInterface
-{
+export class BaAccountUserProviderUnique1774400000000 implements MigrationInterface {
   name = 'BaAccountUserProviderUnique1774400000000';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(/* sql */ `
-      CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS
+      CREATE UNIQUE INDEX IF NOT EXISTS
         "IDX_ba_account_userId_providerId"
         ON "ba_account" ("userId", "providerId")
     `);
@@ -15,7 +13,7 @@ export class BaAccountUserProviderUnique1774400000000
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(/* sql */ `
-      DROP INDEX CONCURRENTLY IF EXISTS "IDX_ba_account_userId_providerId"
+      DROP INDEX IF EXISTS "IDX_ba_account_userId_providerId"
     `);
   }
 }
