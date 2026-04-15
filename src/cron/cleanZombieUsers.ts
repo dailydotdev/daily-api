627fa35588cd5b1ace8a87c76058ece4168ad431
Ido Shamun <1993245+idoshamun@users.noreply.github.com>
2026-04-15 10:58:55 +0300

    refactor: delete zombie users in batch

--- src/cron/cleanZombieUsers.ts
+++ src/cron/cleanZombieUsers.ts
@@ -1,31 +1,35 @@
 import { Cron } from './cron';
 import { User } from '../entity';
-import { LessThan } from 'typeorm';
+import { LessThan, In } from 'typeorm';
 import { subHours } from 'date-fns';
+
+const BATCH_SIZE = 50;
 
 const cron: Cron = {
   name: 'clean-zombie-users',
   handler: async (con, logger) => {
     logger.info('cleaning zombie users...');
     const timeThreshold = subHours(new Date(), 1);
-    const query = con
-      .createQueryBuilder()
-      .delete()
-      .from(User)
-      .where([
-        {
-          infoConfirmed: false,
-        },
-        {
-          emailConfirmed: false,
-        },
-      ])
-      .andWhere({
-        createdAt: LessThan(timeThreshold),
-      });
+    const userRepo = con.getRepository(User);
 
-    const { affected } = await query.execute();
-    logger.info({ count: affected }, 'zombies users cleaned! 🧟');
+    const zombieUsers = await userRepo.find({
+      select: ['id'],
+      where: [
+        { infoConfirmed: false, createdAt: LessThan(timeThreshold) },
+        { emailConfirmed: false, createdAt: LessThan(timeThreshold) },
+      ],
+    });
+
+    const ids = zombieUsers.map((u) => u.id);
+    let totalDeleted = 0;
+
+    for (let i = 0; i < ids.length; i += BATCH_SIZE) {
+      const batch = ids.slice(i, i + BATCH_SIZE);
+      const { affected } = await userRepo.delete({ id: In(batch) });
+      totalDeleted += affected ?? 0;
+    }
+
+    logger.info({ count: totalDeleted }, 'zombies users cleaned! 🧟');
   },
 };
 
